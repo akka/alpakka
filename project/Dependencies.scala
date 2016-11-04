0463399d514407938639aa5fc14cf894877eacc0
Konrad Malawski <ktoso@project13.pl>
2016-11-04 10:09:46 +0100

    Update Dependencies.scala

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._, Keys._
 
 object Dependencies {
 
-  val ScalaVersions = Seq("2.11.8", "2.12.0-RC2")
+  val ScalaVersions = Seq("2.11.8", "2.12.0")
   val AkkaVersion = "2.4.12"
 
   val Common = Seq(
