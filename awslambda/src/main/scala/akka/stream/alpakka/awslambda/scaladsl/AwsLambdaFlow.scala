4c445be1b8e5b46fbb694e8a6d72b7fda86f02df
viktor <viktor.kocherga@trademachines.com>
2017-01-26 17:36:56 +0100

    Added aws lambda flow implementation
    * Added documentation
    * Added asynchronous failure handler

---
+++ awslambda/src/main/scala/akka/stream/alpakka/awslambda/scaladsl/AwsLambdaFlow.scala
@@ -0,0 +1,21 @@
+/*
+ * Copyright (C) 2016 Lightbend Inc. <http://www.lightbend.com>
+ */
+package akka.stream.alpakka.awslambda.scaladsl
+
+import akka.NotUsed
+import akka.stream.alpakka.awslambda.AwsLambdaFlowStage
+import akka.stream.scaladsl.Flow
+import com.amazonaws.services.lambda.model.{InvokeRequest, InvokeResult}
+import com.amazonaws.services.lambda.{AWSLambdaAsyncClient, AWSLambdaClient}
+
+object AwsLambdaFlow {
+
+  /**
+   * Scala API: creates a [[AwsLambdaFlowStage]] for a AWS Lambda function invocation using [[AWSLambdaClient]]
+   */
+  def apply(parallelism: Int)(
+      implicit awsLambdaClient: AWSLambdaAsyncClient): Flow[InvokeRequest, InvokeResult, NotUsed] =
+    Flow.fromGraph(new AwsLambdaFlowStage(awsLambdaClient)(parallelism))
+
+}
