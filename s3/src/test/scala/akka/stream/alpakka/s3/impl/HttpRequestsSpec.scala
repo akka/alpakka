410056cf833e979ff7a5ea5a669366d50047b1c4
Nicolas Joseph <nikel092@gmail.com>
2017-02-13 03:11:19 -0500

    S3 - Add case handling for Us Standard zone (#177)
    * Add case handling for Us Standard zone
    
    * Fixing tests

--- s3/src/test/scala/akka/stream/alpakka/s3/impl/HttpRequestsSpec.scala
+++ s3/src/test/scala/akka/stream/alpakka/s3/impl/HttpRequestsSpec.scala
@@ -14,7 +14,7 @@ import org.scalatest.{FlatSpec, Matchers}
 import scala.collection.immutable.Seq
 
 class HttpRequestsSpec extends FlatSpec with Matchers {
-  it should "initiate multipart upload" in {
+  it should "initiate multipart upload when the region is us-east-1" in {
     implicit val settings = S3Settings(ActorSystem())
 
     val location = S3Location("bucket", "image-1024@2x")
@@ -27,7 +27,27 @@ class HttpRequestsSpec extends FlatSpec with Matchers {
 
     req.entity shouldEqual HttpEntity.empty(contentType)
     req.headers should contain(RawHeader("x-amz-acl", acl.value))
-    req.uri.authority.host.toString shouldEqual "bucket.s3-us-east-1.amazonaws.com"
+    req.uri.authority.host.toString shouldEqual "bucket.s3.amazonaws.com"
+
+    metaHeaders.map { m =>
+      req.headers should contain(RawHeader(s"x-amz-meta-${m._1}", m._2))
+    }
+  }
+
+  it should "initiate multipart upload with other regions" in {
+    implicit val settings = S3Settings(ActorSystem())
+
+    val location = S3Location("bucket", "image-1024@2x")
+    val contentType = MediaTypes.`image/jpeg`
+    val acl = CannedAcl.PublicRead
+    val metaHeaders: Map[String, String] = Map("location" -> "San Francisco", "orientation" -> "portrait")
+
+    val req =
+      HttpRequests.initiateMultipartUploadRequest(location, contentType, acl, "us-east-2", MetaHeaders(metaHeaders))
+
+    req.entity shouldEqual HttpEntity.empty(contentType)
+    req.headers should contain(RawHeader("x-amz-acl", acl.value))
+    req.uri.authority.host.toString shouldEqual "bucket.s3-us-east-2.amazonaws.com"
 
     metaHeaders.map { m =>
       req.headers should contain(RawHeader(s"x-amz-meta-${m._1}", m._2))
