ba5d918e9dc4db970f4e92d4f0a9175ccec65fc3
İsmet Özöztürk <ismet.ozozturk@eu.sony.com>
2017-02-25 22:14:38 +0300

    MongoSource uses Observable to Publisher adapter explicitly instead of implicitly

--- mongodb/src/main/scala/akka/stream/alpakka/mongodb/scaladsl/MongoSource.scala
+++ mongodb/src/main/scala/akka/stream/alpakka/mongodb/scaladsl/MongoSource.scala
@@ -1,13 +1,13 @@
 package akka.stream.alpakka.mongodb.scaladsl
 
-import akka.stream.alpakka.mongodb.Implicits._
+import akka.stream.alpakka.mongodb.ObservableToPublisher
 import akka.stream.scaladsl.Source
 import org.mongodb.scala.{Document, Observable}
 
 object MongoSource {
 
-  def apply(query: Observable[Document]) ={
-    Source.fromPublisher(query)
+  def apply(query: Observable[Document]) = {
+    Source.fromPublisher(ObservableToPublisher(query))
   }
 
 }
