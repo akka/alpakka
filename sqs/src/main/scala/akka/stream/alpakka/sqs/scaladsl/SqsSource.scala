31f9cf110376230d2ce889bf662251254afa7bc8
Daniel Pfeiffer <dpfeiffer@users.noreply.github.com>
2017-03-23 13:59:36 +0100

    = #135 Limit parallelism for the SqsSource (#163)
    * Create a custom thread pool for the SqsSource and limit concurrency with the buffer size
    
    * Provide AWSCredentials for all SqsSource tests
    
    * Provide better java api
    
    * Use ArrayDeque as FIFO queue
    Messages for asserts in SqsSourceSettings
    
    * Remove AmazonSQSAsyncClient from factory method
    
    - Correct the javadoc
    
    * Add implicit AmazonSQSAsync to factory method
    
    - It now conforms again with the SqsSink
    - Passing the clients from extern seems the preferred way (awslambda module)
    
    * Update documentation to reflect thread pool usage
    
    * Improvements from review by ktoso
    
    - Replace IntStream with Source.range
    - Use Sink.head instead of Sink.seq

--- sqs/src/main/scala/akka/stream/alpakka/sqs/scaladsl/SqsSource.scala
+++ sqs/src/main/scala/akka/stream/alpakka/sqs/scaladsl/SqsSource.scala
@@ -6,16 +6,17 @@ package akka.stream.alpakka.sqs.scaladsl
 import akka.NotUsed
 import akka.stream.alpakka.sqs.{SqsSourceSettings, SqsSourceStage}
 import akka.stream.scaladsl.Source
-import com.amazonaws.services.sqs.AmazonSQSAsyncClient
+import com.amazonaws.services.sqs.AmazonSQSAsync
 import com.amazonaws.services.sqs.model.Message
 
 object SqsSource {
 
   /**
-   * Scala API: creates a [[SqsSourceStage]] for a SQS queue using an [[AmazonSQSAsyncClient]]
+   * Scala API: creates a [[SqsSourceStage]] for a SQS queue.
    */
   def apply(queueUrl: String, settings: SqsSourceSettings = SqsSourceSettings.Defaults)(
-      implicit sqsClient: AmazonSQSAsyncClient): Source[Message, NotUsed] =
-    Source.fromGraph(new SqsSourceStage(queueUrl, settings, sqsClient))
+      implicit sqs: AmazonSQSAsync
+  ): Source[Message, NotUsed] =
+    Source.fromGraph(new SqsSourceStage(queueUrl, settings))
 
 }
