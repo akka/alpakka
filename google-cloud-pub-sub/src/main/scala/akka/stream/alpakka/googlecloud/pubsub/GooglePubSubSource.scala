d482b062a84b4f2a2ca7dab8a12ee1bd080dee1d
Stephen Nancekivell <stephen.nancekivell@gmail.com>
2017-03-25 17:38:51 +1100

    PR feedback. Improve java api, java examples, better json marshalling.

--- google-cloud-pub-sub/src/main/scala/akka/stream/alpakka/googlecloud/pubsub/GooglePubSubSource.scala
+++ google-cloud-pub-sub/src/main/scala/akka/stream/alpakka/googlecloud/pubsub/GooglePubSubSource.scala
@@ -1,32 +1,31 @@
 /*
  * Copyright (C) 2016 Lightbend Inc. <http://www.lightbend.com>
  */
-package akka.stream.alpakka.googlepubsub
+package akka.stream.alpakka.googlecloud.pubsub
 
 import akka.actor.ActorSystem
 import akka.stream.{Attributes, Materializer, Outlet, SourceShape}
 import akka.stream.stage.{GraphStage, GraphStageLogic, OutHandler}
 
-import scala.util.Try
+import scala.util.{Failure, Success, Try}
+import GooglePubSubSource._
 
-final class SubscriptionSourceStage(projectId: String,
-                                    apiKey: String,
-                                    session: Session,
-                                    subscription: String,
-                                    httpApi: HttpApi)(implicit as: ActorSystem)
+import scala.collection.immutable
+
+@akka.annotation.InternalApi
+private final class GooglePubSubSource(projectId: String,
+                                       apiKey: String,
+                                       session: Session,
+                                       subscription: String,
+                                       httpApi: HttpApi)(implicit as: ActorSystem)
     extends GraphStage[SourceShape[ReceivedMessage]] {
 
-  val out: Outlet[ReceivedMessage] = Outlet("GooglePubSub.out")
+  val out: Outlet[ReceivedMessage] = Outlet("GooglePubSubSource.out")
   override val shape: SourceShape[ReceivedMessage] = SourceShape(out)
 
   override def createLogic(inheritedAttributes: Attributes): GraphStageLogic =
     new GraphStageLogic(shape) {
       private var state: State = Pending
-
-      sealed trait State
-      case object Pending extends State
-      case object Fetching extends State
-      case class HoldingMessages(xs: Seq[ReceivedMessage]) extends State
 
       def fetch(implicit mat: Materializer): Unit = {
         import mat.executionContext
@@ -41,25 +40,22 @@ final class SubscriptionSourceStage(projectId: String,
       }
 
       private val callback = getAsyncCallback[(Try[PullResponse], Materializer)] {
-        case (tr, mat) =>
-          tr match {
-            case util.Failure(th) =>
-              failStage(th)
-            case util.Success(response) =>
-              response.receivedMessages.getOrElse(Nil) match {
-                case head :: tail =>
-                  state match {
-                    case HoldingMessages(oldHead :: oldTail) =>
-                      state = HoldingMessages(oldTail ++ Seq(head) ++ tail)
-                      push(out, oldHead)
-                    case _ =>
-                      state = HoldingMessages(tail)
-                      push(out, head)
-                  }
-                case Nil =>
-                  state = Fetching
-                  fetch(mat)
+        case (Failure(tr), _) =>
+          failStage(tr)
+        case (Success(response), mat) =>
+          response.receivedMessages.getOrElse(Nil) match {
+            case head :: tail =>
+              state match {
+                case HoldingMessages(oldHead :: oldTail) =>
+                  state = HoldingMessages(oldTail ++ Seq(head) ++ tail)
+                  push(out, oldHead)
+                case _ =>
+                  state = HoldingMessages(tail)
+                  push(out, head)
               }
+            case Nil =>
+              state = Fetching
+              fetch(mat)
           }
       }
 
@@ -84,4 +80,12 @@ final class SubscriptionSourceStage(projectId: String,
           }
       })
     }
+}
+
+@akka.annotation.InternalApi
+private object GooglePubSubSource {
+  private sealed trait State
+  private case object Pending extends State
+  private case object Fetching extends State
+  private case class HoldingMessages(xs: immutable.Seq[ReceivedMessage]) extends State
 }
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -22,7 +22,7 @@ object Dependencies {
       "com.typesafe.akka"     %% "akka-http-xml" % AkkaHttpVersion,
       // in-memory filesystem for file related tests
       "com.google.jimfs"       % "jimfs"         % "1.1"   % Test, // ApacheV2
-      "com.github.tomakehurst" % "wiremock"      % "2.3.1" % Test //ApacheV2
+      "com.github.tomakehurst" % "wiremock"      % "2.5.1" % Test //ApacheV2
     )
   )
 
@@ -123,7 +123,7 @@ object Dependencies {
       "com.typesafe.akka"     %% "akka-http"     % AkkaHttpVersion,
       "com.typesafe.akka" %% "akka-http-spray-json" % AkkaHttpVersion,
       "org.mockito"   % "mockito-core"     % "2.3.7"    % Test, // MIT
-      "com.github.tomakehurst" % "wiremock"      % "2.3.1" % Test //ApacheV2
+      "com.github.tomakehurst" % "wiremock"      % "2.5.1" % Test //ApacheV2
     )
   )
 
