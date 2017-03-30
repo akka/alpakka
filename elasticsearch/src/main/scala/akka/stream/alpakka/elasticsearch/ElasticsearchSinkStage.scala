737f5b0844d09e3b0bfeeac599bd649afe817489
Naoki Takezoe <takezoe@gmail.com>
2017-03-30 18:58:10 +0900

    Fix buffering in ElasticsearchSink

--- elasticsearch/src/main/scala/akka/stream/alpakka/elasticsearch/ElasticsearchSinkStage.scala
+++ elasticsearch/src/main/scala/akka/stream/alpakka/elasticsearch/ElasticsearchSinkStage.scala
@@ -1,15 +1,16 @@
 /*
- * Copyright (C) 2016 Lightbend Inc. <http://www.lightbend.com>
+ * Copyright (C) 2016-2017 Lightbend Inc. <http://www.lightbend.com>
  */
 package akka.stream.alpakka.elasticsearch
 
 import java.util
+import java.util.concurrent.atomic.AtomicReference
 
 import akka.Done
 import akka.stream._
 import akka.stream.stage.{GraphStageLogic, GraphStageWithMaterializedValue, InHandler}
 import org.apache.http.entity.StringEntity
-import org.elasticsearch.client.RestClient
+import org.elasticsearch.client.{Response, ResponseListener, RestClient}
 import spray.json._
 
 import scala.collection.JavaConverters._
@@ -68,38 +69,62 @@ sealed abstract class ElasticsearchSinkLogic[T](indexName: String,
                                                 promise: Promise[Done])
     extends GraphStageLogic(shape) {
 
-  private val buffer = new util.ArrayDeque[IncomingMessage[T]]()
+  private val state = new AtomicReference[State](Idle)
+  private val buffer = new util.concurrent.ConcurrentLinkedQueue[IncomingMessage[T]]()
+  private val sentHandler = getAsyncCallback[Unit](_ => handleSent())
 
   protected def convert(value: T): JsObject
 
   override def preStart(): Unit =
     pull(in)
 
+  private def tryPull(): Unit =
+    if (buffer.size < settings.bufferSize && !isClosed(in) && !hasBeenPulled(in)) {
+      pull(in)
+    }
+
+  private def handleFailure(exception: Throwable): Unit = {
+    failStage(exception)
+    promise.tryFailure(exception)
+  }
+
+  private def handleSuccess(): Unit = {
+    completeStage()
+    promise.trySuccess(Done)
+  }
+
+  private def handleSent(): Unit =
+    tryPull()
+
   setHandler(in,
     new InHandler {
     override def onPush(): Unit = {
       val message = grab(in)
-      buffer.addLast(message)
-      if (buffer.size >= settings.bufferSize) {
-        sendBulkRequest(buffer.toArray(new Array[IncomingMessage[T]](buffer.size)))
-        buffer.clear()
+      buffer.add(message)
+
+      state.get match {
+        case Idle => {
+          state.set(Sending)
+          val messages = (1 to settings.bufferSize).flatMap { _ =>
+            Option(buffer.poll())
+          }
+          sendBulkRequest(messages)
+        }
+        case _ => ()
       }
-      pull(in)
+
+      tryPull()
     }
 
-    override def onUpstreamFailure(exception: Throwable): Unit = {
-      failStage(exception)
-      promise.tryFailure(exception)
-    }
+    override def onUpstreamFailure(exception: Throwable): Unit =
+      handleFailure(exception)
 
-    override def onUpstreamFinish(): Unit = {
-      if (!buffer.isEmpty) {
-        sendBulkRequest(buffer.toArray(new Array[IncomingMessage[T]](buffer.size)))
-        buffer.clear()
+    override def onUpstreamFinish(): Unit =
+      state.get match {
+        case Idle => handleSuccess()
+        case Sending => state.set(Finished)
+        case Finished => ()
       }
-      completeStage()
-      promise.trySuccess(Done)
-    }
 
     private def sendBulkRequest(messages: Seq[IncomingMessage[T]]): Unit =
       try {
@@ -107,14 +132,34 @@ sealed abstract class ElasticsearchSinkLogic[T](indexName: String,
           s"""{"index": {"_index": "${indexName}", "_type": "${typeName}"${message.id.map { id =>
                s""", "_id": "${id}""""
              }.getOrElse("")}}
-               |${convert(message.source).toString}""".stripMargin
+          |${convert(message.source).toString}""".stripMargin
         }.mkString("", "\n", "\n")
 
-        client.performRequest(
+        client.performRequestAsync(
           "POST",
           "_bulk",
           Map[String, String]().asJava,
-          new StringEntity(json)
+          new StringEntity(json),
+          new ResponseListener {
+            override def onFailure(exception: Exception): Unit = handleFailure(exception)
+
+            override def onSuccess(response: Response): Unit = {
+              val messages = (1 to settings.bufferSize).flatMap { _ =>
+                Option(buffer.poll())
+              }
+
+              if (messages.isEmpty) {
+                state.get match {
+                  case Finished => handleSuccess()
+                  case _ => state.set(Idle)
+                }
+              } else {
+                sendBulkRequest(messages)
+              }
+
+              sentHandler.invoke(())
+            }
+          }
         )
       } catch {
         case ex: Exception => failStage(ex)
@@ -122,3 +167,8 @@ sealed abstract class ElasticsearchSinkLogic[T](indexName: String,
   })
 
 }
+
+private sealed trait State
+private case object Idle extends State
+private case object Sending extends State
+private case object Finished extends State
