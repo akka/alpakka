10c8f9505d79658281ff4c07fead7fa8f07dbb31
Grebennikov Roman <grebennikov.roman@gmail.com>
2017-06-19 11:49:55 +0300

    S3: formatting fix

--- s3/src/test/scala/akka/stream/alpakka/s3/scaladsl/S3ClientSpec.scala
+++ s3/src/test/scala/akka/stream/alpakka/s3/scaladsl/S3ClientSpec.scala
@@ -1,3 +1,6 @@
+/*
+ * Copyright (C) 2016-2017 Lightbend Inc. <http://www.lightbend.com>
+ */
 package akka.stream.alpakka.s3.scaladsl
 
 import akka.actor.ActorSystem
@@ -9,12 +12,14 @@ import scala.collection.JavaConverters._
 
 class S3ClientSpec extends FlatSpecLike with Matchers {
   it should "reuse application config from actor system" in {
-    val config = ConfigFactory.parseMap(Map(
-      "akka.stream.alpakka.s3.proxy.host" -> "localhost",
-      "akka.stream.alpakka.s3.proxy.port" -> 8001,
-      "akka.stream.alpakka.s3.proxy.secure" -> false,
-      "akka.stream.alpakka.s3.path-style-access" -> true
-    ).asJava)
+    val config = ConfigFactory.parseMap(
+      Map(
+        "akka.stream.alpakka.s3.proxy.host" -> "localhost",
+        "akka.stream.alpakka.s3.proxy.port" -> 8001,
+        "akka.stream.alpakka.s3.proxy.secure" -> false,
+        "akka.stream.alpakka.s3.path-style-access" -> true
+      ).asJava
+    )
     implicit val system = ActorSystem.create("s3", config)
     implicit val materializer = ActorMaterializer()
     val client = S3Client()
