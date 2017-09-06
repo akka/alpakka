abf6062d263fa94cbf5696945236fa0b65f3f934
Cedric CORBIERE <corbic@gmail.com>
2017-09-06 23:26:19 +0200

    XML Writer #498
    Implementation of the serialization of the ParseEvent from xml-parser

--- xml/src/main/scala/akka/stream/alpakka/xml/Xml.scala
+++ xml/src/main/scala/akka/stream/alpakka/xml/Xml.scala
@@ -4,11 +4,13 @@
 
 package akka.stream.alpakka.xml
 
+import java.nio.charset.Charset
 import java.util.Optional
+import javax.xml.stream.XMLOutputFactory
 
 import akka.stream.stage.{GraphStage, GraphStageLogic, InHandler, OutHandler}
 import akka.stream.{Attributes, FlowShape, Inlet, Outlet}
-import akka.util.ByteString
+import akka.util.{ByteString, ByteStringBuilder}
 import com.fasterxml.aalto.stax.InputFactoryImpl
 import com.fasterxml.aalto.{AsyncByteArrayFeeder, AsyncXMLInputFactory, AsyncXMLStreamReader}
 
@@ -173,6 +175,65 @@ object Xml {
                 else completeStage()
             }
           } else completeStage()
+      }
+  }
+
+  /**
+   * Internal API
+   */
+  private[xml] class StreamingXmlWriter(charset: Charset) extends GraphStage[FlowShape[ParseEvent, ByteString]] {
+    val in: Inlet[ParseEvent] = Inlet("XMLWriter.in")
+    val out: Outlet[ByteString] = Outlet("XMLWriter.out")
+    override val shape: FlowShape[ParseEvent, ByteString] = FlowShape(in, out)
+
+    private val xMLOutputFactory = XMLOutputFactory.newInstance()
+
+    override def createLogic(inheritedAttributes: Attributes): GraphStageLogic =
+      new GraphStageLogic(shape) with InHandler with OutHandler {
+        val byteStringBuilder = new ByteStringBuilder()
+
+        val output = xMLOutputFactory.createXMLStreamWriter(byteStringBuilder.asOutputStream, charset.name())
+
+        setHandlers(in, out, this)
+
+        override def onPush(): Unit = {
+          val ev: ParseEvent = grab(in)
+          ev match {
+            case StartDocument =>
+              output.writeStartDocument()
+
+            case EndDocument =>
+              output.writeEndDocument()
+
+            case StartElement(localName, attributes) =>
+              output.writeStartElement(localName)
+              attributes.foreach(t => output.writeAttribute(t._1, t._2))
+
+            case EndElement(_) =>
+              output.writeEndElement()
+
+            case Characters(text) =>
+              output.writeCharacters(text)
+            case ProcessingInstruction(Some(target), Some(data)) =>
+              output.writeProcessingInstruction(target, data)
+
+            case ProcessingInstruction(Some(target), None) =>
+              output.writeProcessingInstruction(target)
+
+            case ProcessingInstruction(None, Some(data)) =>
+              output.writeProcessingInstruction(None.orNull, data)
+            case ProcessingInstruction(None, None) =>
+            case Comment(text) =>
+              output.writeComment(text)
+
+            case CData(text) =>
+              output.writeCData(text)
+          }
+          push(out, byteStringBuilder.result().compact)
+          byteStringBuilder.clear()
+        }
+
+        override def onPull(): Unit = pull(in)
       }
   }
 
