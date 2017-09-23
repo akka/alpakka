0091fdfdde16ceab3eb6290c444efd466f1d4eeb
miti <pricope@textkernel.nl>
2017-09-23 22:07:48 +0200

    fixing code formating

--- eip/src/test/java/akka/stream/alpakka/eip/javadsl/SplitterExamples.java
+++ eip/src/test/java/akka/stream/alpakka/eip/javadsl/SplitterExamples.java
@@ -1,3 +1,6 @@
+/*
+ * Copyright (C) 2016-2017 Lightbend Inc. <http://www.lightbend.com>
+ */
 package akka.stream.alpakka.eip.javadsl;
 
 import akka.NotUsed;
--- eip/src/test/scala/akka/stream/alpakka/eip/scaladsl/SplitterExamples.scala
+++ eip/src/test/scala/akka/stream/alpakka/eip/scaladsl/SplitterExamples.scala
@@ -1,3 +1,6 @@
+/*
+ * Copyright (C) 2016-2017 Lightbend Inc. <http://www.lightbend.com>
+ */
 package akka.stream.alpakka.eip.scaladsl
 
 import akka.NotUsed
