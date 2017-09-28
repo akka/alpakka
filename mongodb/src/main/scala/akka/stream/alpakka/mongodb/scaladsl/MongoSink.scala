f42e3957874cc5949c516b3dd3e1f697a9937edc
Stephen Nancekivell <stephen.nancekivell@gmail.com>
2017-09-28 22:20:42 +1000

    add mongo update and delete operations

--- mongodb/src/main/scala/akka/stream/alpakka/mongodb/scaladsl/MongoSink.scala
+++ mongodb/src/main/scala/akka/stream/alpakka/mongodb/scaladsl/MongoSink.scala
@@ -7,13 +7,63 @@ import akka.Done
 import akka.stream.scaladsl.{Flow, Keep, Sink}
 
 import scala.concurrent.Future
-
 import org.mongodb.scala.Document
 import org.mongodb.scala.MongoCollection
+import org.mongodb.scala.bson.conversions.Bson
+import org.mongodb.scala.model.UpdateOptions
 
 object MongoSink {
-  def apply(parallelism: Int, collection: MongoCollection[Document]): Sink[Document, Future[Done]] =
+  def insertOne(parallelism: Int, collection: MongoCollection[Document]): Sink[Document, Future[Done]] =
     Flow[Document]
       .mapAsyncUnordered(parallelism)(doc => collection.insertOne(doc).toFuture())
       .toMat(Sink.ignore)(Keep.right)
+
+  def insertMany(parallelism: Int, collection: MongoCollection[Document]): Sink[Seq[Document], Future[Done]] =
+    Flow[Seq[Document]]
+      .mapAsyncUnordered(parallelism)(docs => collection.insertMany(docs).toFuture())
+      .toMat(Sink.ignore)(Keep.right)
+
+  def updateOne(parallelism: Int,
+                collection: MongoCollection[Document],
+                maybeUpdateOptions: Option[UpdateOptions] = None): Sink[DocumentUpdate, Future[Done]] =
+    Flow[DocumentUpdate]
+      .mapAsyncUnordered(parallelism)(
+        documentUpdate => {
+          maybeUpdateOptions match {
+            case None =>
+              collection.updateOne(documentUpdate.filter, documentUpdate.update)
+            case Some(options) =>
+              collection.updateOne(documentUpdate.filter, documentUpdate.update, options)
+          }
+        }.toFuture()
+      )
+      .toMat(Sink.ignore)(Keep.right)
+
+  def updateMany(parallelism: Int,
+                 collection: MongoCollection[Document],
+                 maybeUpdateOptions: Option[UpdateOptions] = None): Sink[DocumentUpdate, Future[Done]] =
+    Flow[DocumentUpdate]
+      .mapAsyncUnordered(parallelism)(
+        documentUpdate => {
+          maybeUpdateOptions match {
+            case None =>
+              collection.updateMany(documentUpdate.filter, documentUpdate.update)
+            case Some(options) =>
+              collection.updateMany(documentUpdate.filter, documentUpdate.update, options)
+          }
+        }.toFuture()
+      )
+      .toMat(Sink.ignore)(Keep.right)
+
+  def deleteOne(parallelism: Int, collection: MongoCollection[Document]): Sink[Bson, Future[Done]] =
+    Flow[Bson]
+      .mapAsyncUnordered(parallelism)(filter => collection.deleteOne(filter).toFuture())
+      .toMat(Sink.ignore)(Keep.right)
+
+  def deleteMany(parallelism: Int, collection: MongoCollection[Document]): Sink[Bson, Future[Done]] =
+    Flow[Bson]
+      .mapAsyncUnordered(parallelism)(filter => collection.deleteMany(filter).toFuture())
+      .toMat(Sink.ignore)(Keep.right)
+
+  final case class DocumentUpdate(filter: Bson, update: Bson)
 }
--- mongodb/src/test/scala/akka/stream/alpakka/mongodb/MongoSinkSpec.scala
+++ mongodb/src/test/scala/akka/stream/alpakka/mongodb/MongoSinkSpec.scala
@@ -7,8 +7,11 @@ import akka.NotUsed
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
 import akka.stream.alpakka.mongodb.scaladsl.MongoSink
+import akka.stream.alpakka.mongodb.scaladsl.MongoSink.DocumentUpdate
 import akka.stream.scaladsl.Source
 import org.mongodb.scala.MongoClient
+import org.mongodb.scala.model.Filters
+import org.mongodb.scala.model.Updates._
 import org.mongodb.scala.bson.collection.immutable.Document
 import org.scalatest._
 import org.scalatest.concurrent.ScalaFutures
@@ -42,27 +45,94 @@ class MongoSinkSpec
   override def afterAll(): Unit =
     Await.result(system.terminate(), 5.seconds)
 
+  val testRange = 0 until 10
+
+  def insertTestRange(): Unit =
+    numbersColl.insertMany(testRange.map(i => Document(s"""{"value":$i}"""))).toFuture().futureValue
+
   "MongoSinkSpec" must {
 
-    "stream the data into Mongo" in {
-      val testRange = 0 until 10
-
+    "save with insertOne" in {
       val source = Source(testRange).map(i => Document(s"""{"value":$i}"""))
 
-      val result = source.runWith(MongoSink(2, numbersColl))
-
-      result.futureValue
+      source.runWith(MongoSink.insertOne(2, numbersColl)).futureValue
 
       val found = numbersColl.find().toFuture().futureValue
 
       found.map(_.getInteger("value")) must contain theSameElementsAs testRange
+    }
+
+    "save with insertMany" in {
+      val source = Source(testRange).map(i => Document(s"""{"value":$i}"""))
+
+      source.grouped(2).runWith(MongoSink.insertMany(2, numbersColl)).futureValue
+
+      val found = numbersColl.find().toFuture().futureValue
+
+      found.map(_.getInteger("value")) must contain theSameElementsAs testRange
+    }
+
+    "update with updateOne" in {
+      insertTestRange()
+
+      val source = Source(testRange).map(
+        i => DocumentUpdate(filter = Filters.equal("value", i), update = set("updateValue", i * -1))
+      )
+
+      source.runWith(MongoSink.updateOne(2, numbersColl)).futureValue
+
+      val found = numbersColl.find().toFuture().futureValue
+
+      found.map(doc => doc.getInteger("value") -> doc.getInteger("updateValue")) must contain theSameElementsAs testRange
+        .map(i => i -> i * -1)
+    }
+
+    "update with updateMany" in {
+      insertTestRange()
+
+      val source = Source
+        .single(0)
+        .map(
+          _ => DocumentUpdate(filter = Filters.gte("value", 0), update = set("updateValue", 0))
+        )
+
+      source.runWith(MongoSink.updateMany(2, numbersColl)).futureValue
+
+      val found = numbersColl.find().toFuture().futureValue
+
+      found.map(doc => doc.getInteger("value") -> doc.getInteger("updateValue")) must contain theSameElementsAs testRange
+        .map(i => i -> 0)
+    }
+
+    "delete with deleteOne" in {
+      insertTestRange()
+
+      val source = Source(testRange).map(i => Filters.equal("value", i))
+
+      source.runWith(MongoSink.deleteOne(2, numbersColl)).futureValue
+
+      val found = numbersColl.find().toFuture().futureValue
+
+      found mustBe empty
+    }
+
+    "delete with deleteMany" in {
+      insertTestRange()
+
+      val source = Source.single(0).map(_ => Filters.gte("value", 0))
+
+      source.runWith(MongoSink.deleteMany(2, numbersColl)).futureValue
+
+      val found = numbersColl.find().toFuture().futureValue
+
+      found mustBe empty
     }
   }
 
   private class ParadoxSnippet() {
     val source: Source[Document, NotUsed] = ???
     //#create-sink
-    source.runWith(MongoSink(parallelism = 2, collection = numbersColl))
+    source.runWith(MongoSink.insertOne(parallelism = 2, collection = numbersColl))
     //#create-sink
   }
 }
