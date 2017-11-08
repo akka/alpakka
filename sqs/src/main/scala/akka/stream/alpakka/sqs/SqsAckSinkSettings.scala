e81874dd873494ed6c776a5cd60f5676d19a80fc
Lomig Mégard <lomig.megard@gmail.com>
2017-11-08 21:25:22 +0100

    More idiomatic SQS MessageAction (#574)

--- sqs/src/main/scala/akka/stream/alpakka/sqs/SqsAckSinkSettings.scala
+++ sqs/src/main/scala/akka/stream/alpakka/sqs/SqsAckSinkSettings.scala
@@ -14,10 +14,67 @@ final case class SqsAckSinkSettings(maxInFlight: Int) {
 }
 //#SqsAckSinkSettings
 
-sealed trait MessageAction
-final case class Delete() extends MessageAction
-final case class Ignore() extends MessageAction
-final case class ChangeMessageVisibility(visibilityTimeout: Int) extends MessageAction {
-  // SQS requirements
-  require(0 <= visibilityTimeout && visibilityTimeout <= 43200)
+sealed abstract class MessageAction
+
+object Delete {
+  @deprecated("Use `MessageAction.Delete` instead", "0.15")
+  def apply(): MessageAction = MessageAction.Delete
+}
+
+object Ignore {
+  @deprecated("Use `MessageAction.Ignore` instead", "0.15")
+  def apply(): MessageAction = MessageAction.Ignore
+}
+
+object ChangeMessageVisibility {
+  @deprecated("Use `MessageAction.ChangeMessageVisibility` instead", "0.15")
+  def apply(visibilityTimeout: Int): MessageAction = MessageAction.ChangeMessageVisibility(visibilityTimeout)
+}
+
+object MessageAction {
+
+  /**
+   * Delete the message from the queue.
+   *
+   * @see [https://docs.aws.amazon.com/AWSSimpleQueueService/latest/APIReference/API_DeleteMessage.html DeleteMessage]
+   */
+  final case object Delete extends MessageAction
+
+  /**
+   * Ignore the message.
+   */
+  final case object Ignore extends MessageAction
+
+  /**
+   * Change the visibility timeout of the message.
+   * The maximum allowed timeout value is 12 hours.
+   *
+   * @param visibilityTimeout new timeout in seconds
+   *
+   * @see [https://docs.aws.amazon.com/AWSSimpleQueueService/latest/APIReference/API_ChangeMessageVisibility.html ChangeMessageVisibility]
+   */
+  final case class ChangeMessageVisibility(visibilityTimeout: Int) extends MessageAction {
+    // SQS requirements
+    require(
+      0 <= visibilityTimeout && visibilityTimeout <= 43200,
+      s"Invalid value ($visibilityTimeout) for visibilityTimeout. Requirement: 0 <= waitTimeSeconds <= 43200"
+    )
+  }
+
+  /**
+   * Java API: Delete the message from the queue.
+   */
+  def delete: MessageAction = Delete
+
+  /**
+   * Java API: Ignore the message.
+   */
+  def ignore: MessageAction = Ignore
+
+  /**
+   * Java API: Change the visibility timeout of the message.
+   * @param visibilityTimeout new timeout in seconds
+   */
+  def changeMessageVisibility(visibilityTimeout: Int): MessageAction = ChangeMessageVisibility(visibilityTimeout)
+
 }
