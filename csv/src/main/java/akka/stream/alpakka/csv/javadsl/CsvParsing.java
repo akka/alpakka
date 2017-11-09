bdb5fc4f7127425a955610407e17a40de2c707a2
Enno <enno@runne.net>
2017-11-09 10:50:07 +0100

    CSV: Fail parsing for too long lines #509
    Introduce `maximumLineLength` in CSV parsing so that the parser fails when
    no line break is encountered for a reaonably long line (defaults to 10 kB).
    
    Fixes #509
    
    * CSV: Fail parsing for too long lines
    
    Adapt to deprecation in 2.12 in a backwards compatible way.

--- csv/src/main/java/akka/stream/alpakka/csv/javadsl/CsvParsing.java
+++ csv/src/main/java/akka/stream/alpakka/csv/javadsl/CsvParsing.java
@@ -7,7 +7,7 @@ package akka.stream.alpakka.csv.javadsl;
 import akka.NotUsed;
 import akka.stream.javadsl.Flow;
 import akka.util.ByteString;
-import scala.collection.JavaConversions;
+import scala.collection.JavaConverters;
 
 import java.util.Collection;
 
@@ -19,14 +19,19 @@ public class CsvParsing {
     public static final byte COLON = ':';
     public static final byte TAB = '\t';
     public static final byte DOUBLE_QUOTE = '"';
+    public static final int MAXIMUM_LINE_LENGTH_DEFAULT = 10 * 1024;
 
     public static Flow<ByteString, Collection<ByteString>, NotUsed> lineScanner() {
-        return lineScanner(COMMA, DOUBLE_QUOTE, BACKSLASH);
+        return lineScanner(COMMA, DOUBLE_QUOTE, BACKSLASH, MAXIMUM_LINE_LENGTH_DEFAULT);
     }
 
     public static Flow<ByteString, Collection<ByteString>, NotUsed> lineScanner(byte delimiter, byte quoteChar, byte escapeChar) {
+        return lineScanner(delimiter, quoteChar, escapeChar, MAXIMUM_LINE_LENGTH_DEFAULT);
+    }
+
+    public static Flow<ByteString, Collection<ByteString>, NotUsed> lineScanner(byte delimiter, byte quoteChar, byte escapeChar, int maximumLineLength) {
         return akka.stream.alpakka.csv.scaladsl.CsvParsing
-                .lineScanner(delimiter, quoteChar, escapeChar).asJava()
-                .map(JavaConversions::asJavaCollection);
+                .lineScanner(delimiter, quoteChar, escapeChar, maximumLineLength).asJava()
+                .map(c -> JavaConverters.asJavaCollectionConverter(c).asJavaCollection());
     }
 }
