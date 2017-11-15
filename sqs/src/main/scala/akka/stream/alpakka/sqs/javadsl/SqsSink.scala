7ff3d933b4bf2b6193be6837e5438641864b7b5a
Lomig Mégard <lomig.megard@gmail.com>
2017-11-15 20:21:27 +0100

    Bump aws dependencies (#573)

--- sqs/src/main/scala/akka/stream/alpakka/sqs/javadsl/SqsSink.scala
+++ sqs/src/main/scala/akka/stream/alpakka/sqs/javadsl/SqsSink.scala
@@ -6,7 +6,7 @@ package akka.stream.alpakka.sqs.javadsl
 
 import java.util.concurrent.CompletionStage
 import akka.Done
-import akka.stream.alpakka.sqs.{SqsFlowStage, SqsSinkSettings, _}
+import akka.stream.alpakka.sqs._
 import akka.stream.javadsl.Sink
 import com.amazonaws.services.sqs.AmazonSQSAsync
 import scala.compat.java8.FutureConverters.FutureOps
