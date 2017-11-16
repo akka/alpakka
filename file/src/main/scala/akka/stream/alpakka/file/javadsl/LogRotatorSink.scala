c74823ebb9150cb866114b9bd44e2f72a03c8009
Enno <enno@runne.net>
2017-11-16 09:51:56 +0100

    File rotator sink: examples & Optional in Java API (#589)
    * Reviewed the file rotator sink examples
    
    * Change Java API to use Optional instead of null

--- file/src/main/scala/akka/stream/alpakka/file/javadsl/LogRotatorSink.scala
+++ file/src/main/scala/akka/stream/alpakka/file/javadsl/LogRotatorSink.scala
@@ -6,6 +6,7 @@ package akka.stream.alpakka.file.javadsl
 
 import java.nio.file.{Path, StandardOpenOption}
 import java.util
+import java.util.Optional
 import java.util.concurrent.CompletionStage
 
 import akka.Done
@@ -18,20 +19,20 @@ import scala.collection.JavaConverters._
 
 object LogRotatorSink {
   def createFromFunction(
-      f: function.Creator[function.Function[ByteString, Path]]
+      f: function.Creator[function.Function[ByteString, Optional[Path]]]
   ): javadsl.Sink[ByteString, CompletionStage[Done]] =
     new Sink(
       akka.stream.alpakka.file.scaladsl
         .LogRotatorSink({ () =>
           val fun = f.create()
           elem =>
-            Option(fun(elem))
+            Option(fun(elem).orElse(null))
         })
         .toCompletionStage()
     )
 
   def createFromFunctionAndOptions[T](
-      f: function.Creator[function.Function[ByteString, Path]],
+      f: function.Creator[function.Function[ByteString, Optional[Path]]],
       options: util.Set[StandardOpenOption]
   ): javadsl.Sink[ByteString, CompletionStage[Done]] =
     new Sink(
@@ -39,7 +40,7 @@ object LogRotatorSink {
         .LogRotatorSink({ () =>
           val fun = f.create()
           elem =>
-            Option(fun(elem))
+            Option(fun(elem).orElse(null))
         }, options.asScala.toSet)
         .toCompletionStage()
     )
