9a93294b412a21efe96666aaa56ba5484f9cc317
d2e8f9 <d2e8f9@posteo.de>
2017-11-23 02:29:29 +0100

    Reformatted Code

--- jms/src/main/scala/akka/stream/alpakka/jms/Jms.scala
+++ jms/src/main/scala/akka/stream/alpakka/jms/Jms.scala
@@ -1,6 +1,7 @@
 /*
  * Copyright (C) 2016-2017 Lightbend Inc. <http://www.lightbend.com>
  */
+
 package akka.stream.alpakka.jms
 
 import java.time.Duration
@@ -8,12 +9,16 @@ import javax.jms.ConnectionFactory
 
 sealed trait JmsSettings {
   def connectionFactory: ConnectionFactory
+
   def destination: Option[Destination]
+
   def credentials: Option[Credentials]
 }
 
 sealed trait Destination
+
 final case class Topic(name: String) extends Destination
+
 final case class Queue(name: String) extends Destination
 
 object JmsSourceSettings {
@@ -29,9 +34,13 @@ final case class JmsSourceSettings(connectionFactory: ConnectionFactory,
                                    selector: Option[String] = None)
     extends JmsSettings {
   def withCredential(credentials: Credentials) = copy(credentials = Some(credentials))
+
   def withBufferSize(size: Int) = copy(bufferSize = size)
+
   def withQueue(name: String) = copy(destination = Some(Queue(name)))
+
   def withTopic(name: String) = copy(destination = Some(Topic(name)))
+
   def withSelector(selector: String) = copy(selector = Some(selector))
 }
 
@@ -47,8 +56,11 @@ final case class JmsSinkSettings(connectionFactory: ConnectionFactory,
                                  timeToLive: Option[Duration] = None)
     extends JmsSettings {
   def withCredential(credentials: Credentials) = copy(credentials = Some(credentials))
+
   def withQueue(name: String) = copy(destination = Some(Queue(name)))
+
   def withTopic(name: String) = copy(destination = Some(Topic(name)))
+
   def withTimeToLive(ttl: Duration) = copy(timeToLive = Some(ttl))
 }
 
