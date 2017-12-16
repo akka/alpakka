07579504b6561f75c489637032642516b106c34c
Juanjo Diaz <juanjo.diaz@intopalo.com>
2017-12-16 21:48:45 +0200

    Update AMQP client version to the latest

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -25,7 +25,7 @@ object Dependencies {
 
   val Amqp = Seq(
     libraryDependencies ++= Seq(
-      "com.rabbitmq" % "amqp-client" % "3.6.1" // APLv2
+      "com.rabbitmq" % "amqp-client" % "5.0.0" // APLv2
     )
   )
 
