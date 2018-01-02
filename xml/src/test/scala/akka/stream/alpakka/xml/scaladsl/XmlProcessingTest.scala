5805c06d6871e27e0e9535b4dd06d24218445002
Korbik <corbic@gmail.com>
2018-01-02 21:15:56 +0100

    XML: Add the possibility to handle namespace (#647)
    Applies for XML parser and writer.

--- xml/src/test/scala/akka/stream/alpakka/xml/scaladsl/XmlProcessingTest.scala
+++ xml/src/test/scala/akka/stream/alpakka/xml/scaladsl/XmlProcessingTest.scala
@@ -38,11 +38,11 @@ class XmlProcessingTest extends WordSpec with Matchers with BeforeAndAfterAll {
       result should ===(
         List(
           StartDocument,
-          StartElement("doc", Map.empty),
-          StartElement("elem", Map.empty),
+          StartElement("doc"),
+          StartElement("elem"),
           Characters("elem1"),
           EndElement("elem"),
-          StartElement("elem", Map.empty),
+          StartElement("elem"),
           Characters("elem2"),
           EndElement("elem"),
           EndElement("doc"),
@@ -60,7 +60,7 @@ class XmlProcessingTest extends WordSpec with Matchers with BeforeAndAfterAll {
       result should ===(
         List(
           StartDocument,
-          StartElement("doc", Map.empty),
+          StartElement("doc"),
           Comment("comment"),
           EndElement("doc"),
           EndDocument
@@ -78,7 +78,7 @@ class XmlProcessingTest extends WordSpec with Matchers with BeforeAndAfterAll {
         List(
           StartDocument,
           ProcessingInstruction(Some("target"), Some("content")),
-          StartElement("doc", Map.empty),
+          StartElement("doc"),
           EndElement("doc"),
           EndDocument
         )
@@ -105,6 +105,109 @@ class XmlProcessingTest extends WordSpec with Matchers with BeforeAndAfterAll {
       )
     }
 
+    "properly process default namespace" in {
+      val doc = """<doc xmlns="test:xml:0.1"><elem>elem1</elem><elem>elem2</elem></doc>"""
+
+      val resultFuture = Source.single(doc).runWith(parse)
+
+      val result = Await.result(resultFuture, 3.seconds)
+      result should ===(
+        List(
+          StartDocument,
+          StartElement("doc",
+                       namespace = Some("test:xml:0.1"),
+                       prefix = None,
+                       namespaceCtx = List(Namespace("test:xml:0.1"))),
+          StartElement("elem", namespace = Some("test:xml:0.1")),
+          Characters("elem1"),
+          EndElement("elem"),
+          StartElement("elem", namespace = Some("test:xml:0.1")),
+          Characters("elem2"),
+          EndElement("elem"),
+          EndElement("doc"),
+          EndDocument
+        )
+      )
+    }
+
+    "properly process prefixed  namespace" in {
+      val doc = """<x xmlns:edi="http://ecommerce.example.org/schema"></x>"""
+
+      val resultFuture = Source.single(doc).runWith(parse)
+
+      val result = Await.result(resultFuture, 3.seconds)
+      result should ===(
+        List(
+          StartDocument,
+          StartElement("x",
+                       namespace = None,
+                       prefix = None,
+                       namespaceCtx = List(Namespace("http://ecommerce.example.org/schema", prefix = Some("edi")))),
+          EndElement("x"),
+          EndDocument
+        )
+      )
+    }
+    "properly process multiple namespaces" in {
+      val doc =
+        """<?xml version="1.0"?><bk:book xmlns:bk='urn:loc.gov:books' xmlns:isbn='urn:ISBN:0-395-36341-6'><bk:title>Cheaper by the Dozen</bk:title><isbn:number>1568491379</isbn:number></bk:book>"""
+
+      val resultFuture = Source.single(doc).runWith(parse)
+
+      val result = Await.result(resultFuture, 3.seconds)
+      result should ===(
+        List(
+          StartDocument,
+          StartElement(
+            "book",
+            namespace = Some("urn:loc.gov:books"),
+            prefix = Some("bk"),
+            namespaceCtx = List(Namespace("urn:loc.gov:books", prefix = Some("bk")),
+                                Namespace("urn:ISBN:0-395-36341-6", prefix = Some("isbn")))
+          ),
+          StartElement(
+            "title",
+            namespace = Some("urn:loc.gov:books"),
+            prefix = Some("bk")
+          ),
+          Characters("Cheaper by the Dozen"),
+          EndElement("title"),
+          StartElement(
+            "number",
+            namespace = Some("urn:ISBN:0-395-36341-6"),
+            prefix = Some("isbn")
+          ),
+          Characters("1568491379"),
+          EndElement("number"),
+          EndElement("book"),
+          EndDocument
+        )
+      )
+    }
+
+    "properly process attributes with prefix and namespace" in {
+      val doc =
+        """<x xmlns:edi='http://ecommerce.example.org/schema'><lineItem edi:taxClass="exempt">Baby food</lineItem></x>"""
+      val resultFuture = Source.single(doc).runWith(parse)
+
+      val result = Await.result(resultFuture, 3.seconds)
+      result should ===(
+        List(
+          StartDocument,
+          StartElement("x",
+                       namespaceCtx = List(Namespace("http://ecommerce.example.org/schema", prefix = Some("edi")))),
+          StartElement(
+            "lineItem",
+            List(Attribute("taxClass", "exempt", Some("edi"), Some("http://ecommerce.example.org/schema")))
+          ),
+          Characters("Baby food"),
+          EndElement("lineItem"),
+          EndElement("x"),
+          EndDocument
+        )
+      )
+    }
+
     "properly process CData blocks" in {
       val doc = """<doc><![CDATA[<not>even</valid>]]></doc>"""
 
@@ -114,7 +217,7 @@ class XmlProcessingTest extends WordSpec with Matchers with BeforeAndAfterAll {
       result should ===(
         List(
           StartDocument,
-          StartElement("doc", Map.empty),
+          StartElement("doc"),
           CData("<not>even</valid>"),
           EndElement("doc"),
           EndDocument
@@ -141,7 +244,7 @@ class XmlProcessingTest extends WordSpec with Matchers with BeforeAndAfterAll {
           case _ => true
         }
         .splitWhen(_ match {
-          case StartElement("elem", _) => true
+          case StartElement("elem", _, _, _, _) => true
           case _ => false
         })
         .collect({
@@ -153,7 +256,6 @@ class XmlProcessingTest extends WordSpec with Matchers with BeforeAndAfterAll {
       val result = Await.result(resultFuture, 3.seconds)
       result should ===(elements)
     }
-
   }
 
   override protected def afterAll(): Unit = system.terminate()
