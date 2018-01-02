b2266c38a9793b9e0583afaafff6df89d3eec64c
Mehmet Cem Gunturkun <mehmetcemgunturkun@gmail.com>
2018-01-02 23:13:00 +0300

    Sqs send message in batch mode (#387)
    * Implemented grouped and batched SqsFlows for sending messages in batch fashion
    * Changed the grouping logic in order not to wait for a batch to be completed and added SqsFlowSetting with parameters: maxBatchSize, maxBatchWait and concurrentRequests
    * Refactored SqsFlowSetting to SqsBatchFlowSetting. Added missing javadsl and scaladsl functions for grouped and batch.
    * Added a note for the users to indicate that there is another option for buffering.

--- sqs/src/main/scala/akka/stream/alpakka/sqs/javadsl/SqsFlow.scala
+++ sqs/src/main/scala/akka/stream/alpakka/sqs/javadsl/SqsFlow.scala
@@ -8,7 +8,10 @@ import akka.NotUsed
 import akka.stream.alpakka.sqs.scaladsl.Result
 import akka.stream.alpakka.sqs._
 import akka.stream.javadsl.Flow
+import akka.stream.scaladsl.{Flow => SFlow}
 import com.amazonaws.services.sqs.AmazonSQSAsync
+import java.lang.{Iterable => JIterable}
+import scala.collection.JavaConverters._
 
 object SqsFlow {
 
@@ -19,8 +22,40 @@ object SqsFlow {
     scaladsl.SqsFlow.apply(queueUrl, settings)(sqsClient).asJava
 
   /**
+   * Java API: creates a flow based on [[SqsBatchFlowStage]] for a SQS queue using an [[AmazonSQSAsync]]
+   */
+  def grouped(queueUrl: String,
+              settings: SqsBatchFlowSettings,
+              sqsClient: AmazonSQSAsync): Flow[String, Result, NotUsed] =
+    scaladsl.SqsFlow.grouped(queueUrl, settings)(sqsClient).asJava
+
+  /**
+   * Java API: creates a flow based on [[SqsBatchFlowStage]] for a SQS queue using an [[AmazonSQSAsync]]
+   */
+  def batch(queueUrl: String,
+            settings: SqsBatchFlowSettings,
+            sqsClient: AmazonSQSAsync): Flow[Seq[String], Seq[Result], NotUsed] =
+    scaladsl.SqsFlow.batch(queueUrl, settings)(sqsClient).asJava
+
+  /**
    * Java API: creates a flow based on [[SqsFlowStage]] for a SQS queue using an [[AmazonSQSAsync]] with default settings.
    */
   def create(queueUrl: String, sqsClient: AmazonSQSAsync): Flow[String, Result, NotUsed] =
     create(queueUrl, SqsSinkSettings.Defaults, sqsClient)
+
+  /**
+   * Java API: creates a flow based on [[SqsBatchFlowStage]] for a SQS queue using an [[AmazonSQSAsync]] with default settings
+   */
+  def grouped(queueUrl: String, sqsClient: AmazonSQSAsync): Flow[String, Result, NotUsed] =
+    grouped(queueUrl, SqsBatchFlowSettings.Defaults, sqsClient)
+
+  /**
+   * Java API: creates a flow based on [[SqsBatchFlowStage]] for a SQS queue using an [[AmazonSQSAsync]] with default settings
+   */
+  def batch(queueUrl: String, sqsClient: AmazonSQSAsync): Flow[JIterable[String], JIterable[Result], NotUsed] =
+    SFlow[JIterable[String]]
+      .map(jIterable => jIterable.asScala)
+      .via(scaladsl.SqsFlow.batch(queueUrl, SqsBatchFlowSettings.Defaults)(sqsClient))
+      .map(sIterable => sIterable.asJava)
+      .asJava
 }
