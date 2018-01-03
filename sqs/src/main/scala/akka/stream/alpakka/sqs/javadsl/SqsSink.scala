6e59ace4d298e88d1237a8b57f6e3eeeeb4d4d23
Enno <enno@runne.net>
2018-01-03 22:35:19 +0100

    Fixed Scaladoc for SQS module

--- sqs/src/main/scala/akka/stream/alpakka/sqs/javadsl/SqsSink.scala
+++ sqs/src/main/scala/akka/stream/alpakka/sqs/javadsl/SqsSink.scala
@@ -18,10 +18,13 @@ import com.amazonaws.services.sqs.model.SendMessageRequest
 import scala.collection.JavaConverters._
 import scala.compat.java8.FutureConverters.FutureOps
 
+/**
+ * Java API to create SQS Sinks.
+ */
 object SqsSink {
 
   /**
-   * Java API: creates a sink based on [[SqsFlowStage]] for a SQS queue using an [[AmazonSQSAsync]]
+   * Creates a sink for a SQS queue using an [[com.amazonaws.services.sqs.AmazonSQSAsync]].
    */
   def create(queueUrl: String,
              settings: SqsSinkSettings,
@@ -29,7 +32,7 @@ object SqsSink {
     scaladsl.SqsSink.apply(queueUrl, settings)(sqsClient).mapMaterializedValue(_.toJava).asJava
 
   /**
-   * Java API: creates a sink based on [[SqsBatchFlowStage]] running in batch mode for a SQS queue using an [[AmazonSQSAsync]]
+   * Creates a grouped sink running in batch mode for a SQS queue using an [[com.amazonaws.services.sqs.AmazonSQSAsync]].
    */
   def grouped(queueUrl: String,
               settings: SqsBatchFlowSettings,
@@ -37,7 +40,7 @@ object SqsSink {
     scaladsl.SqsSink.grouped(queueUrl, settings)(sqsClient).mapMaterializedValue(_.toJava).asJava
 
   /**
-   * Java API: creates a sink based on [[SqsBatchFlowStage]] running in batch mode for a SQS queue using an [[AmazonSQSAsync]]
+   * Creates a sink running in batch mode for a SQS queue using an [[com.amazonaws.services.sqs.AmazonSQSAsync]].
    */
   def batch(queueUrl: String,
             settings: SqsBatchFlowSettings,
@@ -45,19 +48,19 @@ object SqsSink {
     scaladsl.SqsSink.batch(queueUrl, settings)(sqsClient).mapMaterializedValue(_.toJava).asJava
 
   /**
-   * Java API: creates a sink based on [[SqsFlowStage]] for a SQS queue using an [[AmazonSQSAsync]] with default settings.
+   * Creates a sink for a SQS queue using an [[com.amazonaws.services.sqs.AmazonSQSAsync]] with default settings.
    */
   def create(queueUrl: String, sqsClient: AmazonSQSAsync): Sink[String, CompletionStage[Done]] =
     create(queueUrl, SqsSinkSettings.Defaults, sqsClient)
 
   /**
-   * Java API: creates a sink based on [[SqsBatchFlowStage]] running in batch mode for a SQS queue using an [[AmazonSQSAsync]] with default settings.
+   * Creates a sink running in batch mode for a SQS queue using an [[com.amazonaws.services.sqs.AmazonSQSAsync]] with default settings.
    */
   def grouped(queueUrl: String, sqsClient: AmazonSQSAsync): Sink[String, CompletionStage[Done]] =
     scaladsl.SqsSink.grouped(queueUrl, SqsBatchFlowSettings.Defaults)(sqsClient).mapMaterializedValue(_.toJava).asJava
 
   /**
-   * Java API: creates a sink based on [[SqsBatchFlowStage]] running in batch mode for a SQS queue using an [[AmazonSQSAsync]] with default settings.
+   * Creates a sink running in batch mode for a SQS queue using an [[com.amazonaws.services.sqs.AmazonSQSAsync]] with default settings.
    */
   def batch(queueUrl: String, sqsClient: AmazonSQSAsync): Sink[JIterable[String], CompletionStage[Done]] =
     Flow[JIterable[String]]
--- sqs/src/main/scala/akka/stream/alpakka/sqs/javadsl/SqsSource.scala
+++ sqs/src/main/scala/akka/stream/alpakka/sqs/javadsl/SqsSource.scala
@@ -10,16 +10,19 @@ import akka.stream.javadsl.Source
 import com.amazonaws.services.sqs.AmazonSQSAsync
 import com.amazonaws.services.sqs.model.Message
 
+/**
+ * Java API to create SQS sources.
+ */
 object SqsSource {
 
   /**
-   * Java API: creates a [[SqsSourceStage]] for a SQS queue.
+   * Creates a source for a SQS queue.
    */
   def create(queueUrl: String, settings: SqsSourceSettings, sqs: AmazonSQSAsync): Source[Message, NotUsed] =
     Source.fromGraph(new SqsSourceStage(queueUrl, settings)(sqs))
 
   /**
-   * Java API: creates a [[SqsSourceStage]] for a SQS queue with default settings.
+   * Creates a source for a SQS queue with default settings.
    */
   def create(queueUrl: String, sqs: AmazonSQSAsync): Source[Message, NotUsed] =
     Source.fromGraph(new SqsSourceStage(queueUrl, SqsSourceSettings.Defaults)(sqs))
