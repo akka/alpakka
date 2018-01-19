1ce8055687d063418dc48a2df534669e4c18e543
Ferdy Moon Soo Beekmans <ferdy.beekmans@ovoenergy.com>
2018-01-19 17:19:41 +0000

    Javadsl tests

--- cryptography/src/test/java/akka/stream/alpakka/cryptography/javadsl/ExampleTest.java
+++ cryptography/src/test/java/akka/stream/alpakka/cryptography/javadsl/ExampleTest.java
@@ -6,7 +6,6 @@ package akka.stream.alpakka.cryptography.javadsl;
 
 import akka.NotUsed;
 import akka.actor.ActorSystem;
-import akka.japi.Pair;
 import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Sink;
 import akka.stream.javadsl.Source;
@@ -16,8 +15,11 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
 import java.util.stream.Collectors;
 import static org.junit.Assert.*;
@@ -39,21 +41,48 @@ public class ExampleTest {
 
     @Test
     public void symmetricEncryptionDecryptionExampleTest() throws Exception {
+        //#java-symmetric
         KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
         SecretKey randomKey = keyGenerator.generateKey();
 
         List<String> toEncrypt = Arrays.asList("Some", "string", "for", "you");
-        List<ByteString> byteStringsToEncrypt = toEncrypt.stream().map(s -> ByteString$.MODULE$.apply(s)).collect(Collectors.toList());
+        List<ByteString> byteStringsToEncrypt = toEncrypt.stream().map(ByteString$.MODULE$::apply).collect(Collectors.toList());
 
 
         Source<ByteString, NotUsed> sourceOfUnencryptedData = Source.from(byteStringsToEncrypt);
         Source<ByteString, NotUsed> sourceOfEncryptedData = sourceOfUnencryptedData.via(CryptographicFlows.symmetricEncryption(randomKey));
         Source<ByteString, NotUsed> sourceOfDecryptedData = sourceOfEncryptedData.via(CryptographicFlows.symmetricDecryption(randomKey));
-        Sink<ByteString, CompletionStage<ByteString>> concatSink = Sink.<ByteString, ByteString>fold(ByteString.empty(), (acc, nxt) -> acc);
+        Sink<ByteString, CompletionStage<ByteString>> concatSink = Sink.fold(ByteString.empty(), ByteString::concat);
+
+        CompletableFuture<ByteString> resultOfDecryption = sourceOfDecryptedData
+                .runWith(concatSink, materializer).whenComplete((r, error) -> assertEquals(r.utf8String(), "Somestringforyou") )
+                .toCompletableFuture();
+
+        assertEquals(resultOfDecryption.get().utf8String(), "Somestringforyou");
+        //#java-symmetric
+    }
+
+    @Test
+    public void asymmetricEncryptionDecryptionExampleTest() throws Exception {
+        //#java-asymmetric
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+        KeyPair randomKeyPair = keyPairGenerator.generateKeyPair();
+
+        List<String> toEncrypt = Arrays.asList("Some", "string", "for", "you");
+        List<ByteString> byteStringsToEncrypt = toEncrypt.stream().map(ByteString$.MODULE$::apply).collect(Collectors.toList());
 
 
-        CompletionStage<ByteString> resultOfDecryption = sourceOfDecryptedData
-                 .runWith(concatSink, materializer).whenComplete((r, error) -> assertEquals(r.utf8String(), "Somestringforyou") );
+        Source<ByteString, NotUsed> sourceOfUnencryptedData = Source.from(byteStringsToEncrypt);
+        Source<ByteString, NotUsed> sourceOfEncryptedData = sourceOfUnencryptedData.via(CryptographicFlows.asymmetricEncryption(randomKeyPair.getPublic()));
+        Source<ByteString, NotUsed> sourceOfDecryptedData = sourceOfEncryptedData.via(CryptographicFlows.asymmetricDecryption(randomKeyPair.getPrivate()));
+        Sink<ByteString, CompletionStage<ByteString>> concatSink = Sink.fold(ByteString.empty(), ByteString::concat);
 
+
+        CompletableFuture<ByteString> resultOfDecryption = sourceOfDecryptedData
+                .runWith(concatSink, materializer).whenComplete((r, error) -> assertEquals(r.utf8String(), "Somestringforyou") )
+                .toCompletableFuture();
+
+        assertEquals(resultOfDecryption.get().utf8String(), "Somestringforyou");
+        //#java-asymmetric
     }
 }
