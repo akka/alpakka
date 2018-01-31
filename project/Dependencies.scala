6e40f52dbeea51549a636031d9560d67ab29947d
NOUGUIER Olivier <olivier.nouguier@gmail.com>
2018-01-31 10:10:45 +0100

    :arrow_up: geode 1.4.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -105,7 +105,7 @@ object Dependencies {
   )
 
   val Geode = {
-    val geodeVersion = "1.3.0"
+    val geodeVersion = "1.4.0"
     val slf4jVersion = "1.7.25"
     Seq(
       libraryDependencies ++=
