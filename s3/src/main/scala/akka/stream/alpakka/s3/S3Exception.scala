c8656c9c240a4cb6250e4dc55d3bd526c8991ee1
Stefano Bonetti <svez.faz@gmail.com>
2018-02-04 17:41:48 +0000

    S3: add toString to S3Exception (#765)

--- s3/src/main/scala/akka/stream/alpakka/s3/S3Exception.scala
+++ s3/src/main/scala/akka/stream/alpakka/s3/S3Exception.scala
@@ -6,7 +6,7 @@ package akka.stream.alpakka.s3
 
 import scala.xml.{Elem, XML}
 
-class S3Exception(val code: String, val message: String, val requestID: String, val hostId: String)
+class S3Exception(val code: String, val message: String, val requestId: String, val hostId: String)
     extends RuntimeException(message) {
 
   def this(xmlResponse: Elem) =
@@ -16,4 +16,6 @@ class S3Exception(val code: String, val message: String, val requestID: String, 
          (xmlResponse \ "HostID").text)
 
   def this(response: String) = this(XML.loadString(response))
+
+  override def toString = s"${super.toString} (Code: $code, RequestID: $requestId, HostID: $hostId)"
 }
