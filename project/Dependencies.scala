056b7ad4af217c5cf743cde25c144401642c0213
Enno <458526+ennru@users.noreply.github.com>
2018-02-11 22:03:33 +0100

    Upgrade to Scala 2.11.12 and Akka 2.5.9 (#770)
    * Upgrade to Scala 2.11.12 and Akka 2.5.9
    * Static methods in interface require -target:jvm-1.8

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
 
   val AkkaVersion = sys.env.get("AKKA_SERIES") match {
     case Some("2.4") => sys.error("Akka 2.4 is not supported anymore")
-    case _ => "2.5.8"
+    case _ => "2.5.9"
   }
 
   val AwsSdkVersion = "1.11.226"
