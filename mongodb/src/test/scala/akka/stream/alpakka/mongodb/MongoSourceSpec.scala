8a82028913dc3a256df419c37174508d2bebf193
mikel <mikel@poshly.com>
2018-02-22 13:12:12 +0100

    Mongo: Merge codec registry support test case from f6c11c10f282cd8c07303ba2b4bbe47faee9ac1f

--- mongodb/src/test/scala/akka/stream/alpakka/mongodb/MongoSourceSpec.scala
+++ mongodb/src/test/scala/akka/stream/alpakka/mongodb/MongoSourceSpec.scala
@@ -9,7 +9,10 @@ import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
 import akka.stream.alpakka.mongodb.scaladsl.MongoSource
 import akka.stream.scaladsl.{Sink, Source}
+import org.bson.codecs.configuration.CodecRegistries.{fromProviders, fromRegistries}
 import org.mongodb.scala.MongoClient
+import org.mongodb.scala.bson.codecs.DEFAULT_CODEC_REGISTRY
+import org.mongodb.scala.bson.codecs.Macros._
 import org.mongodb.scala.bson.collection.immutable.Document
 import org.scalatest._
 import org.scalatest.concurrent.ScalaFutures
@@ -25,6 +28,11 @@ class MongoSourceSpec
     with BeforeAndAfterAll
     with MustMatchers {
 
+  // case class and codec for mongodb macros
+  case class Number(_id: Int)
+
+  val codecRegistry = fromRegistries(fromProviders(classOf[Number]), DEFAULT_CODEC_REGISTRY)
+
   //#init-mat
   implicit val system = ActorSystem()
   implicit val mat = ActorMaterializer()
@@ -37,7 +45,7 @@ class MongoSourceSpec
 
   //#init-connection
   private val client = MongoClient(s"mongodb://localhost:27017")
-  private val db = client.getDatabase("alpakka-mongo")
+  private val db = client.getDatabase("alpakka-mongo").withCodecRegistry(codecRegistry)
   private val numbersColl = db.getCollection("numbers")
   //#init-connection
 
@@ -75,6 +83,21 @@ class MongoSourceSpec
       //#run-source
 
       rows.futureValue.map(_.getInteger("_id")) must contain theSameElementsAs data
+    }
+
+    "support codec registry to read case class objects" in {
+      val data: Seq[Number] = seed().map(Number)
+
+      //#create-source
+      val source: Source[Number, NotUsed] =
+        MongoSource[Number](numbersColl.find())
+      //#create-source
+
+      //#run-source
+      val rows: Future[Seq[Number]] = source.runWith(Sink.seq)
+      //#run-source
+
+      rows.futureValue must contain theSameElementsAs data
     }
 
     "support multiple materializations" in {
