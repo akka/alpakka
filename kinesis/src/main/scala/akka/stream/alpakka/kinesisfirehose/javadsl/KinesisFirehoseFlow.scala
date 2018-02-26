61584fa7eb91e0c0581babb6a52457e6e4eb209e
Greg Beech <greg@gregbeech.com>
2018-02-26 10:31:35 +0000

    Port Java DSL and tests to Kinesis Firehose

---
+++ kinesis/src/main/scala/akka/stream/alpakka/kinesisfirehose/javadsl/KinesisFirehoseFlow.scala
@@ -0,0 +1,24 @@
+/*
+ * Copyright (C) 2016-2018 Lightbend Inc. <http://www.lightbend.com>
+ */
+
+package akka.stream.alpakka.kinesisfirehose.javadsl
+
+import akka.NotUsed
+import akka.stream.alpakka.kinesisfirehose.{scaladsl, KinesisFirehoseFlowSettings}
+import akka.stream.javadsl.Flow
+import com.amazonaws.services.kinesisfirehose.AmazonKinesisFirehoseAsync
+import com.amazonaws.services.kinesisfirehose.model.{PutRecordBatchResponseEntry, Record}
+
+object KinesisFirehoseFlow {
+
+  def apply(streamName: String,
+            kinesisClient: AmazonKinesisFirehoseAsync): Flow[Record, PutRecordBatchResponseEntry, NotUsed] =
+    apply(streamName, KinesisFirehoseFlowSettings.defaultInstance, kinesisClient)
+
+  def apply(streamName: String,
+            settings: KinesisFirehoseFlowSettings,
+            kinesisClient: AmazonKinesisFirehoseAsync): Flow[Record, PutRecordBatchResponseEntry, NotUsed] =
+    scaladsl.KinesisFirehoseFlow.apply(streamName, settings)(kinesisClient).asJava
+
+}
