a3d56c36115a153e9621a8fc423e76f1c838993a
Tim Sheppard <tim.sheppard@anatwine.com>
2018-04-19 06:46:55 +0100

    Added aws credentials to `DynamoSettings` and create from config if defined

--- dynamodb/src/main/scala/akka/stream/alpakka/dynamodb/impl/DynamoSettings.scala
+++ dynamodb/src/main/scala/akka/stream/alpakka/dynamodb/impl/DynamoSettings.scala
@@ -5,20 +5,40 @@
 package akka.stream.alpakka.dynamodb.impl
 
 import akka.actor.ActorSystem
+import com.amazonaws.auth._
+import com.typesafe.config.Config
+
+import scala.util.Try
 
 object DynamoSettings {
-  def apply(system: ActorSystem): DynamoSettings = {
-    val config = system.settings.config.getConfig("akka.stream.alpakka.dynamodb")
+  def apply(system: ActorSystem): DynamoSettings = apply(system.settings.config)
+
+  def apply(baseConfig: Config): DynamoSettings = {
+    val config = baseConfig.getConfig("akka.stream.alpakka.dynamodb")
+    val awsCredentialsProvider = Try(config.getConfig("credentials"))
+      .map { credentialsConfig =>
+        val accessKey = credentialsConfig.getString("access-key-id")
+        val secretKey = credentialsConfig.getString("secret-key-id")
+        new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKey, secretKey))
+      }
+      .getOrElse(new DefaultAWSCredentialsProviderChain())
+
     DynamoSettings(
-      region = config getString "region",
-      host = config getString "host",
-      port = config getInt "port",
-      parallelism = config getInt "parallelism"
+      region = config.getString("region"),
+      host = config.getString("host"),
+      port = config.getInt("port"),
+      parallelism = config.getInt("parallelism"),
+      credentialsProvider = awsCredentialsProvider
     )
   }
 }
 
-case class DynamoSettings(region: String, host: String, port: Int, parallelism: Int) extends ClientSettings {
+case class DynamoSettings(region: String,
+                          host: String,
+                          port: Int,
+                          parallelism: Int,
+                          credentialsProvider: AWSCredentialsProvider)
+    extends ClientSettings {
   require(host.nonEmpty, "A host name must be provided.")
   require(port > -1, "A port number must be provided.")
 }
