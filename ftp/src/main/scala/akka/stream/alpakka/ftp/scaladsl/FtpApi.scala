844b54fd4ff7b5afcbbf5a36711675b11495428b
Ferdy Moon Soo Beekmans <1469938+fmsbeekmans@users.noreply.github.com>
2018-04-25 14:13:57 +0100

    FTP: Support custom SSH configuration (#875)
    * Make it possible to use custom SSH configuration.
    * add readme and examples for custom ssh client

--- ftp/src/main/scala/akka/stream/alpakka/ftp/scaladsl/FtpApi.scala
+++ ftp/src/main/scala/akka/stream/alpakka/ftp/scaladsl/FtpApi.scala
@@ -167,6 +167,20 @@ sealed trait FtpApi[FtpClient] { _: FtpSourceFactory[FtpClient] =>
   protected[this] implicit def ftpLike: FtpLike[FtpClient, S]
 }
 
+class SftpApi extends FtpApi[SSHClient] with SftpSourceParams
 object Ftp extends FtpApi[FTPClient] with FtpSourceParams
 object Ftps extends FtpApi[FTPClient] with FtpsSourceParams
-object Sftp extends FtpApi[SSHClient] with SftpSourceParams
+
+object Sftp extends SftpApi {
+
+  /**
+   * Scala API: creates a [[akka.stream.alpakka.ftp.scaladsl.SftpApi]]
+   *
+   * @param customSshClient custom ssh client
+   * @return A [[akka.stream.alpakka.ftp.scaladsl.SftpApi]]
+   */
+  def apply(customSshClient: SSHClient): SftpApi =
+    new SftpApi {
+      override val sshClient = customSshClient
+    }
+}
---
+++ ftp/src/test/java/akka/stream/alpakka/ftp/examples/ConfigureCustomSSHClient.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (C) 2016-2018 Lightbend Inc. <http://www.lightbend.com>
+ */
+
+package akka.stream.alpakka.ftp.examples;
+
+//#configure-custom-ssh-client
+
+import akka.stream.alpakka.ftp.javadsl.Sftp;
+import akka.stream.alpakka.ftp.javadsl.SftpApi;
+import net.schmizz.sshj.DefaultConfig;
+import net.schmizz.sshj.SSHClient;
+
+public class ConfigureCustomSSHClient {
+
+    public ConfigureCustomSSHClient() {
+        SSHClient sshClient = new SSHClient(new DefaultConfig());
+        SftpApi sftp = Sftp.create(sshClient);
+
+    }
+}
+//#configure-custom-ssh-client
