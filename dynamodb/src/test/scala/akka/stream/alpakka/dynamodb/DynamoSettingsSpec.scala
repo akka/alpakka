d5f54d99c505e2d0e0d7c43b66a04159c6794a75
Random Coder <randomcoder@randomcoding.co.uk>
2018-04-30 06:14:39 +0100

    AWS Dynamodb: add configurable credentials for client (#901)
    * Add AWS credentials to `DynamoSettings` and create from config if defined
    * Update docs to show usage of static credentials in config and programmatic creation
    * Check for both required keys in config before creating basic auth

---
+++ dynamodb/src/test/scala/akka/stream/alpakka/dynamodb/DynamoSettingsSpec.scala
@@ -0,0 +1,67 @@
+/*
+ * Copyright (C) 2016-2018 Lightbend Inc. <http://www.lightbend.com>
+ */
+
+package akka.stream.alpakka.dynamodb
+
+import akka.actor.ActorSystem
+import akka.stream.alpakka.dynamodb.impl.DynamoSettings
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain
+import com.typesafe.config.ConfigFactory
+import org.scalatest.{Matchers, WordSpecLike}
+
+class DynamoSettingsSpec extends WordSpecLike with Matchers {
+
+  "DynamoSettings" should {
+    "read the reference config from an actor system" in {
+      val system = ActorSystem()
+      val settings = DynamoSettings(system)
+
+      settings.region should be("us-east-1")
+      settings.host should be("localhost")
+      settings.port should be(8001)
+      settings.parallelism should be(4)
+      settings.credentialsProvider shouldBe a[DefaultAWSCredentialsProviderChain]
+    }
+
+    "use the DefaultAWSCredentialsProviderChain if the config defines an incomplete akka.stream.alpakka.dynamodb.credentials" in {
+      val config = ConfigFactory.parseString("""
+          |region = "eu-west-1"
+          |host = "localhost"
+          |port: 443
+          |parallelism = 32
+          |credentials {
+          |  access-key-id = "dummy-access-key"
+          |}
+        """.stripMargin)
+
+      val settings = DynamoSettings(config)
+      settings.credentialsProvider shouldBe a[DefaultAWSCredentialsProviderChain]
+    }
+
+    "read static aws credentials from a config that defines an akka.stream.alpakka.dynamodb.credentials" in {
+      val configWithStaticCredentials =
+        // #static-creds
+        """
+          | akka.stream.alpakka.dynamodb {
+          |  region = "eu-west-1"
+          |  host = "localhost"
+          |  port: 443
+          |  parallelism = 32
+          |  credentials {
+          |    access-key-id = "dummy-access-key"
+          |    secret-key-id = "dummy-secret-key"
+          |  }
+          |}""".stripMargin
+      // #static-creds
+
+      val config = ConfigFactory.parseString(configWithStaticCredentials).getConfig("akka.stream.alpakka.dynamodb")
+
+      val settings = DynamoSettings(config)
+      val credentials = settings.credentialsProvider.getCredentials
+      credentials.getAWSAccessKeyId should be("dummy-access-key")
+      credentials.getAWSSecretKey should be("dummy-secret-key")
+
+    }
+  }
+}
