335f400ec2cebf19b1b817e948e2c275d3921169
Enno <458526+ennru@users.noreply.github.com>
2018-06-08 17:31:51 +0200

    Add CSV to Kafka example in Java

---
+++ doc-examples/src/main/scala/playground/KafkaEmbedded.scala
@@ -0,0 +1,19 @@
+/*
+ * Copyright (C) 2016-2018 Lightbend Inc. <http://www.lightbend.com>
+ */
+
+package playground
+
+import net.manub.embeddedkafka.{EmbeddedKafka, EmbeddedKafkaConfig}
+
+object KafkaEmbedded {
+
+  def start(): Unit =
+    start(19000)
+
+  def start(kafkaPort: Int): Unit =
+    EmbeddedKafka.start()(EmbeddedKafkaConfig(kafkaPort))
+
+  def stop() =
+    EmbeddedKafka.stop()
+}
