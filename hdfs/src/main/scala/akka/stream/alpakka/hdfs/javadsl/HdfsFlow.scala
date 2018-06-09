9b481f3307f990ffeb2dc8cf0c06bfc180880132
Burak KOSE <burakks41@gmail.com>
2018-06-10 01:30:34 +0200

    Hdfs: Remove unused imports

--- hdfs/src/main/scala/akka/stream/alpakka/hdfs/javadsl/HdfsFlow.scala
+++ hdfs/src/main/scala/akka/stream/alpakka/hdfs/javadsl/HdfsFlow.scala
@@ -7,7 +7,6 @@ package akka.stream.alpakka.hdfs.javadsl
 import akka.NotUsed
 import akka.japi.Pair
 import akka.stream.alpakka.hdfs._
-import akka.stream.alpakka.hdfs.impl.strategy.DefaultSyncStrategy
 import akka.stream.alpakka.hdfs.scaladsl.{HdfsFlow => ScalaHdfsFlow}
 import akka.stream.javadsl
 import akka.stream.scaladsl.Flow
