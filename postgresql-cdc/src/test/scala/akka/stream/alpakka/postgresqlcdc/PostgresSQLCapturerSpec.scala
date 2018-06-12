dacf19245c5c548e6e0ec540067942ef1c298b2c
sebastianharko <sebastian.harko@lightbend.com>
2018-06-12 16:21:40 -0700

    test with jsonb data type

--- postgresql-cdc/src/test/scala/akka/stream/alpakka/postgresqlcdc/PostgresSQLCapturerSpec.scala
+++ postgresql-cdc/src/test/scala/akka/stream/alpakka/postgresqlcdc/PostgresSQLCapturerSpec.scala
@@ -12,6 +12,7 @@ import akka.stream.{ActorMaterializer, Attributes}
 import akka.stream.alpakka.postgresqlcdc.scaladsl.PostgreSQLCapturer
 import akka.stream.testkit.scaladsl.TestSink
 import akka.testkit.{ImplicitSender, TestKit}
+import org.postgresql.util.PGobject
 import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpecLike}
 
 class PostgresSQLCapturerSpec
@@ -35,7 +36,7 @@ class PostgresSQLCapturerSpec
     DriverManager.getConnection(connectionString)
   }
 
-  private def createCustomersTable(implicit conn: Connection): Unit = {
+  private def createCustomersTable()(implicit conn: Connection): Unit = {
     val createStatement =
       conn.prepareStatement("""
           |CREATE TABLE customers (
@@ -50,12 +51,23 @@ class PostgresSQLCapturerSpec
     createStatement.execute()
   }
 
-  private def dropTableCustomers()(implicit conn: Connection): Unit = {
-    val dropStatement =
-      conn.prepareStatement("""|DROP TABLE customers;
+  private def createSalesTable()(implicit conn: Connection): Unit = {
+    val createStatement =
+      conn.prepareStatement("""
+          |CREATE TABLE SALES(
+          | id SERIAL NOT NULL PRIMARY KEY,
+          | info JSONB NOT NULL
+          |);
         """.stripMargin)
-    dropStatement.execute()
+    createStatement.execute()
+
   }
+
+  private def dropTableCustomers()(implicit conn: Connection): Unit =
+    conn.prepareStatement("DROP TABLE customers;").execute()
+
+  private def dropTableSales()(implicit conn: Connection): Unit =
+    conn.prepareStatement("DROP TABLE sales;").execute()
 
   private def insertCustomer(id: Int, fName: String, lName: String, email: String)(implicit conn: Connection): Unit = {
     val insertStatement =
@@ -81,6 +93,35 @@ class PostgresSQLCapturerSpec
     deleteStatement.execute()
   }
 
+  private def insertSale(id: Int, info: String)(implicit conn: Connection): Unit = {
+    val pgObject = new PGobject
+    pgObject.setType("jsonb")
+    pgObject.setValue(info)
+    val insertStatement =
+      conn.prepareStatement("INSERT INTO sales(id, info) VALUES (?, ?);")
+    insertStatement.setInt(1, id)
+    insertStatement.setObject(2, pgObject)
+    insertStatement.execute()
+  }
+
+  private def updateSale(id: Int, newInfo: String)(implicit conn: Connection): Unit = {
+    val pgObject = new PGobject
+    pgObject.setType("jsonb")
+    pgObject.setValue(newInfo)
+    val updateStatement =
+      conn.prepareStatement("UPDATE sales SET info = ? WHERE id = ?;")
+    updateStatement.setObject(1, pgObject)
+    updateStatement.setInt(2, id)
+    updateStatement.execute()
+  }
+
+  private def deleteSale(id: Int)(implicit conn: Connection): Unit = {
+    val deleteStatement =
+      conn.prepareStatement("DELETE FROM sales WHERE id = ?;")
+    deleteStatement.setInt(1, id)
+    deleteStatement.execute()
+  }
+
   private def setUpLogicalDecodingSlot()(implicit conn: Connection): Unit = {
     val stmt = conn.prepareStatement("SELECT * FROM pg_create_logical_replication_slot('scalatest','test_decoding')")
     stmt.execute()
@@ -95,20 +136,7 @@ class PostgresSQLCapturerSpec
     log.info("setting up logical decoding slot and creating customers table")
     setUpLogicalDecodingSlot()
     createCustomersTable
-    log.info("inserting data into customers table")
-    // some inserts
-    insertCustomer(id = 0, fName = "John", lName = "Lennon", email = "john.lennon@akka.io")
-    insertCustomer(id = 1, fName = "George", lName = "Harrison", email = "george.harrison@akka.io")
-    insertCustomer(id = 2, fName = "Paul", lName = "McCartney", email = "paul.mccartney@akka.io")
-    insertCustomer(id = 3, fName = "Ringo", lName = "Star", email = "ringo.star@akka.io")
-    // some updates
-    updateCustomerEmail(id = 0, "john.lennon@thebeatles.com")
-    updateCustomerEmail(id = 1, "george.harrison@thebeatles.com")
-    updateCustomerEmail(id = 2, "paul.mccartney@thebeatles.com")
-    updateCustomerEmail(id = 3, "ringo.star@thebeatles.com")
-    // some deletes
-    deleteCustomers()
-
+    createSalesTable
   }
 
   override def afterAll: Unit = {
@@ -119,6 +147,7 @@ class PostgresSQLCapturerSpec
     // the CI creates fresh docker containers and destroys them after the test is complete anyway.
     dropLogicalDecodingSlot()
     dropTableCustomers()
+    dropTableSales()
 
     conn.close()
 
@@ -126,7 +155,21 @@ class PostgresSQLCapturerSpec
 
   "A PostgreSQL change data capture source" must {
 
-    "capture changes to database tables" in {
+    "capture changes to a table with numeric / character columns " in {
+
+      log.info("inserting data into customers table")
+      // some inserts
+      insertCustomer(id = 0, fName = "John", lName = "Lennon", email = "john.lennon@akka.io")
+      insertCustomer(id = 1, fName = "George", lName = "Harrison", email = "george.harrison@akka.io")
+      insertCustomer(id = 2, fName = "Paul", lName = "McCartney", email = "paul.mccartney@akka.io")
+      insertCustomer(id = 3, fName = "Ringo", lName = "Star", email = "ringo.star@akka.io")
+      // some updates
+      updateCustomerEmail(id = 0, "john.lennon@thebeatles.com")
+      updateCustomerEmail(id = 1, "george.harrison@thebeatles.com")
+      updateCustomerEmail(id = 2, "paul.mccartney@thebeatles.com")
+      updateCustomerEmail(id = 3, "ringo.star@thebeatles.com")
+      // some deletes
+      deleteCustomers()
 
       val settings = PostgreSQLChangeDataCaptureSettings(connectionString, slotName = "scalatest")
 
@@ -219,6 +262,38 @@ class PostgresSQLCapturerSpec
         .expectNextChainingPF {
           case c @ ChangeSet(_, deleteEvents)
               if deleteEvents.size == 4 && deleteEvents.count(_.isInstanceOf[RowDeleted]) == 4 => // success
+        }
+
+    }
+
+    "capture changes to a table with a jsonb columns" in {
+      insertSale(id = 0, info = """{"name": "alpaca", "countries": ["Peru", "Bolivia", "Ecuador", "Chile"]}""")
+      updateSale(id = 0, newInfo = """{"name": "alpakka", "countries": ["*"]}""")
+      deleteSale(0)
+
+      val settings = PostgreSQLChangeDataCaptureSettings(connectionString, slotName = "scalatest")
+
+      PostgreSQLCapturer(settings)
+        .log("postgresqlcdc", cs => s"captured change: ${cs.toString}")
+        .withAttributes(Attributes.logLevels(onElement = Logging.InfoLevel))
+        .runWith(TestSink.probe[ChangeSet])
+        .request(3)
+        .expectNextChainingPF {
+          case c @ ChangeSet(_, List(RowInserted("public", "sales", fields)))
+              if fields.map(f => f.columnName -> f.value).toSet == Set(
+                "id" -> "0",
+                "info" -> """{"name": "alpaca", "countries": ["Peru", "Bolivia", "Ecuador", "Chile"]}"""
+              ) => // success
+        }
+        .expectNextChainingPF {
+          case c @ ChangeSet(_, List(RowUpdated("public", "sales", fields)))
+              if fields.map(f => f.columnName -> f.value).toSet == Set(
+                "id" -> "0",
+                "info" -> """{"name": "alpakka", "countries": ["*"]}"""
+              ) => // success
+        }
+        .expectNextChainingPF {
+          case c @ ChangeSet(_, List(RowDeleted(_, _, _))) => // success
         }
 
     }
