72bb303290044dc7a330ffa4d19128fcc2f1a86d
Enno <458526+ennru@users.noreply.github.com>
2018-06-14 13:57:11 +0200

    New module: Text en-/decoding (#1011)
    This module offers flows for character set encoding, decoding and transcoding.

---
+++ text/src/test/java/docs/javadsl/CharsetCodingFlowsDoc.java
@@ -0,0 +1,104 @@
+/*
+ * Copyright (C) 2016-2018 Lightbend Inc. <http://www.lightbend.com>
+ */
+
+package docs.javadsl;
+
+import akka.actor.ActorSystem;
+import akka.stream.ActorMaterializer;
+import akka.stream.Materializer;
+
+// #encoding
+import akka.stream.alpakka.text.javadsl.TextFlow;
+import akka.stream.IOResult;
+import akka.stream.javadsl.FileIO;
+import akka.stream.javadsl.Sink;
+import akka.stream.javadsl.Source;
+import akka.util.ByteString;
+
+import java.nio.charset.StandardCharsets;
+
+// #encoding
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Properties;
+import java.util.concurrent.CompletionStage;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.*;
+
+public class CharsetCodingFlowsDoc {
+
+    private final static ActorSystem system = ActorSystem.create();
+    private final static Materializer materializer = ActorMaterializer.create(system);
+
+    @AfterClass
+    public static void afterAll() {
+        system.terminate();
+    }
+
+    @Test
+    public void encodingExample() throws Exception {
+        Path targetFile = Paths.get("target/outdata.txt");
+
+        Properties properties = System.getProperties();
+        List<String> strings = properties.stringPropertyNames().stream().map(p -> p + " -> " + properties.getProperty(p)).collect(Collectors.toList());
+        // #encoding
+        Source<String, ?> stringSource = // ...
+                // #encoding
+                Source.from(strings);
+        final CompletionStage<IOResult> streamCompletion =
+                // #encoding
+                stringSource
+                        .via(TextFlow.encoding(StandardCharsets.US_ASCII))
+                        .intersperse(ByteString.fromString("\n"))
+                        .runWith(FileIO.toPath(targetFile), materializer);
+        // #encoding
+        IOResult result = streamCompletion.toCompletableFuture().get(1, TimeUnit.SECONDS);
+        assertTrue(result.wasSuccessful());
+    }
+
+    @Test
+    public void decodingExample() throws Exception {
+        ByteString utf16bytes = ByteString.fromString("äåûßêëé", StandardCharsets.UTF_16);
+        // #decoding
+        Source<ByteString, ?> byteStringSource = // ...
+                // #decoding
+                Source.single(utf16bytes);
+        CompletionStage<List<String>> streamCompletion =
+                // #decoding
+                byteStringSource
+                        .via(TextFlow.decoding(StandardCharsets.UTF_16))
+                        .runWith(Sink.seq(), materializer);
+        // #decoding
+        List<String> result = streamCompletion.toCompletableFuture().get(1, TimeUnit.SECONDS);
+        assertEquals(Arrays.asList("äåûßêëé"), result);
+
+    }
+
+    @Test
+    public void transcodingExample() throws Exception {
+        Path targetFile = Paths.get("target/outdata.txt");
+        ByteString utf16bytes = ByteString.fromString("äåûßêëé", StandardCharsets.UTF_16);
+
+        // #transcoding
+        Source<ByteString, ?> byteStringSource = // ...
+                // #transcoding
+                Source.single(utf16bytes);
+        CompletionStage<IOResult> streamCompletion =
+                // #transcoding
+                byteStringSource
+                        .via(TextFlow.transcoding(StandardCharsets.UTF_16, StandardCharsets.UTF_8))
+                        .runWith(FileIO.toPath(targetFile), materializer);
+        // #transcoding
+        IOResult result = streamCompletion.toCompletableFuture().get(1, TimeUnit.SECONDS);
+        assertTrue(result.wasSuccessful());
+    }
+}
---
+++ text/src/test/scala/akka/stream/alpakka/text/scaladsl/CharsetCodingFlowsSpec.scala
@@ -0,0 +1,177 @@
+/*
+ * Copyright (C) 2016-2018 Lightbend Inc. <http://www.lightbend.com>
+ */
+
+package akka.stream.alpakka.text.scaladsl
+
+import java.nio.charset.{Charset, StandardCharsets, UnmappableCharacterException}
+import java.nio.file.Paths
+
+import akka.Done
+import akka.actor.ActorSystem
+import akka.stream.scaladsl.{Keep, Sink, Source}
+import akka.stream.testkit.scaladsl.{TestSink, TestSource}
+import akka.stream.{ActorMaterializer, IOResult, Materializer}
+import akka.testkit.TestKit
+import akka.util.ByteString
+import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.{BeforeAndAfterAll, Matchers, RecoverMethods, WordSpecLike}
+
+import scala.collection.immutable
+import scala.concurrent.duration.DurationInt
+import scala.concurrent.{ExecutionContextExecutor, Future}
+import scala.util.Success
+
+class CharsetCodingFlowsSpec
+    extends TestKit(ActorSystem("charset"))
+    with WordSpecLike
+    with Matchers
+    with BeforeAndAfterAll
+    with ScalaFutures
+    with RecoverMethods {
+
+  private implicit val mat: Materializer = ActorMaterializer()
+  private implicit val executionContext: ExecutionContextExecutor = system.dispatcher
+
+  val multiByteChars = "äåû經濟商行政管理总局التجارى"
+
+  "Encoding" should {
+    def verifyEncoding(charsetOut: Charset, value: String) = {
+      val result = Source
+        .single(value)
+        .via(TextFlow.encoding(charsetOut))
+        .map(_.decodeString(charsetOut))
+        .runWith(Sink.head)
+      result.futureValue should be(value)
+    }
+
+    "work for UTF-16" in {
+      verifyEncoding(StandardCharsets.UTF_16, multiByteChars)
+    }
+
+    "fail for non-representable chars" in {
+      recoverToSucceededIf[UnmappableCharacterException] {
+        Source
+          .single("經濟部")
+          .via(TextFlow.encoding(StandardCharsets.US_ASCII))
+          .runWith(Sink.ignore)
+      }
+    }
+
+    "be illustrated in a documentation example" in {
+      import java.nio.charset.StandardCharsets
+
+      import akka.stream.scaladsl.FileIO
+
+      // #encoding
+      import scala.collection.JavaConverters._
+      val targetFile = Paths.get("target/outdata.txt")
+      val strings = System.getProperties.asScala.map(p => p._1 + " -> " + p._2).toList
+      val stringSource: Source[String, _] = Source(strings)
+      val result =
+        stringSource
+          .via(TextFlow.encoding(StandardCharsets.US_ASCII))
+          .intersperse(ByteString("\n"))
+          .runWith(FileIO.toPath(targetFile))
+      result.futureValue.status should be(Success(Done))
+    }
+  }
+
+  "Decoding" should {
+    "be illustrated in a documentation example" in {
+      import java.nio.charset.StandardCharsets
+
+      val utf16bytes = ByteString("äåûßêëé", StandardCharsets.UTF_16)
+      val byteStringSource: Source[ByteString, _] =
+        Source
+          .single(utf16bytes)
+
+      val result: Future[immutable.Seq[String]] =
+        byteStringSource
+          .via(TextFlow.decoding(StandardCharsets.UTF_16))
+          .runWith(Sink.seq)
+      result.futureValue should be(Seq("äåûßêëé"))
+    }
+
+  }
+
+  "Transcoding" should {
+    "be illustrated in a documentation example" in {
+      import java.nio.charset.StandardCharsets
+
+      import akka.stream.scaladsl.FileIO
+
+      val utf16bytes = ByteString("äåûßêëé", StandardCharsets.UTF_16)
+      val targetFile = Paths.get("target/outdata-transcoding.txt")
+      val byteStringSource: Source[ByteString, _] =
+        Source
+          .single(utf16bytes)
+      val result: Future[IOResult] =
+        byteStringSource
+          .via(TextFlow.transcoding(StandardCharsets.UTF_16, StandardCharsets.UTF_8))
+          .runWith(FileIO.toPath(targetFile))
+      result.futureValue.status should be(Success(Done))
+    }
+
+    def verifyTranscoding(charsetIn: Charset, charsetOut: Charset, value: String) = {
+      val result = Source
+        .single(value)
+        .map(s => ByteString(s, charsetIn))
+        .via(TextFlow.transcoding(charsetIn, charsetOut))
+        .map(_.decodeString(charsetOut))
+        .runWith(Sink.head)
+      result.futureValue should be(value)
+    }
+
+    def verifyByteSends(charsetIn: Charset, charsetOut: Charset, in: String) = {
+      val (source, sink) = TestSource
+        .probe[ByteString]
+        .via(TextFlow.transcoding(charsetIn, charsetOut))
+        .map(_.decodeString(charsetOut))
+        .toMat(Sink.seq)(Keep.both)
+        .run()
+
+      val bs = ByteString(in, charsetIn)
+      bs.sliding(1).foreach { bs =>
+        source.sendNext(bs)
+      }
+      source.sendComplete()
+      sink.futureValue.mkString should be(in)
+    }
+
+    "work for UTF-16" in {
+      verifyTranscoding(StandardCharsets.UTF_16, StandardCharsets.UTF_16, multiByteChars)
+    }
+
+    "work for UTF-16BE to LE" in {
+      verifyTranscoding(StandardCharsets.UTF_16BE, StandardCharsets.UTF_16LE, multiByteChars)
+    }
+
+    "work for Windows-1252 to UTF-16LE" in {
+      verifyTranscoding(Charset.forName("windows-1252"), StandardCharsets.UTF_16LE, "äåûßêëé")
+    }
+
+    "complete" in {
+      val (source, sink) = TestSource
+        .probe[ByteString]
+        .via(TextFlow.transcoding(StandardCharsets.UTF_8, StandardCharsets.UTF_8))
+        .toMat(TestSink.probe[ByteString])(Keep.both)
+        .run()
+      source.sendNext(ByteString("eins,zwei,drei"))
+      sink.request(3)
+      sink.expectNext(ByteString("eins,zwei,drei"))
+      sink.expectNoMessage(100.millis)
+      source.sendComplete()
+      sink.expectComplete()
+    }
+
+    "work byte by byte from UTF-16LE" in {
+      verifyByteSends(StandardCharsets.UTF_16LE, StandardCharsets.UTF_8, multiByteChars)
+    }
+
+    "work for byte by byte windows-1252" in {
+      verifyByteSends(Charset.forName("windows-1252"), StandardCharsets.UTF_8, "äåûßêëé")
+    }
+  }
+
+}
