5aa3e27e66b6342a25c58519c70ebf0e76742d59
Martynas Mickevičius <self@2m.lt>
2018-06-19 13:52:21 +0300

    Add graph stage implementation

---
+++ reference/src/main/scala/akka/stream/alpakka/reference/impl/ReferenceSource.scala
@@ -0,0 +1,57 @@
+/*
+ * Copyright (C) 2016-2018 Lightbend Inc. <http://www.lightbend.com>
+ */
+
+package akka.stream.alpakka.reference.impl
+
+import akka.NotUsed
+import akka.annotation.InternalApi
+import akka.event.Logging
+import akka.stream._
+import akka.stream.alpakka.reference.{ReferenceReadMessage, SourceSettings}
+import akka.stream.stage.{GraphStageLogic, GraphStageWithMaterializedValue}
+
+import scala.concurrent.{Future, Promise}
+
+/**
+ * private package hides the class from the API in Scala. However it is still
+ * visible in Java. Use "InternalApi" annotation to communicate to Java developers
+ * that this is private API.
+ */
+@InternalApi private[reference] final class ReferenceSourceStageLogic(
+    val settings: SourceSettings,
+    val startupPromise: Promise[NotUsed],
+    val shape: SourceShape[ReferenceReadMessage]
+) extends GraphStageLogic(shape) {
+
+  private def out = shape.out
+
+  /**
+   * Initialization logic
+   */
+  override def preStart(): Unit =
+    startupPromise.success(NotUsed)
+
+  /**
+   * Cleanup logic
+   */
+  override def postStop(): Unit = {}
+}
+
+@InternalApi final class ReferenceSource(settings: SourceSettings)
+    extends GraphStageWithMaterializedValue[SourceShape[ReferenceReadMessage], Future[NotUsed]] {
+  val out: Outlet[ReferenceReadMessage] = Outlet(Logging.simpleName(this) + ".out")
+
+  override def initialAttributes: Attributes =
+    Attributes.name(Logging.simpleName(this))
+
+  override val shape: SourceShape[ReferenceReadMessage] = SourceShape(out)
+
+  override def createLogicAndMaterializedValue(inheritedAttributes: Attributes): (GraphStageLogic, Future[NotUsed]) = {
+    // materialized value created as a new instance on every materialization
+    val startupPromise = Promise[NotUsed]
+    val logic = new ReferenceSourceStageLogic(settings, startupPromise, shape)
+    (logic, startupPromise.future)
+  }
+
+}
--- reference/src/main/scala/akka/stream/alpakka/reference/scaladsl/Reference.scala
+++ reference/src/main/scala/akka/stream/alpakka/reference/scaladsl/Reference.scala
@@ -5,6 +5,7 @@
 package akka.stream.alpakka.reference.scaladsl
 
 import akka.NotUsed
+import akka.stream.alpakka.reference.impl.{ReferenceFlow, ReferenceSource}
 import akka.stream.alpakka.reference.{ReferenceReadMessage, ReferenceWriteMessage, SourceSettings}
 import akka.stream.scaladsl.{Flow, Source}
 
@@ -12,7 +13,18 @@ import scala.concurrent.Future
 
 object Reference {
 
-  def source(settings: SourceSettings): Source[ReferenceReadMessage, Future[NotUsed]] = ???
+  /**
+   * API doc should describe where the messages are coming from.
+   *
+   * Also describe the significance of the materialized value.
+   */
+  def source(settings: SourceSettings): Source[ReferenceReadMessage, Future[NotUsed]] =
+    Source.fromGraph(new ReferenceSource(settings))
 
-  def flow(): Flow[ReferenceWriteMessage, ReferenceWriteMessage, NotUsed] = ???
+  /**
+   * API doc should describe what will be done to the incoming messages to the flow,
+   * and what messages will be emitted by the flow.
+   */
+  def flow(): Flow[ReferenceWriteMessage, ReferenceWriteMessage, NotUsed] =
+    Flow.fromGraph(new ReferenceFlow())
 }
