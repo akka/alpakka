6167f29e302920d90f0d5de779d22a9a2fa342a0
Burak KOSE <burakks41@gmail.com>
2018-06-27 11:19:19 +0300

    HDFS: Add sources and flows (#965)
    Ref: #557
    
    In this pull request, you will find the Hdfs connector.
    
    - Documentation
    - Java Tests
    - Support HDFS version 2.x
    - pass-through

---
+++ docs/src/main/paradox/hdfs.md
@@ -0,0 +1,227 @@
+# Hadoop Distributed File System - HDFS
+
+The connector offers Flows and Sources that interact with HDFS file systems.
+
+For more information about Hadoop, please visit the [Hadoop documentation](https://hadoop.apache.org/).
+
+### Reported issues
+
+[Tagged issues at Github](https://github.com/akka/alpakka/labels/p%3Ahdfs)
+
+## Artifacts
+
+@@dependency [sbt,Maven,Gradle] {
+  group=com.lightbend.akka
+  artifact=akka-stream-alpakka-hdfs_$scalaBinaryVersion$
+  version=$version$
+}
+
+## Specifying a Hadoop Version
+
+By default, HDFS connector uses Hadoop **3.1.0**. If you are using a different version of Hadoop, 
+you should exclude the Hadoop libraries from the connector dependency and add the dependency for your preferred version.
+
+## Set up client
+
+Flows provided by this connector need a prepared `org.apache.hadoop.fs.FileSystem` to 
+interact with HDFS.
+
+
+Scala
+: @@snip ($alpakka$/hdfs/src/test/scala/akka/stream/alpakka/hdfs/HdfsWriterSpec.scala) { #init-client }
+
+Java
+: @@snip ($alpakka$/hdfs/src/test/java/akka/stream/alpakka/hdfs/HdfsWriterTest.java) { #init-client }
+
+
+## Writing
+
+The connector provides three Flows. Each flow requires `RotationStrategy` and `SyncStrategy` to run.
+@scala[@scaladoc[HdfsFlow](akka.stream.alpakka.hdfs.scaladsl.HdfsFlow$).]
+@java[@scaladoc[HdfsFlow](akka.stream.alpakka.hdfs.javadsl.HdfsFlow$).]
+
+The flows push `OutgoingMessage` to a downstream.
+
+### Data Writer
+
+Use `HdfsFlow.data` to stream with @javadoc[FSDataOutputStream](org.apache.hadoop.fs.FSDataOutputStream) without any compression.
+
+
+Scala
+: @@snip ($alpakka$/hdfs/src/test/scala/akka/stream/alpakka/hdfs/HdfsWriterSpec.scala) { #define-data }
+
+Java
+: @@snip ($alpakka$/hdfs/src/test/java/akka/stream/alpakka/hdfs/HdfsWriterTest.java) { #define-data }
+
+
+### Compressed Data Writer
+
+First, create @javadoc[CompressionCodec](org.apache.hadoop.io.compress.CompressionCodec).
+
+
+Scala
+: @@snip ($alpakka$/hdfs/src/test/scala/akka/stream/alpakka/hdfs/HdfsWriterSpec.scala) { #define-codec }
+
+Java
+: @@snip ($alpakka$/hdfs/src/test/java/akka/stream/alpakka/hdfs/HdfsWriterTest.java) { #define-codec }
+
+
+Then, use `HdfsFlow.compress` to stream with @javadoc[CompressionOutputStream](org.apache.hadoop.io.compress.CompressionOutputStream) and @javadoc[CompressionCodec](org.apache.hadoop.io.compress.CompressionCodec). 
+
+
+Scala
+: @@snip ($alpakka$/hdfs/src/test/scala/akka/stream/alpakka/hdfs/HdfsWriterSpec.scala) { #define-compress }
+
+Java
+: @@snip ($alpakka$/hdfs/src/test/java/akka/stream/alpakka/hdfs/HdfsWriterTest.java) { #define-compress }
+
+
+### Sequence Writer
+
+Use `HdfsFlow.sequence` to stream a flat file consisting of binary key/value pairs.
+
+#### Without Compression
+
+
+Scala
+: @@snip ($alpakka$/hdfs/src/test/scala/akka/stream/alpakka/hdfs/HdfsWriterSpec.scala) { #define-sequence }
+
+Java
+: @@snip ($alpakka$/hdfs/src/test/java/akka/stream/alpakka/hdfs/HdfsWriterTest.java) { #define-sequence }
+
+
+#### With Compression
+
+First, define a codec.
+
+
+Scala
+: @@snip ($alpakka$/hdfs/src/test/scala/akka/stream/alpakka/hdfs/HdfsWriterSpec.scala) { #define-codec }
+
+Java
+: @@snip ($alpakka$/hdfs/src/test/java/akka/stream/alpakka/hdfs/HdfsWriterTest.java) { #define-codec }
+
+
+Then, create a flow.
+
+
+Scala
+: @@snip ($alpakka$/hdfs/src/test/scala/akka/stream/alpakka/hdfs/HdfsWriterSpec.scala) { #define-sequence-compressed }
+
+Java
+: @@snip ($alpakka$/hdfs/src/test/java/akka/stream/alpakka/hdfs/HdfsWriterTest.java) { #define-sequence-compressed }
+
+### Passing data through HdfsFlow
+
+Use `HdfsFlow.dataWithPassThrough`, `HdfsFlow.compressedWithPassThrough` or `HdfsFlow.sequenceWithPassThrough`.
+
+When streaming documents from Kafka, you might want to commit to Kafka. The flow will emit two messages.
+For every input, it will produce `WrittenMessage` and when it rotates, `RotationMessage`.
+
+Let's say that we have these classes.
+
+
+Scala
+: @@snip ($alpakka$/hdfs/src/test/scala/akka/stream/alpakka/hdfs/HdfsWriterSpec.scala) { #define-kafka-classes }
+
+Java
+: @@snip ($alpakka$/hdfs/src/test/java/akka/stream/alpakka/hdfs/HdfsWriterTest.java) { #define-kafka-classes }
+
+
+Then, we can stream with `passThrough`.
+
+
+Scala
+: @@snip ($alpakka$/hdfs/src/test/scala/akka/stream/alpakka/hdfs/HdfsWriterSpec.scala) { #kafka-example }
+
+Java
+: @@snip ($alpakka$/hdfs/src/test/java/akka/stream/alpakka/hdfs/HdfsWriterTest.java) { #kafka-example }
+
+
+## Configuration
+
+We can configure the sink by `HdfsWritingSettings`. 
+
+
+Scala
+: @@snip ($alpakka$/hdfs/src/test/scala/akka/stream/alpakka/hdfs/HdfsWriterSpec.scala) { #define-settings }
+
+Java
+: @@snip ($alpakka$/hdfs/src/test/java/akka/stream/alpakka/hdfs/HdfsWriterTest.java) { #define-settings }
+
+
+### File path generator
+
+@scaladoc[FilePathGenerator](akka.stream.alpakka.hdfs.FilePathGenerator$) provides a functionality to generate rotation path in HDFS. 
+
+Scala
+: @@snip ($alpakka$/hdfs/src/test/scala/akka/stream/alpakka/hdfs/HdfsWriterSpec.scala) { #define-generator }
+
+Java
+: @@snip ($alpakka$/hdfs/src/test/java/akka/stream/alpakka/hdfs/HdfsWriterTest.java) { #define-generator }
+
+
+### Rotation Strategy
+
+
+@scaladoc[RotationStrategy](akka.stream.alpakka.hdfs.RotationStrategyFactory$) provides a functionality to decide when to rotate files.
+
+### Sync Strategy
+
+
+@scaladoc[SyncStrategy](akka.stream.alpakka.hdfs.SyncStrategyFactory$) provides a functionality to decide when to synchronize the output.
+
+## Reading
+
+Use `HdfsSource` to read from HDFS.
+@scala[@scaladoc[HdfsSource](akka.stream.alpakka.hdfs.scaladsl.HdfsSource$).]
+@java[@scaladoc[HdfsSource](akka.stream.alpakka.hdfs.javadsl.HdfsSource$).]
+
+
+### Data Reader
+
+
+Scala
+: @@snip ($alpakka$/hdfs/src/test/scala/akka/stream/alpakka/hdfs/HdfsReaderSpec.scala) { #define-data-source }
+
+Java
+: @@snip ($alpakka$/hdfs/src/test/java/akka/stream/alpakka/hdfs/HdfsReaderTest.java) { #define-data-source }
+
+
+### Compressed Data Reader
+
+
+Scala
+: @@snip ($alpakka$/hdfs/src/test/scala/akka/stream/alpakka/hdfs/HdfsReaderSpec.scala) { #define-compressed-source }
+
+Java
+: @@snip ($alpakka$/hdfs/src/test/java/akka/stream/alpakka/hdfs/HdfsReaderTest.java) { #define-compressed-source }
+
+
+### Sequence Reader
+
+
+Scala
+: @@snip ($alpakka$/hdfs/src/test/scala/akka/stream/alpakka/hdfs/HdfsReaderSpec.scala) { #define-sequence-source }
+
+Java
+: @@snip ($alpakka$/hdfs/src/test/java/akka/stream/alpakka/hdfs/HdfsReaderTest.java) { #define-sequence-source }
+
+
+## Running the example code
+
+The code in this guide is part of runnable tests of this project. You are welcome to edit the code and run it in sbt.
+
+Scala
+:   ```
+    sbt
+    > hdfs/testOnly *.HdfsWriterSpec
+    > hdfs/testOnly *.HdfsReaderSpec
+    ```
+
+Java
+:   ```
+    sbt
+    > hdfs/testOnly *.HdfsWriterTest
+    > hdfs/testOnly *.HdfsReaderTest
+    ```
---
+++ hdfs/src/main/scala/akka/stream/alpakka/hdfs/javadsl/HdfsFlow.scala
@@ -0,0 +1,236 @@
+/*
+ * Copyright (C) 2016-2018 Lightbend Inc. <http://www.lightbend.com>
+ */
+
+package akka.stream.alpakka.hdfs.javadsl
+
+import akka.NotUsed
+import akka.japi.Pair
+import akka.stream.alpakka.hdfs._
+import akka.stream.alpakka.hdfs.scaladsl.{HdfsFlow => ScalaHdfsFlow}
+import akka.stream.javadsl
+import akka.stream.scaladsl.Flow
+import akka.util.ByteString
+import org.apache.hadoop.fs.FileSystem
+import org.apache.hadoop.io.SequenceFile.CompressionType
+import org.apache.hadoop.io.Writable
+import org.apache.hadoop.io.compress.CompressionCodec
+
+object HdfsFlow {
+
+  /**
+   * Java API: creates a Flow for [[org.apache.hadoop.fs.FSDataOutputStream]]
+   *
+   * @param fs Hadoop file system
+   * @param syncStrategy sync strategy
+   * @param rotationStrategy rotation strategy
+   * @param settings hdfs writing settings
+   */
+  def data(
+      fs: FileSystem,
+      syncStrategy: SyncStrategy,
+      rotationStrategy: RotationStrategy,
+      settings: HdfsWritingSettings
+  ): javadsl.Flow[HdfsWriteMessage[ByteString, NotUsed], RotationMessage, NotUsed] =
+    ScalaHdfsFlow.data(fs, syncStrategy, rotationStrategy, settings).asJava
+
+  /**
+   * Java API: creates a flow for [[org.apache.hadoop.fs.FSDataOutputStream]]
+   * with `passThrough` of type `C`
+   *
+   * @param fs Hadoop file system
+   * @param syncStrategy sync strategy
+   * @param rotationStrategy rotation strategy
+   * @param settings hdfs writing settings
+   */
+  def dataWithPassThrough[P](
+      fs: FileSystem,
+      syncStrategy: SyncStrategy,
+      rotationStrategy: RotationStrategy,
+      settings: HdfsWritingSettings
+  ): javadsl.Flow[HdfsWriteMessage[ByteString, P], OutgoingMessage[P], NotUsed] =
+    ScalaHdfsFlow
+      .dataWithPassThrough[P](
+        fs,
+        syncStrategy,
+        rotationStrategy,
+        settings
+      )
+      .asJava
+
+  /**
+   * Java API: creates a flow for [[org.apache.hadoop.io.compress.CompressionOutputStream]]
+   *
+   * @param fs Hadoop file system
+   * @param syncStrategy sync strategy
+   * @param rotationStrategy rotation strategy
+   * @param compressionCodec a streaming compression/decompression pair
+   * @param settings hdfs writing settings
+   */
+  def compressed(
+      fs: FileSystem,
+      syncStrategy: SyncStrategy,
+      rotationStrategy: RotationStrategy,
+      compressionCodec: CompressionCodec,
+      settings: HdfsWritingSettings
+  ): javadsl.Flow[HdfsWriteMessage[ByteString, NotUsed], RotationMessage, NotUsed] =
+    ScalaHdfsFlow.compressed(fs, syncStrategy, rotationStrategy, compressionCodec, settings).asJava
+
+  /**
+   * Java API: creates a flow for [[org.apache.hadoop.io.compress.CompressionOutputStream]]
+   * with `passThrough` of type `C`
+   *
+   * @param fs Hadoop file system
+   * @param syncStrategy sync strategy
+   * @param rotationStrategy rotation strategy
+   * @param compressionCodec a streaming compression/decompression pair
+   * @param settings hdfs writing settings
+   */
+  def compressedWithPassThrough[P](
+      fs: FileSystem,
+      syncStrategy: SyncStrategy,
+      rotationStrategy: RotationStrategy,
+      compressionCodec: CompressionCodec,
+      settings: HdfsWritingSettings
+  ): javadsl.Flow[HdfsWriteMessage[ByteString, P], OutgoingMessage[P], NotUsed] =
+    ScalaHdfsFlow
+      .compressedWithPassThrough[P](
+        fs,
+        syncStrategy,
+        rotationStrategy,
+        compressionCodec,
+        settings
+      )
+      .asJava
+
+  /**
+   * Java API: creates a flow for [[org.apache.hadoop.io.SequenceFile.Writer]]
+   * without a compression
+   *
+   * @param fs Hadoop file system
+   * @param syncStrategy sync strategy
+   * @param rotationStrategy rotation strategy
+   * @param settings hdfs writing settings
+   * @param classK a key class
+   * @param classV a value class
+   */
+  def sequence[K <: Writable, V <: Writable](
+      fs: FileSystem,
+      syncStrategy: SyncStrategy,
+      rotationStrategy: RotationStrategy,
+      settings: HdfsWritingSettings,
+      classK: Class[K],
+      classV: Class[V]
+  ): javadsl.Flow[HdfsWriteMessage[Pair[K, V], NotUsed], RotationMessage, NotUsed] =
+    sequenceWithPassThrough[K, V, NotUsed](fs, syncStrategy, rotationStrategy, settings, classK, classV)
+      .collect(ScalaHdfsFlow.OnlyRotationMessage)
+
+  /**
+   * Java API: creates a flow for [[org.apache.hadoop.io.SequenceFile.Writer]]
+   * with a compression
+   *
+   * @param fs Hadoop file system
+   * @param syncStrategy sync strategy
+   * @param rotationStrategy rotation strategy
+   * @param compressionType a compression type used to compress key/value pairs in the SequenceFile
+   * @param compressionCodec a streaming compression/decompression pair
+   * @param settings hdfs writing settings
+   * @param classK a key class
+   * @param classV a value class
+   */
+  def sequence[K <: Writable, V <: Writable](
+      fs: FileSystem,
+      syncStrategy: SyncStrategy,
+      rotationStrategy: RotationStrategy,
+      compressionType: CompressionType,
+      compressionCodec: CompressionCodec,
+      settings: HdfsWritingSettings,
+      classK: Class[K],
+      classV: Class[V]
+  ): javadsl.Flow[HdfsWriteMessage[Pair[K, V], NotUsed], RotationMessage, NotUsed] =
+    sequenceWithPassThrough[K, V, NotUsed](
+      fs,
+      syncStrategy,
+      rotationStrategy,
+      compressionType,
+      compressionCodec,
+      settings,
+      classK,
+      classV
+    ).collect(ScalaHdfsFlow.OnlyRotationMessage)
+
+  /**
+   * Java API: creates a flow for [[org.apache.hadoop.io.SequenceFile.Writer]]
+   * with `passThrough` of type `C` and without a compression
+   *
+   * @param fs Hadoop file system
+   * @param syncStrategy sync strategy
+   * @param rotationStrategy rotation strategy
+   * @param settings hdfs writing settings
+   * @param classK a key class
+   * @param classV a value class
+   */
+  def sequenceWithPassThrough[K <: Writable, V <: Writable, P](
+      fs: FileSystem,
+      syncStrategy: SyncStrategy,
+      rotationStrategy: RotationStrategy,
+      settings: HdfsWritingSettings,
+      classK: Class[K],
+      classV: Class[V]
+  ): javadsl.Flow[HdfsWriteMessage[Pair[K, V], P], OutgoingMessage[P], NotUsed] =
+    Flow[HdfsWriteMessage[Pair[K, V], P]]
+      .map(message => message.copy(source = message.source.toScala))
+      .via(
+        ScalaHdfsFlow
+          .sequenceWithPassThrough[K, V, P](
+            fs,
+            syncStrategy,
+            rotationStrategy,
+            settings,
+            classK,
+            classV
+          )
+      )
+      .asJava
+
+  /**
+   * Java API: creates a flow for [[org.apache.hadoop.io.SequenceFile.Writer]]
+   * with `passThrough` of type `C` and a compression
+   *
+   * @param fs Hadoop file system
+   * @param syncStrategy sync strategy
+   * @param rotationStrategy rotation strategy
+   * @param compressionType a compression type used to compress key/value pairs in the SequenceFile
+   * @param compressionCodec a streaming compression/decompression pair
+   * @param settings hdfs writing settings
+   * @param classK a key class
+   * @param classV a value class
+   */
+  def sequenceWithPassThrough[K <: Writable, V <: Writable, P](
+      fs: FileSystem,
+      syncStrategy: SyncStrategy,
+      rotationStrategy: RotationStrategy,
+      compressionType: CompressionType,
+      compressionCodec: CompressionCodec,
+      settings: HdfsWritingSettings,
+      classK: Class[K],
+      classV: Class[V]
+  ): javadsl.Flow[HdfsWriteMessage[Pair[K, V], P], OutgoingMessage[P], NotUsed] =
+    Flow[HdfsWriteMessage[Pair[K, V], P]]
+      .map(message => message.copy(source = message.source.toScala))
+      .via(
+        ScalaHdfsFlow
+          .sequenceWithPassThrough[K, V, P](
+            fs,
+            syncStrategy,
+            rotationStrategy,
+            compressionType,
+            compressionCodec,
+            settings,
+            classK,
+            classV
+          )
+      )
+      .asJava
+
+}
