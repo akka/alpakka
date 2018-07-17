fd2645d082a144c6708067fcfbb8dda10b4de6ad
Gabriel Francisco <gabfssilva@gmail.com>
2018-07-17 17:56:29 -0300

    Add support for using PublishRequest to publish messages to SNS

--- sns/src/main/scala/akka/stream/alpakka/sns/scaladsl/SnsPublisher.scala
+++ sns/src/main/scala/akka/stream/alpakka/sns/scaladsl/SnsPublisher.scala
@@ -8,7 +8,7 @@ import akka.stream.alpakka.sns.SnsPublishFlowStage
 import akka.stream.scaladsl.{Flow, Keep, Sink}
 import akka.{Done, NotUsed}
 import com.amazonaws.services.sns.AmazonSNSAsync
-import com.amazonaws.services.sns.model.PublishResult
+import com.amazonaws.services.sns.model.{PublishRequest, PublishResult}
 
 import scala.concurrent.Future
 
@@ -18,6 +18,14 @@ object SnsPublisher {
    * Scala API: creates a [[akka.stream.scaladsl.Flow Flow]] to publish messages to a SNS topic using an [[com.amazonaws.services.sns.AmazonSNSAsync AmazonSNSAsync]]
    */
   def flow(topicArn: String)(implicit snsClient: AmazonSNSAsync): Flow[String, PublishResult, NotUsed] =
+    Flow
+      .fromFunction((message: String) => new PublishRequest(topicArn, message))
+      .via(publishFlow(topicArn))
+
+  /**
+   * Scala API: creates a [[akka.stream.scaladsl.Flow Flow]] to publish messages to a SNS topic using an [[com.amazonaws.services.sns.AmazonSNSAsync AmazonSNSAsync]]
+   */
+  def publishFlow(topicArn: String)(implicit snsClient: AmazonSNSAsync): Flow[PublishRequest, PublishResult, NotUsed] =
     Flow.fromGraph(new SnsPublishFlowStage(topicArn, snsClient))
 
   /**
@@ -26,4 +34,9 @@ object SnsPublisher {
   def sink(topicArn: String)(implicit snsClient: AmazonSNSAsync): Sink[String, Future[Done]] =
     flow(topicArn).toMat(Sink.ignore)(Keep.right)
 
+  /**
+   * Scala API: creates a [[akka.stream.scaladsl.Sink Sink]] to publish messages to a SNS topic using an [[com.amazonaws.services.sns.AmazonSNSAsync AmazonSNSAsync]]
+   */
+  def publishSink(topicArn: String)(implicit snsClient: AmazonSNSAsync): Sink[PublishRequest, Future[Done]] =
+    publishFlow(topicArn).toMat(Sink.ignore)(Keep.right)
 }
