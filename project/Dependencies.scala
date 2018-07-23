1f9c5b5df5edbf063e56f0e911a34f8cf4315da0
Naoki Takezoe <takezoe@gmail.com>
2018-07-23 21:19:31 +0900

    Upgrade libraries (#1119)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,7 +10,7 @@ object Dependencies {
     case _ => "2.5.13"
   }
 
-  val AwsSdkVersion = "1.11.342"
+  val AwsSdkVersion = "1.11.371"
   val AkkaHttpVersion = "10.1.3"
 
   val Common = Seq(
@@ -28,26 +28,26 @@ object Dependencies {
 
   val Amqp = Seq(
     libraryDependencies ++= Seq(
-      "com.rabbitmq" % "amqp-client" % "5.1.1" // APLv2
+      "com.rabbitmq" % "amqp-client" % "5.3.0" // APLv2
     )
   )
 
   val AwsLambda = Seq(
     libraryDependencies ++= Seq(
       "com.amazonaws" % "aws-java-sdk-lambda" % AwsSdkVersion, // ApacheV2
-      "org.mockito" % "mockito-core" % "2.7.17" % Test // MIT
+      "org.mockito" % "mockito-core" % "2.19.1" % Test // MIT
     )
   )
 
   val AzureStorageQueue = Seq(
     libraryDependencies ++= Seq(
-      "com.microsoft.azure" % "azure-storage" % "5.0.0" // ApacheV2
+      "com.microsoft.azure" % "azure-storage" % "8.0.0" // ApacheV2
     )
   )
 
   val Cassandra = Seq(
     libraryDependencies ++= Seq(
-      "com.datastax.cassandra" % "cassandra-driver-core" % "3.3.0" // ApacheV2
+      "com.datastax.cassandra" % "cassandra-driver-core" % "3.5.1" // ApacheV2
     )
   )
 
@@ -64,17 +64,17 @@ object Dependencies {
       "com.typesafe.akka" %% "akka-http-xml" % AkkaHttpVersion,
       "com.typesafe.akka" %% "akka-http-spray-json" % AkkaHttpVersion,
       // https://github.com/akka/alpakka-kafka/releases
-      "com.typesafe.akka" %% "akka-stream-kafka" % "0.21",
+      "com.typesafe.akka" %% "akka-stream-kafka" % "0.22",
       // https://github.com/manub/scalatest-embedded-kafka/tags
-      "net.manub" %% "scalatest-embedded-kafka" % "1.0.0", // MIT
+      "net.manub" %% "scalatest-embedded-kafka" % "1.1.0", // MIT
       // https://github.com/javaee/javax.jms
       "javax.jms" % "jms" % "1.1", // CDDL Version 1.1
       // http://activemq.apache.org/download.html
-      "org.apache.activemq" % "activemq-all" % "5.14.4" exclude ("log4j", "log4j") exclude ("org.slf4j", "slf4j-log4j12"), // ApacheV2
-      "com.h2database" % "h2" % "1.4.196", // Eclipse Public License 1.0
-      "org.elasticsearch.client" % "elasticsearch-rest-client" % "6.2.2", // ApacheV2
-      "org.codelibs" % "elasticsearch-cluster-runner" % "6.2.2.0", // ApacheV2
-      "io.netty" % "netty-all" % "4.1.16.Final", // ApacheV2
+      "org.apache.activemq" % "activemq-all" % "5.15.4" exclude ("log4j", "log4j") exclude ("org.slf4j", "slf4j-log4j12"), // ApacheV2
+      "com.h2database" % "h2" % "1.4.197", // Eclipse Public License 1.0
+      "org.elasticsearch.client" % "elasticsearch-rest-client" % "6.3.1", // ApacheV2
+      "org.codelibs" % "elasticsearch-cluster-runner" % "6.3.1.0", // ApacheV2
+      "io.netty" % "netty-all" % "4.1.27.Final", // ApacheV2
       "com.fasterxml.jackson.datatype" % "jackson-datatype-jdk8" % "2.9.6",
       "com.fasterxml.jackson.datatype" % "jackson-datatype-jsr310" % "2.9.6",
       "org.slf4j" % "log4j-over-slf4j" % "1.7.25",
@@ -92,10 +92,10 @@ object Dependencies {
 
   val Elasticsearch = Seq(
     libraryDependencies ++= Seq(
-      "org.elasticsearch.client" % "rest" % "5.5.3", // ApacheV2
-      "io.spray" %% "spray-json" % "1.3.3", // ApacheV2
+      "org.elasticsearch.client" % "elasticsearch-rest-client" % "6.3.1", // ApacheV2
+      "io.spray" %% "spray-json" % "1.3.4", // ApacheV2
       "com.fasterxml.jackson.core" % "jackson-databind" % "2.9.6", // ApacheV2
-      "org.codelibs" % "elasticsearch-cluster-runner" % "5.6.0.0" % Test // ApacheV2
+      "org.codelibs" % "elasticsearch-cluster-runner" % "6.3.1.0" % Test // ApacheV2
     )
   )
 
@@ -108,10 +108,10 @@ object Dependencies {
   val Ftp = Seq(
     libraryDependencies ++= Seq(
       "commons-net" % "commons-net" % "3.6", // ApacheV2
-      "com.hierynomus" % "sshj" % "0.23.0", // ApacheV2
+      "com.hierynomus" % "sshj" % "0.24.0", // ApacheV2
       "org.apache.ftpserver" % "ftpserver-core" % "1.1.1" % Test, // ApacheV2
-      "org.apache.sshd" % "sshd-core" % "1.6.0" % Test, // ApacheV2
-      "net.i2p.crypto" % "eddsa" % "0.2.0" % Test, // CC0 1.0 Universal
+      "org.apache.sshd" % "sshd-core" % "1.7.0" % Test, // ApacheV2
+      "net.i2p.crypto" % "eddsa" % "0.3.0" % Test, // CC0 1.0 Universal
       "com.google.jimfs" % "jimfs" % "1.1" % Test // ApacheV2
     )
   )
@@ -134,8 +134,8 @@ object Dependencies {
     libraryDependencies ++= Seq(
       "com.typesafe.akka" %% "akka-http" % AkkaHttpVersion,
       "com.typesafe.akka" %% "akka-http-spray-json" % AkkaHttpVersion,
-      "org.mockito" % "mockito-core" % "2.3.7" % Test, // MIT
-      "com.github.tomakehurst" % "wiremock" % "2.5.1" % Test // ApacheV2
+      "org.mockito" % "mockito-core" % "2.19.1" % Test, // MIT
+      "com.github.tomakehurst" % "wiremock" % "2.18.0" % Test // ApacheV2
     )
   )
 
@@ -143,21 +143,21 @@ object Dependencies {
     libraryDependencies ++= Seq(
       "com.typesafe.akka" %% "akka-http" % AkkaHttpVersion,
       "com.typesafe.akka" %% "akka-http-spray-json" % AkkaHttpVersion,
-      "com.pauldijou" %% "jwt-core" % "0.14.1", //ApacheV2
-      "org.mockito" % "mockito-core" % "2.7.22" % Test //ApacheV2
+      "com.pauldijou" %% "jwt-core" % "0.16.0", //ApacheV2
+      "org.mockito" % "mockito-core" % "2.19.1" % Test // MIT
     )
   )
 
   val HBase = {
-    val hbaseVersion = "1.2.4"
-    val hadoopVersion = "2.5.1"
+    val hbaseVersion = "1.2.6.1"
+    val hadoopVersion = "2.5.2"
     Seq(
       libraryDependencies ++= Seq(
         "org.apache.hbase" % "hbase-client" % hbaseVersion exclude ("log4j", "log4j") exclude ("org.slf4j", "slf4j-log4j12"), // ApacheV2,
         "org.apache.hbase" % "hbase-common" % hbaseVersion exclude ("log4j", "log4j") exclude ("org.slf4j", "slf4j-log4j12"), // ApacheV2,
         "org.apache.hadoop" % "hadoop-common" % hadoopVersion exclude ("log4j", "log4j") exclude ("org.slf4j", "slf4j-log4j12"), // ApacheV2,
         "org.apache.hadoop" % "hadoop-mapreduce-client-core" % hadoopVersion exclude ("log4j", "log4j") exclude ("org.slf4j", "slf4j-log4j12"), // ApacheV2,
-        "org.slf4j" % "log4j-over-slf4j" % "1.7.21" % Test // MIT like: http://www.slf4j.org/license.html
+        "org.slf4j" % "log4j-over-slf4j" % "1.7.25" % Test // MIT like: http://www.slf4j.org/license.html
       )
     )
   }
@@ -181,43 +181,43 @@ object Dependencies {
   val IronMq = Seq(
     libraryDependencies ++= Seq(
       "com.typesafe.akka" %% "akka-http" % AkkaHttpVersion,
-      "de.heikoseeberger" %% "akka-http-circe" % "1.20.1" // ApacheV2
+      "de.heikoseeberger" %% "akka-http-circe" % "1.21.0" // ApacheV2
     )
   )
 
   val Jms = Seq(
     libraryDependencies ++= Seq(
       "javax.jms" % "jms" % "1.1" % Provided, // CDDL + GPLv2
-      "org.apache.activemq" % "activemq-broker" % "5.14.1" % Test, // ApacheV2
-      "org.apache.activemq" % "activemq-client" % "5.14.1" % Test // ApacheV2
+      "org.apache.activemq" % "activemq-broker" % "5.15.4" % Test, // ApacheV2
+      "org.apache.activemq" % "activemq-client" % "5.15.4" % Test // ApacheV2
     ),
     resolvers += ("jboss" at "https://repository.jboss.org/nexus/content/groups/public")
   )
 
   val JsonStreaming = Seq(
     libraryDependencies ++= Seq(
-      "com.github.jsurfer" % "jsurfer" % "1.4.2", // MIT,
-      "com.github.jsurfer" % "jsurfer-jackson" % "1.4.2" // MIT
+      "com.github.jsurfer" % "jsurfer" % "1.4.3", // MIT,
+      "com.github.jsurfer" % "jsurfer-jackson" % "1.4.3" // MIT
     )
   )
 
   val Kinesis = Seq(
     libraryDependencies ++= Seq(
       "com.amazonaws" % "aws-java-sdk-kinesis" % AwsSdkVersion, // ApacheV2
-      "org.mockito" % "mockito-core" % "2.7.11" % Test // MIT
+      "org.mockito" % "mockito-core" % "2.19.1" % Test // MIT
     )
   )
 
   val Kudu = Seq(
     libraryDependencies ++= Seq(
-      "org.apache.kudu" % "kudu-client-tools" % "1.7.0", // ApacheV2
-      "org.apache.kudu" % "kudu-client" % "1.7.0" % Test // ApacheV2
+      "org.apache.kudu" % "kudu-client-tools" % "1.7.1", // ApacheV2
+      "org.apache.kudu" % "kudu-client" % "1.7.1" % Test // ApacheV2
     )
   )
 
   val MongoDb = Seq(
     libraryDependencies ++= Seq(
-      "org.mongodb.scala" %% "mongo-scala-driver" % "2.3.0" // ApacheV2
+      "org.mongodb.scala" %% "mongo-scala-driver" % "2.4.0" // ApacheV2
     )
   )
 
@@ -229,8 +229,8 @@ object Dependencies {
 
   val OrientDB = Seq(
     libraryDependencies ++= Seq(
-      "com.orientechnologies" % "orientdb-graphdb" % "2.2.30", // ApacheV2
-      "com.orientechnologies" % "orientdb-object" % "2.2.30" // ApacheV2
+      "com.orientechnologies" % "orientdb-graphdb" % "3.0.4", // ApacheV2
+      "com.orientechnologies" % "orientdb-object" % "3.0.4" // ApacheV2
     )
   )
 
@@ -247,7 +247,7 @@ object Dependencies {
       "com.amazonaws" % "aws-java-sdk-core" % AwsSdkVersion, // ApacheV2
       // in-memory filesystem for file related tests
       "com.google.jimfs" % "jimfs" % "1.1" % Test, // ApacheV2
-      "com.github.tomakehurst" % "wiremock" % "2.5.1" % Test // ApacheV2
+      "com.github.tomakehurst" % "wiremock" % "2.18.0" % Test // ApacheV2
     )
   )
 
@@ -268,7 +268,7 @@ object Dependencies {
     )
   }
 
-  val SlickVersion = "3.2.1"
+  val SlickVersion = "3.2.3"
   val Slick = Seq(
     libraryDependencies ++= Seq(
       "com.typesafe.slick" %% "slick" % SlickVersion, // BSD 2-clause "Simplified" License
@@ -280,12 +280,12 @@ object Dependencies {
   val Sns = Seq(
     libraryDependencies ++= Seq(
       "com.amazonaws" % "aws-java-sdk-sns" % AwsSdkVersion, // ApacheV2
-      "org.mockito" % "mockito-core" % "2.12.0" % Test // MIT
+      "org.mockito" % "mockito-core" % "2.19.1" % Test // MIT
     )
   )
 
   val Solr = {
-    val solrjVersion = "7.2.0"
+    val solrjVersion = "7.4.0"
     val slf4jVersion = "1.7.25"
     Seq(
       libraryDependencies ++= Seq(
@@ -301,13 +301,13 @@ object Dependencies {
   val Sqs = Seq(
     libraryDependencies ++= Seq(
       "com.amazonaws" % "aws-java-sdk-sqs" % AwsSdkVersion, // ApacheV2
-      "org.elasticmq" %% "elasticmq-rest-sqs" % "0.13.11" % Test excludeAll (
+      "org.elasticmq" %% "elasticmq-rest-sqs" % "0.14.1" % Test excludeAll (
         // elasticmq-rest-sqs depends on Akka 2.5, exclude it, so we can choose Akka version
         ExclusionRule(organization = "com.typesafe.akka") //
       ), // ApacheV2
       // pull up akka-http version to the latest version for elasticmq-rest-sqs
       "com.typesafe.akka" %% "akka-http" % AkkaHttpVersion % Test, // ApacheV2
-      "org.mockito" % "mockito-core" % "2.18.3" % Test // MIT
+      "org.mockito" % "mockito-core" % "2.19.1" % Test // MIT
     )
   )
 
