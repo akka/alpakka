07f8fe21789064d996fa6ae7f9bc3f3225c8295c
dannylesnik <danny.lesnik@gmail.com>
2018-08-16 23:08:29 +0300

    Fixing Build. Adding missing configuration to build.sbt

--- avroparquet/src/main/scala/akka/stream/alpakka/avroparquet/javadsl/AvroParquetSink.scala
+++ avroparquet/src/main/scala/akka/stream/alpakka/avroparquet/javadsl/AvroParquetSink.scala
@@ -14,7 +14,8 @@ import org.apache.parquet.hadoop.ParquetWriter
 object AvroParquetSink {
 
   def create(writer: ParquetWriter[GenericRecord]): Sink[GenericRecord, CompletionStage[Done]] =
-    Flow.fromGraph(new AvroParquetFlow(writer: ParquetWriter[GenericRecord]))
+    Flow
+      .fromGraph(new AvroParquetFlow(writer: ParquetWriter[GenericRecord]))
       .toMat(Sink.ignore(), Keep.right[NotUsed, CompletionStage[Done]])
 
 }
