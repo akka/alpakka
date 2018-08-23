e2912789fc11d2b4645bceaaf67686758671d223
Andreas Schroeder <andreas.sa.schroeder@bmw.de>
2018-08-23 20:20:53 +0200

    Add scaladocs to exception marker traits
    In order to explain the intent, added some brief comments to the marker
    traits.

--- jms/src/main/scala/akka/stream/alpakka/jms/Exceptions.scala
+++ jms/src/main/scala/akka/stream/alpakka/jms/Exceptions.scala
@@ -4,32 +4,38 @@
 
 package akka.stream.alpakka.jms
 
-trait RecoverableException extends Exception
+/**
+ * Marker trait indicating that the exception thrown is intermittent. The failed operation might succeed if tried again.
+ */
+trait RetriableException extends Exception
 
-trait NonRecoverableException extends Exception
+/**
+ * Marker trait indicating that the exception thrown is persistent. The operation will always fail when retried.
+ */
+trait NonRetriableException extends Exception
 
 case class UnsupportedMessagePropertyType(propertyName: String, propertyValue: Any, message: JmsMessage)
     extends Exception(
       s"Jms property '$propertyName' has unknown type '${propertyValue.getClass.getName}'. " +
       "Only primitive types and String are supported as property values."
     )
-    with NonRecoverableException
+    with NonRetriableException
 
 case class NullMessageProperty(propertyName: String, message: JmsMessage)
     extends Exception(
       s"null value was given for Jms property '$propertyName'."
     )
-    with NonRecoverableException
+    with NonRetriableException
 
 case class UnsupportedMapMessageEntryType(entryName: String, entryValue: Any, message: JmsMapMessage)
     extends Exception(
       s"Jms MapMessage entry '$entryName' has unknown type '${entryValue.getClass.getName}'. " +
       "Only primitive types, String, and Byte array are supported as entry values."
     )
-    with NonRecoverableException
+    with NonRetriableException
 
 case class NullMapMessageEntry(entryName: String, message: JmsMapMessage)
     extends Exception(
       s"null value was given for Jms MapMessage entry '$entryName'."
     )
-    with NonRecoverableException
+    with NonRetriableException
