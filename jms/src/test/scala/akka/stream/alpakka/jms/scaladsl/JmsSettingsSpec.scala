54ffa1c5a1b802d1a99441bb6ba3289e5069f3d5
Andreas Schroeder <andreas.sa.schroeder@bmw.de>
2018-08-27 07:11:20 +0200

    Avoid case class constructor in docs
    The contributor advice states that case classes should not be used
    as settings objects. Therefore, this commit changes the producer
    config docs example from using the case class constructor to using
    the withXxx methods.

--- jms/src/test/scala/akka/stream/alpakka/jms/scaladsl/JmsSettingsSpec.scala
+++ jms/src/test/scala/akka/stream/alpakka/jms/scaladsl/JmsSettingsSpec.scala
@@ -5,7 +5,7 @@
 package akka.stream.alpakka.jms.scaladsl
 import java.time.Duration
 
-import akka.stream.alpakka.jms.{Credentials, JmsProducerSettings, JmsSpec, Topic}
+import akka.stream.alpakka.jms.{Credentials, JmsProducerSettings, JmsSpec}
 import org.apache.activemq.ActiveMQConnectionFactory
 
 class JmsSettingsSpec extends JmsSpec {
@@ -13,13 +13,11 @@ class JmsSettingsSpec extends JmsSpec {
   "Jms producer" should {
     "have producer settings" in {
       //#producer-settings
-      val settings = JmsProducerSettings(
-        new ActiveMQConnectionFactory("broker-url"),
-        destination = Some(Topic("target-topic")),
-        credentials = Some(Credentials("username", "password")),
-        sessionCount = 10,
-        timeToLive = Some(Duration.ofHours(1))
-      )
+      val settings = JmsProducerSettings(new ActiveMQConnectionFactory("broker-url"))
+        .withTopic("target-topic")
+        .withCredential(Credentials("username", "password"))
+        .withSessionCount(10)
+        .withTimeToLive(Duration.ofHours(1))
       //#producer-settings
     }
   }
