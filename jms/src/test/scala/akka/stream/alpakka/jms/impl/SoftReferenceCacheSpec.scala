ec1260f3ee23a238fb9b7851ab2a28dcdae1bf95
Andreas Schroeder <andreas.sa.schroeder@bmw.de>
2018-09-02 15:10:48 +0200

    Improve runtime of SoftReferenceCacheSpec

--- jms/src/test/scala/akka/stream/alpakka/jms/impl/SoftReferenceCacheSpec.scala
+++ jms/src/test/scala/akka/stream/alpakka/jms/impl/SoftReferenceCacheSpec.scala
@@ -20,38 +20,48 @@ class SoftReferenceCacheSpec extends WordSpec with Matchers {
     }
 
     "return previous value on hit" in {
+      System.gc() // if memory pressure exists, reduce it now.
       val cache = new SoftReferenceCache[Int, String]
       cache.lookup(1, "one")
       cache.lookup(1, "two") shouldBe "one"
+    }
+
+    "not evaluate default value on hit" in {
+      System.gc() // if memory pressure exists, reduce it now.
+      val cache = new SoftReferenceCache[Int, String]
+      cache.lookup(1, "one")
+      cache.lookup(1, throw new RuntimeException("Should not be evaluated")) shouldBe "one"
     }
 
     "remove entries on garbage collection" in {
       val cache = new SoftReferenceCache[Int, Array[Byte]]
 
       val deadline = System.currentTimeMillis() + 1.minute.toMillis // try for 1 minute.
-      var i = 2
+      var i = 1
 
-      def fill(key: Int): Unit = cache.lookup(key, Array.fill(40 * 1024 * 1024)(1.toByte))
-
-      def fill100(): Unit = for (_ <- 1 to 100) {
-        fill(i)
+      def addCacheEntries(): Unit = for (_ <- 1 to 40) {
+        val array = new Array[Byte](1024 * 1024)
+        cache.lookup(i, array)
         i += 1
       }
 
-      fill(1) // fill with old value, whose length is not 1024.
       val newValue = Array.fill(1024)(1.toByte)
-      // we try to get value for key 1 collected by gc.
-      // while the entry for key 1 is the old one, add entries to the cache,
-      // and ask the JVM for a gc (pretty please).
-      while (cache.lookup(1, newValue).length != 1024 && System.currentTimeMillis() < deadline) {
-        fill100()
+
+      // detect eviction by inserting a different value into the cache for a previously set key.
+      def entryEvicted(index: Int): Boolean = cache.lookup(index, newValue).length == 1024
+
+      def noEntryEvicted: Boolean = !(1 until i).exists(entryEvicted)
+
+      while (noEntryEvicted && System.currentTimeMillis() < deadline) {
+        addCacheEntries()
         System.gc()
       }
 
-      cache.lookup(1, newValue).length shouldBe 1024
+      noEntryEvicted shouldBe false
     }
 
     "makes entries visible across threads" in {
+      System.gc() // if memory pressure exists, reduce it now.
       val cache = new SoftReferenceCache[Int, String]
       val ex = Executors.newFixedThreadPool(32)
       implicit val ec: ExecutionContextExecutor = ExecutionContext.fromExecutor(ex)
