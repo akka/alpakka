b3b8a89eb0f24ed93a019ca0e19ef50f329d28f7
Enno <458526+ennru@users.noreply.github.com>
2018-09-03 12:29:04 +0200

    Move tests to docs pacakges

--- kudu/src/main/scala/akka/stream/alpakka/kudu/scaladsl/KuduTable.scala
+++ kudu/src/main/scala/akka/stream/alpakka/kudu/scaladsl/KuduTable.scala
@@ -12,11 +12,20 @@ import org.apache.kudu.client.KuduClient
 
 import scala.concurrent.Future
 
+/**
+ * Scala API
+ */
 object KuduTable {
 
+  /**
+   * Create a Sink writing elements to a Kudu table.
+   */
   def sink[A](settings: KuduTableSettings[A])(implicit kuduClient: KuduClient): Sink[A, Future[Done]] =
     flow(settings)(kuduClient).toMat(Sink.ignore)(Keep.right)
 
+  /**
+   * Create a Flow writing elements to a Kudu table.
+   */
   def flow[A](settings: KuduTableSettings[A])(implicit kuduClient: KuduClient): Flow[A, A, NotUsed] =
     Flow.fromGraph(new KuduFlowStage[A](settings, kuduClient))
 
