fc22e2899fe6b42b7a6cfd8485a07939fbc968c2
Enno <458526+ennru@users.noreply.github.com>
2018-09-04 11:24:51 +0200

    Un-case-classed MqttMessage (API CHANGE)

--- mqtt/src/main/scala/akka/stream/alpakka/mqtt/model.scala
+++ mqtt/src/main/scala/akka/stream/alpakka/mqtt/model.scala
@@ -4,33 +4,74 @@
 
 package akka.stream.alpakka.mqtt
 
-import akka.util.ByteString
+import scala.compat.java8.OptionConverters._
 
-final case class MqttMessage(topic: String, payload: ByteString, qos: Option[MqttQoS] = None, retained: Boolean = false)
+final class MqttMessage private (val topic: String,
+                                 val payload: akka.util.ByteString,
+                                 val qos: Option[MqttQoS],
+                                 val retained: Boolean) {
+
+  /** Java API */
+  def getTopic: String = topic
+
+  /** Java API */
+  def getPayload: akka.util.ByteString = payload
+
+  /** Java API */
+  def getQos: java.util.Optional[MqttQoS] = qos.asJava
+
+  /** Java API */
+  def isRetained: Boolean = retained
+
+  def withTopic(value: String): MqttMessage = copy(topic = value)
+  def withPayload(value: akka.util.ByteString): MqttMessage = copy(payload = value)
+  def withPayload(value: Array[Byte]): MqttMessage = copy(payload = akka.util.ByteString(value))
+  def withQos(value: MqttQoS): MqttMessage = copy(qos = Option(value))
+  def withRetained(value: Boolean): MqttMessage = if (retained == value) this else copy(retained = value)
+
+  private def copy(topic: String = topic,
+                   payload: akka.util.ByteString = payload,
+                   qos: Option[MqttQoS] = qos,
+                   retained: Boolean = retained): MqttMessage =
+    new MqttMessage(topic = topic, payload = payload, qos = qos, retained = retained)
+
+  override def toString =
+    s"""MqttMessage(topic=$topic,payload=$payload,qos=$qos,retained=$retained)"""
+
+  override def equals(other: Any): Boolean = other match {
+    case that: MqttMessage =>
+      java.util.Objects.equals(this.topic, that.topic) &&
+      java.util.Objects.equals(this.payload, that.payload) &&
+      java.util.Objects.equals(this.qos, that.qos) &&
+      java.util.Objects.equals(this.retained, that.retained)
+    case _ => false
+  }
+
+  override def hashCode(): Int =
+    java.util.Objects.hash(topic, payload, qos, Boolean.box(retained))
+}
 
 object MqttMessage {
 
-  /**
-   * Java API: create  [[MqttMessage]]
-   */
-  def create(topic: String, payload: ByteString) =
-    MqttMessage(topic, payload)
+  /** Scala API */
+  def apply(
+      topic: String,
+      payload: akka.util.ByteString,
+  ): MqttMessage = new MqttMessage(
+    topic,
+    payload,
+    qos = None,
+    retained = false
+  )
 
-  /**
-   * Java API: create  [[MqttMessage]]
-   */
-  def create(topic: String, payload: ByteString, qos: MqttQoS) =
-    MqttMessage(topic, payload, Some(qos))
-
-  /**
-   * Java API: create  [[MqttMessage]]
-   */
-  def create(topic: String, payload: ByteString, retained: Boolean) =
-    MqttMessage(topic, payload, retained = retained)
-
-  /**
-   * Java API: create  [[MqttMessage]]
-   */
-  def create(topic: String, payload: ByteString, qos: MqttQoS, retained: Boolean) =
-    MqttMessage(topic, payload, Some(qos), retained = retained)
+  /** Java API */
+  def create(
+      topic: String,
+      payload: akka.util.ByteString,
+  ): MqttMessage = new MqttMessage(
+    topic,
+    payload,
+    qos = None,
+    retained = false
+  )
 }
