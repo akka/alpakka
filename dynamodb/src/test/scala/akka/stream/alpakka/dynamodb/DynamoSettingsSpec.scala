593a3431fcb76b08baf700dfa8a636d286c6439c
Enno <458526+ennru@users.noreply.github.com>
2018-09-11 12:48:29 +0200

    Changed DynamoSettings to non-case class; reviewed tests and docs

--- dynamodb/src/test/scala/akka/stream/alpakka/dynamodb/DynamoSettingsSpec.scala
+++ dynamodb/src/test/scala/akka/stream/alpakka/dynamodb/DynamoSettingsSpec.scala
@@ -5,13 +5,38 @@
 package akka.stream.alpakka.dynamodb
 
 import akka.actor.ActorSystem
-import com.amazonaws.auth.DefaultAWSCredentialsProviderChain
+import com.amazonaws.auth.{
+  AWSCredentialsProvider,
+  AWSStaticCredentialsProvider,
+  BasicAWSCredentials,
+  DefaultAWSCredentialsProviderChain
+}
 import com.typesafe.config.ConfigFactory
 import org.scalatest.{Matchers, WordSpecLike}
+
+import scala.concurrent.Await
+import scala.concurrent.duration._
 
 class DynamoSettingsSpec extends WordSpecLike with Matchers {
 
   "DynamoSettings" should {
+    "create settings" in {
+      // #credentials-provider
+      val provider: AWSCredentialsProvider = // ???
+        // #credentials-provider
+        new AWSStaticCredentialsProvider(new BasicAWSCredentials("accessKey", "secretKey"))
+      // #credentials-provider
+      val settings = DynamoSettings("eu-west-1", "host")
+        .withCredentialsProvider(provider)
+      // #credentials-provider
+
+      settings.region should be("eu-west-1")
+      settings.host should be("host")
+      settings.port should be(443)
+      settings.parallelism should be(4)
+      settings.credentialsProvider shouldBe a[AWSStaticCredentialsProvider]
+    }
+
     "read the reference config from an actor system" in {
       val system = ActorSystem()
       val settings = DynamoSettings(system)
@@ -21,13 +46,14 @@ class DynamoSettingsSpec extends WordSpecLike with Matchers {
       settings.port should be(8001)
       settings.parallelism should be(4)
       settings.credentialsProvider shouldBe a[DefaultAWSCredentialsProviderChain]
+      Await.result(system.terminate(), 1.second)
     }
 
     "use the DefaultAWSCredentialsProviderChain if the config defines an incomplete akka.stream.alpakka.dynamodb.credentials" in {
       val config = ConfigFactory.parseString("""
           |region = "eu-west-1"
           |host = "localhost"
-          |port: 443
+          |port = 443
           |parallelism = 32
           |credentials {
           |  access-key-id = "dummy-access-key"
@@ -40,19 +66,19 @@ class DynamoSettingsSpec extends WordSpecLike with Matchers {
 
     "read static aws credentials from a config that defines an akka.stream.alpakka.dynamodb.credentials" in {
       val configWithStaticCredentials =
-        // #static-creds
         """
-          | akka.stream.alpakka.dynamodb {
-          |  region = "eu-west-1"
-          |  host = "localhost"
-          |  port: 443
-          |  parallelism = 32
-          |  credentials {
-          |    access-key-id = "dummy-access-key"
-          |    secret-key-id = "dummy-secret-key"
-          |  }
-          |}""".stripMargin
+        // #static-creds
+           akka.stream.alpakka.dynamodb {
+            region = "eu-west-1"
+            host = "localhost"
+            port = 443
+            parallelism = 32
+            credentials {
+              access-key-id = "dummy-access-key"
+              secret-key-id = "dummy-secret-key"
+            }
       // #static-creds
+          }""".stripMargin
 
       val config = ConfigFactory.parseString(configWithStaticCredentials).getConfig("akka.stream.alpakka.dynamodb")
 
--- dynamodb/src/test/scala/docs/scaladsl/ExampleSpec.scala
+++ dynamodb/src/test/scala/docs/scaladsl/ExampleSpec.scala
@@ -4,121 +4,117 @@
 
 package docs.scaladsl
 
+import java.lang
+
 import akka.NotUsed
 import akka.actor.ActorSystem
 import akka.stream.{ActorMaterializer, Materializer}
 import akka.stream.alpakka.dynamodb.scaladsl._
 import akka.stream.alpakka.dynamodb.{AwsOp, DynamoSettings}
-import akka.stream.scaladsl.Source
+import akka.stream.scaladsl.{Sink, Source}
 import akka.testkit.TestKit
 import com.amazonaws.services.dynamodbv2.model._
+import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpecLike}
 
 import scala.concurrent.duration._
 import scala.concurrent.{Await, Future}
 
-class ExampleSpec extends TestKit(ActorSystem("ExampleSpec")) with WordSpecLike with Matchers with BeforeAndAfterAll {
+class ExampleSpec
+    extends TestKit(ActorSystem("ExampleSpec"))
+    with WordSpecLike
+    with Matchers
+    with BeforeAndAfterAll
+    with ScalaFutures {
 
-  val settings = DynamoSettings(system)
+  implicit val materializer: Materializer = ActorMaterializer()
+
+  var dynamoClient: DynamoClient = _
 
   override def beforeAll() = {
     System.setProperty("aws.accessKeyId", "someKeyId")
     System.setProperty("aws.secretKey", "someSecretKey")
+    val settings = DynamoSettings(system)
+    dynamoClient = DynamoClient(settings)
   }
+
+  override def afterAll(): Unit = shutdown()
 
   "DynamoDB Client" should {
 
     "provide a simple usage example" in {
 
-      //#init-client
+      //#client-construct
       implicit val system: ActorSystem = ActorSystem()
       implicit val materializer: Materializer = ActorMaterializer()
-      //#init-client
 
-      //#client-construct
       val settings = DynamoSettings(system)
-      val client = DynamoClient(settings)
+      val dynamoClient = DynamoClient(settings)
       //#client-construct
 
       //##simple-request
       import DynamoImplicits._
-      val listTablesResult: Future[ListTablesResult] = client.single(new ListTablesRequest())
+      val listTablesResult: Future[ListTablesResult] =
+        dynamoClient.single(new ListTablesRequest())
       //##simple-request
 
       Await.result(listTablesResult, 5.seconds)
 
-      system.terminate()
+      TestKit.shutdownActorSystem(system)
 
     }
 
-    "allow multiple requests - current api" in {
-      implicit val system = ActorSystem()
-      implicit val materializer = ActorMaterializer()
-
-      val settings = DynamoSettings(system)
-      val client = DynamoClient(settings)
-
+    "allow multiple requests - explicit types" in {
       import DynamoImplicits._
-      Source
+      val source = Source
         .single[AwsOp](new CreateTableRequest().withTableName("testTable"))
-        .via(client.flow)
-        .map(_.asInstanceOf[CreateTable#B]) // <-- this is not very intuitive
+        .via(dynamoClient.flow)
+        .map(_.asInstanceOf[CreateTableResult]) // <-- this is not very intuitive
         .map[AwsOp]( // <-- this is required to trigger the following implicit conversion, which takes some time to find out as well
           result => new DescribeTableRequest().withTableName(result.getTableDescription.getTableName)
         )
-        .via(client.flow)
-        .map(_.asInstanceOf[DescribeTable#B])
+        .via(dynamoClient.flow)
+        .map(_.asInstanceOf[DescribeTableResult])
         .map(result => result.getTable.getItemCount)
+      val streamCompletion = source.runWith(Sink.seq)
+      streamCompletion.failed.futureValue shouldBe a[AmazonDynamoDBException]
     }
   }
 
-  "allow multiple requests - proposal" in {
-    implicit val system = ActorSystem()
-    implicit val materializer = ActorMaterializer()
-
-    val settings = DynamoSettings(system)
-    val client = DynamoClient(settings)
-
-    import DynamoImplicits._
+  "allow multiple requests" in {
     //##flow
-    Source
-      .single(new CreateTableRequest().withTableName("testTable").toOp)
-      .via(client.flow)
+    import DynamoImplicits._
+    val source: Source[String, NotUsed] = Source
+      .single(new CreateTableRequest().withTableName("testTable"))
+      .map(_.toOp) // converts to corresponding AwsOp
+      .via(dynamoClient.flow)
       .map(_.getTableDescription.getTableArn)
     //##flow
+    val streamCompletion = source.runWith(Sink.seq)
+    streamCompletion.failed.futureValue shouldBe a[AmazonDynamoDBException]
   }
 
-  "allow multiple requests - proposal - single source" in {
-    implicit val system = ActorSystem()
-    implicit val materializer = ActorMaterializer()
-
-    val settings = DynamoSettings(system)
-    val client = DynamoClient(settings)
-
+  "allow multiple requests - single source" in {
     import DynamoImplicits._
-    client
+    val source: Source[lang.Long, NotUsed] = dynamoClient
       .source(new CreateTableRequest().withTableName("testTable")) // creating a source from a single req is common enough to warrant a utility function
-      .map(result => new DescribeTableRequest().withTableName(result.getTableDescription.getTableName).toOp)
-      .via(client.flow)
+      .map(result => new DescribeTableRequest().withTableName(result.getTableDescription.getTableName))
+      .map(_.toOp)
+      .via(dynamoClient.flow)
       .map(result => result.getTable.getItemCount)
-
+    val streamCompletion = source.runWith(Sink.seq)
+    streamCompletion.failed.futureValue shouldBe a[AmazonDynamoDBException]
   }
 
   "provide a paginated requests example" in {
-    implicit val system = ActorSystem()
-    implicit val materializer = ActorMaterializer()
-
-    val settings = DynamoSettings(system)
-    val client = DynamoClient(settings)
-
     import DynamoImplicits._
 
     //##paginated
     val scanPages: Source[ScanResult, NotUsed] =
-      client.source(new ScanRequest().withTableName("testTable"))
+      dynamoClient.source(new ScanRequest().withTableName("testTable"))
     //##paginated
-
-    system.terminate()
+    val streamCompletion = scanPages.runWith(Sink.seq)
+    streamCompletion.failed.futureValue shouldBe a[AmazonDynamoDBException]
   }
 
 }
