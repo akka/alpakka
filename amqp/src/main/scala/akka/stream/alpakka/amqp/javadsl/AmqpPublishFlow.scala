b7ffa4f5d5486e45594bbc9bbe877688ec01a252
Julien Caré <julien.care@iadvize.com>
2018-11-16 11:52:44 +0100

    fix scala 2.11 build

--- amqp/src/main/scala/akka/stream/alpakka/amqp/javadsl/AmqpPublishFlow.scala
+++ amqp/src/main/scala/akka/stream/alpakka/amqp/javadsl/AmqpPublishFlow.scala
@@ -8,6 +8,7 @@ import java.util.concurrent.CompletionStage
 
 import akka.Done
 import akka.japi.Pair
+import akka.japi.function.Function
 import akka.stream.alpakka.amqp._
 import akka.stream.javadsl.{Flow, Keep}
 import akka.util.ByteString
@@ -24,8 +25,9 @@ object AmqpPublishFlow {
    */
   def createSimple[O](settings: AmqpSinkSettings): Flow[Pair[ByteString, O], O, CompletionStage[Done]] =
     Flow
-      .create[Pair[ByteString, O]]()
-      .map(pair => pair.toScala)
+      .fromFunction(new Function[Pair[ByteString, O], (ByteString, O)] {
+        override def apply(pair: Pair[ByteString, O]): (ByteString, O) = pair.toScala
+      })
       .viaMat[O, CompletionStage[Done], CompletionStage[Done]](
         scaladsl.AmqpPublishFlow
           .simple[O](settings)
@@ -43,8 +45,9 @@ object AmqpPublishFlow {
    */
   def create[O](settings: AmqpSinkSettings): Flow[Pair[OutgoingMessage, O], O, CompletionStage[Done]] =
     Flow
-      .create[Pair[OutgoingMessage, O]]()
-      .map(pair => pair.toScala)
+      .fromFunction(new Function[Pair[OutgoingMessage, O], (OutgoingMessage, O)] {
+        override def apply(pair: Pair[OutgoingMessage, O]): (OutgoingMessage, O) = pair.toScala
+      })
       .viaMat[O, CompletionStage[Done], CompletionStage[Done]](
         scaladsl
           .AmqpPublishFlow[O](settings)
