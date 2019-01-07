3078b53bef1966c336bea752e4a73e891faabb1b
Martynas Mickevičius <self@2m.lt>
2019-01-07 21:20:16 +0700

    Publish docs using rsync

---
+++ project/PublishRsync.scala
@@ -0,0 +1,27 @@
+import com.typesafe.sbt.site.SitePlugin
+import sbt._
+import scala.sys.process._
+
+trait PublishRsyncKeys {
+  val publishRsyncArtifact = taskKey[(File, String)]("File or directory and a path to publish to")
+  val publishRsyncHost = settingKey[String]("hostname to publish to")
+  val publishRsync = taskKey[Int]("Deploy using rsync")
+}
+
+object PublishRsyncPlugin extends AutoPlugin {
+
+  override def requires = SitePlugin
+  override def trigger = noTrigger
+
+  object autoImport extends PublishRsyncKeys
+  import autoImport._
+
+  override def projectSettings = publishRsyncSettings()
+
+  def publishRsyncSettings(): Seq[Setting[_]] = Seq(
+    publishRsync := {
+      val (from, to) = publishRsyncArtifact.value
+      s"rsync --recursive --links --compress --verbose $from/ ${publishRsyncHost.value}:$to"!
+    }
+  )
+}
