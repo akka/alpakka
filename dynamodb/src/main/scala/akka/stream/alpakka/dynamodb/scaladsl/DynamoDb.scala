61fad8efeaad56bf0c21718af8d39b4e85dc73fe
Martynas Mickevičius <self@2m.lt>
2019-01-21 12:48:02 +0100

    Resolve client during materialization

--- dynamodb/src/main/scala/akka/stream/alpakka/dynamodb/scaladsl/DynamoDb.scala
+++ dynamodb/src/main/scala/akka/stream/alpakka/dynamodb/scaladsl/DynamoDb.scala
@@ -5,49 +5,54 @@
 package akka.stream.alpakka.dynamodb.scaladsl
 
 import akka.NotUsed
-import akka.actor.ActorSystem
+import akka.stream.Materializer
+import akka.stream.alpakka.dynamodb.impl.{Paginator, Setup}
 import akka.stream.alpakka.dynamodb.{AwsOp, AwsPagedOp, DynamoClientExt}
-import akka.stream.scaladsl.{Flow, Source}
+import akka.stream.scaladsl.{Flow, Sink, Source}
 
 import scala.concurrent.Future
 
 /**
- * A factory of operations that use managed DynamoClient.
+ * Factory of DynamoDB Akka Stream operators.
  */
 object DynamoDb {
 
   /**
-   * Create a Flow that emits a response for every request.
-   *
-   * @param sys actor system that will be used to resolved managed DynamoClient
+   * Create a Flow that emits a response for every request to DynamoDB.
    */
-  def flow[Op <: AwsOp](implicit sys: ActorSystem): Flow[Op, Op#B, NotUsed] =
-    DynamoDbExternal.flow(DynamoClientExt(sys).dynamoClient)
+  def flow[Op <: AwsOp]: Flow[Op, Op#B, NotUsed] =
+    Setup
+      .flow { mat => _ =>
+        DynamoClientExt(mat.system).dynamoClient.underlying.flow[Op]
+      }
+      .mapMaterializedValue(_ => NotUsed)
 
   /**
    * Create a Source that will emit a response for a given request.
    *
    * @param op request to send
-   * @param sys actor system that will be used to resolved managed DynamoClient
    */
-  def source(op: AwsPagedOp)(implicit sys: ActorSystem): Source[op.B, NotUsed] =
-    DynamoDbExternal.source(op)(DynamoClientExt(sys).dynamoClient)
+  def source(op: AwsPagedOp): Source[op.B, NotUsed] =
+    Setup
+      .source { mat => _ =>
+        Paginator.source(DynamoClientExt(mat.system).dynamoClient.underlying.flow, op)
+      }
+      .mapMaterializedValue(_ => NotUsed)
 
   /**
    * Create a Source that will emit a response for a given request.
    *
    * @param op request to send
-   * @param sys actor system that will be used to resolved managed DynamoClient
    */
-  def source(op: AwsOp)(implicit sys: ActorSystem): Source[op.B, NotUsed] =
-    DynamoDbExternal.source(op)(DynamoClientExt(sys).dynamoClient)
+  def source(op: AwsOp): Source[op.B, NotUsed] =
+    Source.single(op).via(flow).map(_.asInstanceOf[op.B])
 
   /**
    * Create a Future that will be completed with a response to a given request.
    *
    * @param op request to send
-   * @param sys actor system that will be used to resolved managed DynamoClient
+   * @param mat materialized that will be used to run the stream
    */
-  def single(op: AwsOp)(implicit sys: ActorSystem): Future[op.B] =
-    DynamoDbExternal.single(op)(DynamoClientExt(sys).dynamoClient)
+  def single(op: AwsOp)(implicit mat: Materializer): Future[op.B] =
+    source(op).runWith(Sink.head)
 }
