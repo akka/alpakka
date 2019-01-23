f6effdc86e145168a6523fde7efb4f774e26fcf3
adriano.santos <sleipnir@bsd.com.br>
2019-01-22 23:27:25 -0200

    Spring-web: support for Spring Boot 2.0 #1439

--- spring-web/src/main/java/akka/stream/alpakka/spring/web/SpringWebAkkaStreamsConfiguration.java
+++ spring-web/src/main/java/akka/stream/alpakka/spring/web/SpringWebAkkaStreamsConfiguration.java
@@ -9,13 +9,11 @@ package akka.stream.alpakka.spring.web;
 import akka.actor.ActorSystem;
 import akka.stream.ActorMaterializer;
 import akka.stream.Materializer;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.ReactiveAdapterRegistry;
-import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
 
 @Configuration
 @ConditionalOnClass(akka.stream.javadsl.Source.class)
@@ -24,11 +22,8 @@ public class SpringWebAkkaStreamsConfiguration {
   private final ActorSystem system;
   private final ActorMaterializer mat;
 
-  @Autowired
-  public SpringWebAkkaStreamsConfiguration(
-      RequestMappingHandlerAdapter requestMappingHandlerAdapter) {
-    final ReactiveAdapterRegistry registry =
-        requestMappingHandlerAdapter.getReactiveAdapterRegistry();
+  public SpringWebAkkaStreamsConfiguration() {
+    final ReactiveAdapterRegistry registry = ReactiveAdapterRegistry.getSharedInstance();
 
     system = ActorSystem.create("SpringWebAkkaStreamsSystem");
     mat = ActorMaterializer.create(system);
