093116368b17f4fef9e6dba8fa6f55d6c11e6b52
Enno <458526+ennru@users.noreply.github.com>
2019-01-25 18:45:57 +0100

    collect result, simplify paging

--- orientdb/src/main/scala/akka/stream/alpakka/orientdb/impl/OrientDBSourceStage.scala
+++ orientdb/src/main/scala/akka/stream/alpakka/orientdb/impl/OrientDBSourceStage.scala
@@ -16,6 +16,7 @@ import com.orientechnologies.orient.core.sql.query.{OSQLNonBlockingQuery, OSQLSy
 
 import scala.collection.JavaConverters._
 import scala.collection.immutable
+import scala.collection.mutable.ListBuffer
 
 /**
  * INTERNAL API
@@ -31,13 +32,7 @@ private[orientdb] final class OrientDBSourceStage[T](className: String,
   override val shape = SourceShape(out)
 
   override def createLogic(inheritedAttributes: Attributes) =
-    new OrientDBSourceLogic[T](className,
-                               query,
-                               settings,
-                               out,
-                               shape,
-                               SkipAndLimit(settings.skip, settings.limit),
-                               clazz)
+    new OrientDBSourceLogic[T](className, query, settings, out, shape, clazz)
 }
 
 private[orientdb] sealed class OrientDBSourceLogic[T](className: String,
@@ -45,7 +40,6 @@ private[orientdb] sealed class OrientDBSourceLogic[T](className: String,
                                                       settings: OrientDBSourceSettings,
                                                       out: Outlet[OrientDbReadResult[T]],
                                                       shape: SourceShape[OrientDbReadResult[T]],
-                                                      skipAndLimit: SkipAndLimit,
                                                       clazz: Option[Class[T]] = None)
     extends GraphStageLogic(shape)
     with OutHandler {
@@ -54,6 +48,7 @@ private[orientdb] sealed class OrientDBSourceLogic[T](className: String,
 
   private var client: ODatabaseDocumentTx = _
   private var oObjectClient: OObjectDatabaseTx = _
+  private var skip = settings.skip
 
   override def preStart(): Unit = {
     client = settings.oDatabasePool.acquire()
@@ -66,7 +61,15 @@ private[orientdb] sealed class OrientDBSourceLogic[T](className: String,
     client.close()
   }
 
-  def sendOSQLRequest(): Unit =
+  def handleResponse(res: List[T]): Unit =
+    if (res.isEmpty)
+      completeStage()
+    else
+      emitMultiple(out, res.map(OrientDbReadResult(_)).toIterator)
+
+  setHandler(out, this)
+
+  override def onPull(): Unit =
     if (clazz.isEmpty) {
       ODatabaseRecordThreadLocal.instance().set(client)
       if (query.nonEmpty) {
@@ -74,7 +77,7 @@ private[orientdb] sealed class OrientDBSourceLogic[T](className: String,
       } else {
         client.query[java.util.List[T]](
           new OSQLNonBlockingQuery[T](
-            s"SELECT * FROM $className SKIP ${skipAndLimit.skip} LIMIT ${skipAndLimit.limit}",
+            s"SELECT * FROM $className SKIP ${skip} LIMIT ${settings.limit}",
             new OSQLCallback
           )
         )
@@ -93,45 +96,34 @@ private[orientdb] sealed class OrientDBSourceLogic[T](className: String,
           oObjectClient
             .query[java.util.List[T]](
               new OSQLSynchQuery[T](
-                s"SELECT * FROM $className SKIP ${skipAndLimit.skip} LIMIT ${skipAndLimit.limit}"
+                s"SELECT * FROM $className SKIP ${skip} LIMIT ${settings.limit}"
               )
             )
             .asScala
             .toList
         if (oDocs.nonEmpty) {
-          skipAndLimit.skip = skipAndLimit.skip + skipAndLimit.limit
+          skip += settings.limit
         }
         handleResponse(oDocs)
       }
     }
 
-  def handleResponse(res: List[T]): Unit =
-    if (res.isEmpty)
-      completeStage()
-    else
-      emitMultiple(out, res.map(OrientDbReadResult(_)).toIterator)
-
-  setHandler(out, this)
-
-  override def onPull(): Unit = sendOSQLRequest()
-
-  class OSQLCallback extends OCommandResultListener {
-    var oDocs: List[T] = List()
+  final private class OSQLCallback extends OCommandResultListener {
+    val oDocs = ListBuffer.newBuilder[T]
 
     override def result(iRecord: scala.Any): Boolean = {
-      oDocs :+= iRecord.asInstanceOf[T]
+      oDocs += iRecord.asInstanceOf[T]
       true
     }
 
     override def getResult: AnyRef = oDocs
 
     override def end(): Unit = {
-      if (oDocs.nonEmpty) {
-        skipAndLimit.skip = skipAndLimit.skip + skipAndLimit.limit
+      val res = oDocs.result().toList
+      if (res.nonEmpty) {
+        skip += settings.limit
       }
-      responseHandler.invoke(oDocs)
+      responseHandler.invoke(res)
     }
   }
 }
-
-private[orientdb] case class SkipAndLimit(var skip: Int, var limit: Int)
