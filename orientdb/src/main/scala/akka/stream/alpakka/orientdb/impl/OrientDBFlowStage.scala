4c3fa1b4c53e8488d09a4564e6a807be1bf76785
Enno <458526+ennru@users.noreply.github.com>
2019-01-25 14:49:23 +0100

    Remove broken failure handling

--- orientdb/src/main/scala/akka/stream/alpakka/orientdb/impl/OrientDBFlowStage.scala
+++ orientdb/src/main/scala/akka/stream/alpakka/orientdb/impl/OrientDBFlowStage.scala
@@ -4,11 +4,10 @@
 
 package akka.stream.alpakka.orientdb.impl
 
-import akka.NotUsed
 import akka.annotation.InternalApi
+import akka.stream._
 import akka.stream.alpakka.orientdb.{OrientDBUpdateSettings, OrientDbWriteMessage}
 import akka.stream.stage._
-import akka.stream.{Attributes, FlowShape, Inlet, Outlet}
 import com.orientechnologies.orient.`object`.db.OObjectDatabaseTx
 import com.orientechnologies.orient.core.db.ODatabaseRecordThreadLocal
 import com.orientechnologies.orient.core.db.document.ODatabaseDocumentTx
@@ -16,8 +15,8 @@ import com.orientechnologies.orient.core.record.ORecord
 import com.orientechnologies.orient.core.record.impl.ODocument
 import com.orientechnologies.orient.core.tx.OTransaction
 
-import scala.collection.mutable
 import scala.collection.immutable
+import scala.util.control.NonFatal
 
 /**
  * INTERNAL API
@@ -32,6 +31,7 @@ private[orientdb] class OrientDBFlowStage[T, C](
   private val in = Inlet[immutable.Seq[OrientDbWriteMessage[T, C]]]("in")
   private val out = Outlet[immutable.Seq[OrientDbWriteMessage[T, C]]]("out")
   override val shape = FlowShape(in, out)
+  override def initialAttributes: Attributes = super.initialAttributes and ActorAttributes.IODispatcher
 
   override def createLogic(inheritedAttributes: Attributes): GraphStageLogic =
     clazz match {
@@ -77,42 +77,32 @@ private[orientdb] class OrientDBFlowStage[T, C](
         client.getMetadata.getSchema.createClass(className)
       }
       client.begin(OTransaction.TXTYPE.OPTIMISTIC)
-
-      var faultyMessages: List[OrientDbWriteMessage[T, C]] = List()
-      var successfulMessages: List[OrientDbWriteMessage[T, C]] = List()
-      messages.foreach {
-        case OrientDbWriteMessage(oDocument: ODocument, passThrough: C) =>
-          val document = new ODocument()
-          oDocument
-            .asInstanceOf[ODocument]
-            .fieldNames()
-            .zip(oDocument.asInstanceOf[ODocument].fieldValues())
-            .foreach {
-              case (fieldName, fieldVal) =>
-                document.field(fieldName, fieldVal)
-                ()
-            }
-          document.setClassName(className)
-          client.save(document)
-          successfulMessages = successfulMessages ++ List(
-            OrientDbWriteMessage(oDocument.asInstanceOf[T], passThrough)
-          )
-          ()
-        case OrientDbWriteMessage(oRecord: ORecord, passThrough: C) =>
-          client.save(oRecord)
-          successfulMessages = successfulMessages ++ List(
-            OrientDbWriteMessage(oRecord.asInstanceOf[T], passThrough)
-          )
-          ()
-        case OrientDbWriteMessage(others: AnyRef, passThrough: C) =>
-          faultyMessages = faultyMessages ++ List(OrientDbWriteMessage(others.asInstanceOf[T], passThrough))
-          ()
-      }
-      client.commit()
-      if (faultyMessages.nonEmpty) {
-        push(out, faultyMessages)
-      } else {
-        push(out, successfulMessages)
+      try {
+        messages.foreach {
+          case OrientDbWriteMessage(oDocument: ODocument, _) =>
+            val document = new ODocument()
+            oDocument
+              .asInstanceOf[ODocument]
+              .fieldNames()
+              .zip(oDocument.asInstanceOf[ODocument].fieldValues())
+              .foreach {
+                case (fieldName, fieldVal) =>
+                  document.field(fieldName, fieldVal)
+                  ()
+              }
+            document.setClassName(className)
+            client.save(document)
+          case OrientDbWriteMessage(oRecord: ORecord, _) =>
+            client.save(oRecord)
+          case OrientDbWriteMessage(others: AnyRef, _) =>
+            failStage(new RuntimeException(s"unexpected type [${others.getClass()}], ORecord required"))
+        }
+        client.commit()
+        push(out, messages)
+      } catch {
+        case NonFatal(e) =>
+          client.rollback()
+          throw e
       }
     }
   }
@@ -123,24 +113,11 @@ private[orientdb] class OrientDBFlowStage[T, C](
       ODatabaseRecordThreadLocal.instance().set(client)
       client.setDatabaseOwner(oObjectClient)
       oObjectClient.getEntityManager.registerEntityClass(clazz)
-
-      var faultyMessages: List[OrientDbWriteMessage[T, C]] = List()
-      var successfulMessages: List[OrientDbWriteMessage[T, C]] = List()
       messages.foreach {
-        case OrientDbWriteMessage(typeRecord: T, passThrough: C) =>
+        case OrientDbWriteMessage(typeRecord: Any, _) =>
           oObjectClient.save(typeRecord)
-          successfulMessages = successfulMessages ++ List(OrientDbWriteMessage(typeRecord, passThrough))
-          ()
-        case OrientDbWriteMessage(others: AnyRef, passThrough: C) =>
-          faultyMessages = faultyMessages ++ List(OrientDbWriteMessage(others.asInstanceOf[T], passThrough))
-          ()
       }
-
-      if (faultyMessages.nonEmpty) {
-        push(out, faultyMessages)
-      } else {
-        push(out, successfulMessages)
-      }
+      push(out, messages)
     }
 
   }
