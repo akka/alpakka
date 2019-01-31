2c17566914e79c9c732ca7c7fc1b26654094ec43
Enno <458526+ennru@users.noreply.github.com>
2019-01-31 14:42:18 +0100

    Remove some scalaesque stuff from Java API

--- ironmq/src/main/scala/akka/stream/alpakka/ironmq/javadsl/IronMqProducer.scala
+++ ironmq/src/main/scala/akka/stream/alpakka/ironmq/javadsl/IronMqProducer.scala
@@ -6,6 +6,7 @@ package akka.stream.alpakka.ironmq.javadsl
 
 import java.util.concurrent.CompletionStage
 
+import akka.japi.Pair
 import akka.{Done, NotUsed}
 import akka.stream.alpakka.ironmq._
 import akka.stream.javadsl.{Flow, Sink}
@@ -18,8 +19,12 @@ object IronMqProducer {
 
   import FutureConverters._
 
-  def producerFlow(queueName: String, settings: IronMqSettings): Flow[PushMessage, Message.Id, NotUsed] =
-    ScalaIronMqProducer.producerFlow(queueName, settings).asJava
+  def producerFlow(queueName: String, settings: IronMqSettings): Flow[PushMessage, String, NotUsed] =
+    ScalaIronMqProducer
+      .producerFlow(queueName, settings)
+      .asJava
+      // To make Message.Id a String
+      .asInstanceOf[Flow[PushMessage, String, NotUsed]]
 
   def producerSink(queueName: String, settings: IronMqSettings): Sink[PushMessage, CompletionStage[Done]] =
     ScalaIronMqProducer.producerSink(queueName, settings).mapMaterializedValue(_.toJava).asJava
@@ -27,13 +32,15 @@ object IronMqProducer {
   def atLeastOnceProducerFlow[C1 <: Committable](
       queueName: String,
       settings: IronMqSettings
-  ): Flow[CommittablePushMessage[C1], Message.Id, NotUsed] =
+  ): Flow[CommittablePushMessage[C1], String, NotUsed] =
     ScalaFlow[CommittablePushMessage[C1]]
       .map { cm =>
         cm.message -> cm.toCommit.asScala
       }
       .via(ScalaIronMqProducer.atLeastOnceProducerFlow(queueName, settings))
       .asJava
+      // To make Message.Id a String
+      .asInstanceOf[Flow[CommittablePushMessage[C1], String, NotUsed]]
 
   def atLeastOnceProducerSink[C1 <: Committable](queueName: String,
                                                  settings: IronMqSettings): Sink[CommittablePushMessage[C1], NotUsed] =
@@ -48,7 +55,7 @@ object IronMqProducer {
       queueName: String,
       settings: IronMqSettings,
       commitFlow: Flow[ToCommit, CommitResult, CommitMat]
-  ): Flow[CommittablePushMessage[ToCommit], (Message.Id, CommitResult), CommitMat] =
+  ): Flow[CommittablePushMessage[ToCommit], Pair[String, CommitResult], CommitMat] =
     ScalaFlow[CommittablePushMessage[ToCommit]]
       .map { cm =>
         cm.message -> cm.toCommit
@@ -56,6 +63,7 @@ object IronMqProducer {
       .viaMat(ScalaIronMqProducer.atLeastOnceProducerFlow(queueName, settings, commitFlow.asScala))(
         Keep.right
       )
+      .map(p => Pair(p._1.asInstanceOf[String], p._2))
       .asJava
 
   def atLeastOnceProducerSink[ToCommit, CommitResult, CommitMat](
