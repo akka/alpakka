78b1678fa998929be6aee8876bc7ec31addec5b5
Enno <458526+ennru@users.noreply.github.com>
2019-02-01 15:08:19 +0100

    OrientDb: Total make-over (#1445)
    * Un-case classe OrientDb settings
    * Rename stream element classes
    * No automagic buffering
    * Remove async handlers
    * Remove retry logic
    * Split logic
    * Use transaction for typed updates
    * Equal APIs in Scala and Java
    * Upgrade to latest version available for Docker
    * Assert stage stopping
    * Use pinned dispatcher to meet OrientDB's non-multithreadingness

--- orientdb/src/main/scala/akka/stream/alpakka/orientdb/model.scala
+++ orientdb/src/main/scala/akka/stream/alpakka/orientdb/model.scala
@@ -6,22 +6,20 @@ package akka.stream.alpakka.orientdb
 
 import akka.NotUsed
 
-object OIncomingMessage {
+object OrientDbWriteMessage {
   // Apply method to use when not using passThrough
-  def apply[T](oDocument: T): OIncomingMessage[T, NotUsed] =
-    OIncomingMessage(oDocument, NotUsed)
+  def apply[T](oDocument: T): OrientDbWriteMessage[T, NotUsed] =
+    OrientDbWriteMessage(oDocument, NotUsed)
 
   // Java-api - without passThrough
-  def create[T](oDocument: T): OIncomingMessage[T, NotUsed] =
-    OIncomingMessage(oDocument, NotUsed)
+  def create[T](oDocument: T): OrientDbWriteMessage[T, NotUsed] =
+    OrientDbWriteMessage(oDocument, NotUsed)
 
   // Java-api - with passThrough
   def create[T, C](oDocument: T, passThrough: C) =
-    OIncomingMessage(oDocument, passThrough)
+    OrientDbWriteMessage(oDocument, passThrough)
 }
 
-final case class OIncomingMessage[T, C](oDocument: T, passThrough: C)
+final case class OrientDbWriteMessage[T, C](oDocument: T, passThrough: C)
 
-final case class OOutgoingMessage[T](oDocument: T)
-
-case class OSQLResponse[T](error: Option[String], result: Seq[OOutgoingMessage[T]])
+final case class OrientDbReadResult[T](oDocument: T)
