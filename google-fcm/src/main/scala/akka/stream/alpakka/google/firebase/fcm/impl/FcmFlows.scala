1337382b3063e430f66632b5df584e6bde13faa2
Martynas Mickevičius <self@2m.lt>
2019-02-04 15:32:59 +0200

    Fix a couple of typos

--- google-fcm/src/main/scala/akka/stream/alpakka/google/firebase/fcm/impl/FcmFlows.scala
+++ google-fcm/src/main/scala/akka/stream/alpakka/google/firebase/fcm/impl/FcmFlows.scala
@@ -20,15 +20,15 @@ private[fcm] object FcmFlows {
   private[fcm] def fcmWithData[T](conf: FcmSettings,
                                   sender: FcmSender): Flow[(FcmNotification, T), (FcmResponse, T), NotUsed] =
     Setup
-      .flow { implicit materializer => attributes =>
+      .flow { implicit materializer => _ =>
         import materializer.executionContext
         val http = Http()(materializer.system)
         val session: GoogleSession = new GoogleSession(conf.clientEmail, conf.privateKey, new GoogleTokenApi(http))
         Flow[(FcmNotification, T)]
-          .mapAsync(conf.maxConcurentConnections)(
+          .mapAsync(conf.maxConcurrentConnections)(
             in =>
               session.getToken().flatMap { token =>
-                sender.send(conf.projectid, token, http, FcmSend(conf.isTest, in._1)).zip(Future.successful(in._2))
+                sender.send(conf.projectId, token, http, FcmSend(conf.isTest, in._1)).zip(Future.successful(in._2))
             }
           )
       }
@@ -36,16 +36,16 @@ private[fcm] object FcmFlows {
 
   private[fcm] def fcm(conf: FcmSettings, sender: FcmSender): Flow[FcmNotification, FcmResponse, NotUsed] =
     Setup
-      .flow { implicit materializer => attributes =>
+      .flow { implicit materializer => _ =>
         import materializer.executionContext
         val http = Http()(materializer.system)
         val session: GoogleSession = new GoogleSession(conf.clientEmail, conf.privateKey, new GoogleTokenApi(http))
         val sender: FcmSender = new FcmSender()
         Flow[FcmNotification]
-          .mapAsync(conf.maxConcurentConnections)(
+          .mapAsync(conf.maxConcurrentConnections)(
             in =>
               session.getToken().flatMap { token =>
-                sender.send(conf.projectid, token, http, FcmSend(conf.isTest, in))
+                sender.send(conf.projectId, token, http, FcmSend(conf.isTest, in))
             }
           )
       }
