f6be2801e70868d355af148c13ca0d7118047977
Martynas Mickevičius <self@2m.lt>
2019-02-09 12:08:22 +0000

    Cleanup hbase tests

--- hbase/src/main/scala/akka/stream/alpakka/hbase/javadsl/HTableStage.scala
+++ hbase/src/main/scala/akka/stream/alpakka/hbase/javadsl/HTableStage.scala
@@ -4,26 +4,19 @@
 
 package akka.stream.alpakka.hbase.javadsl
 
+import java.util.concurrent.CompletionStage
+
 import akka.stream.alpakka.hbase.HTableSettings
 import akka.stream.alpakka.hbase.internal.HBaseFlowStage
 import akka.stream.scaladsl.{Flow, Keep, Sink}
 import akka.{Done, NotUsed}
-import org.apache.hadoop.conf.Configuration
-import org.apache.hadoop.hbase.TableName
-import org.apache.hadoop.hbase.client.Mutation
 
-import scala.concurrent.Future
+import scala.compat.java8.FutureConverters._
 
 object HTableStage {
 
-  def table[T](conf: Configuration,
-               tableName: TableName,
-               columnFamilies: java.util.List[String],
-               converter: java.util.function.Function[T, java.util.List[Mutation]]): HTableSettings[T] =
-    HTableSettings.create(conf, tableName, columnFamilies, converter)
-
-  def sink[A](config: HTableSettings[A]): akka.stream.javadsl.Sink[A, Future[Done]] =
-    Flow[A].via(flow(config)).toMat(Sink.ignore)(Keep.right).asJava
+  def sink[A](config: HTableSettings[A]): akka.stream.javadsl.Sink[A, CompletionStage[Done]] =
+    Flow[A].via(flow(config)).toMat(Sink.ignore)(Keep.right).mapMaterializedValue(toJava).asJava
 
   def flow[A](settings: HTableSettings[A]): akka.stream.javadsl.Flow[A, A, NotUsed] =
     Flow.fromGraph(new HBaseFlowStage[A](settings)).asJava
