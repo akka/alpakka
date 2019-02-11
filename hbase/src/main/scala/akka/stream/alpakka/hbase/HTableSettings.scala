cf8cb022ccead978054eeb83381c978978804530
Martynas Mickevičius <self@2m.lt>
2019-02-11 14:27:49 +0200

    Add missing with* methods

--- hbase/src/main/scala/akka/stream/alpakka/hbase/HTableSettings.scala
+++ hbase/src/main/scala/akka/stream/alpakka/hbase/HTableSettings.scala
@@ -9,11 +9,45 @@ import org.apache.hadoop.hbase.TableName
 import org.apache.hadoop.hbase.client.Mutation
 
 import scala.collection.immutable
+import scala.collection.JavaConverters._
+import scala.compat.java8.FunctionConverters._
 
 final class HTableSettings[T] private (val conf: Configuration,
                                        val tableName: TableName,
                                        val columnFamilies: immutable.Seq[String],
-                                       val converter: T => immutable.Seq[Mutation]) {}
+                                       val converter: T => immutable.Seq[Mutation]) {
+
+  def withConf(conf: Configuration): HTableSettings[T] =
+    copy(conf = conf)
+
+  def withTableName(tableName: TableName): HTableSettings[T] =
+    copy(tableName = tableName)
+
+  def withColumnFamilies(columnFamilies: immutable.Seq[String]): HTableSettings[T] =
+    copy(columnFamilies = columnFamilies)
+
+  /**
+   * Java Api
+   */
+  def withColumnFamilies(columnFamilies: java.util.List[String]): HTableSettings[T] =
+    copy(columnFamilies = columnFamilies.asScala.to[immutable.Seq])
+
+  def withConverter(converter: T => immutable.Seq[Mutation]): HTableSettings[T] =
+    copy(converter = converter)
+
+  /**
+   * Java Api
+   */
+  def withConverter(converter: java.util.function.Function[T, java.util.List[Mutation]]): HTableSettings[T] =
+    copy(converter = converter.asScala(_).asScala.to[immutable.Seq])
+
+  private def copy(conf: Configuration = conf,
+                   tableName: TableName = tableName,
+                   columnFamilies: immutable.Seq[String] = columnFamilies,
+                   converter: T => immutable.Seq[Mutation] = converter) =
+    new HTableSettings[T](conf, tableName, columnFamilies, converter)
+
+}
 
 object HTableSettings {
 
@@ -33,16 +67,9 @@ object HTableSettings {
   def create[T](conf: Configuration,
                 tableName: TableName,
                 columnFamilies: java.util.List[String],
-                converter: java.util.function.Function[T, java.util.List[Mutation]]): HTableSettings[T] = {
-    import scala.compat.java8.FunctionConverters._
-    import scala.collection.JavaConverters._
-
-    val mutationsFunc = (m: T) => {
-      val list = asScalaFromFunction(converter)(m)
-
-      immutable.Seq(list.asScala: _*)
-    }
-
-    HTableSettings(conf, tableName, immutable.Seq(columnFamilies.asScala: _*), mutationsFunc)
-  }
+                converter: java.util.function.Function[T, java.util.List[Mutation]]): HTableSettings[T] =
+    HTableSettings(conf,
+                   tableName,
+                   columnFamilies.asScala.to[immutable.Seq],
+                   converter.asScala(_).asScala.to[immutable.Seq])
 }
