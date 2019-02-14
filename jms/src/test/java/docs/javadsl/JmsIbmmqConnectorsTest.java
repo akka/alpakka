3c80a806141438170d8bf5b65758c18cde209996
Enno <458526+ennru@users.noreply.github.com>
2019-02-14 14:46:13 +0100

    JMS: Adapt to JMS 2.0 API change in tests (#1496)
    * Add methods required by JMS 2.0
    * Put ibmmq in sorted list

--- jms/src/test/java/docs/javadsl/JmsIbmmqConnectorsTest.java
+++ jms/src/test/java/docs/javadsl/JmsIbmmqConnectorsTest.java
@@ -45,170 +45,170 @@ import static org.junit.Assert.assertEquals;
 
 public class JmsIbmmqConnectorsTest {
 
-    private static ActorSystem system;
-    private static Materializer materializer;
-    private static MQQueueConnectionFactory queueConnectionFactory;
-    private static MQTopicConnectionFactory topicConnectionFactory;
+  private static ActorSystem system;
+  private static Materializer materializer;
+  private static MQQueueConnectionFactory queueConnectionFactory;
+  private static MQTopicConnectionFactory topicConnectionFactory;
 
-    @BeforeClass
-    public static void setup() throws JMSException {
-        system = ActorSystem.create();
-        materializer = ActorMaterializer.create(system);
-        // #ibmmq-connection-factory
-        // Create the IBM MQ MQQueueConnectionFactory
-        MQQueueConnectionFactory connectionFactory = new MQQueueConnectionFactory();
+  @BeforeClass
+  public static void setup() throws JMSException {
+    system = ActorSystem.create();
+    materializer = ActorMaterializer.create(system);
+    // #ibmmq-connection-factory
+    // Create the IBM MQ MQQueueConnectionFactory
+    MQQueueConnectionFactory connectionFactory = new MQQueueConnectionFactory();
 
-        // #ibmmq-connection-factory
-        JmsIbmmqConnectorsTest.queueConnectionFactory =
-            (MQQueueConnectionFactory) initDefaultFactory(connectionFactory);
-        JmsIbmmqConnectorsTest.topicConnectionFactory =
-            (MQTopicConnectionFactory) initDefaultFactory(new MQTopicConnectionFactory());
-    }
+    // #ibmmq-connection-factory
+    JmsIbmmqConnectorsTest.queueConnectionFactory =
+        (MQQueueConnectionFactory) initDefaultFactory(connectionFactory);
+    JmsIbmmqConnectorsTest.topicConnectionFactory =
+        (MQTopicConnectionFactory) initDefaultFactory(new MQTopicConnectionFactory());
+  }
 
-    private static MQConnectionFactory initDefaultFactory(MQConnectionFactory connectionFactory)
-        throws JMSException {
-        // #ibmmq-connection-factory
-        // align to docker image: ibmcom/mq:9.1.1.0
-        connectionFactory.setHostName("localhost");
-        connectionFactory.setPort(1414);
-        connectionFactory.setQueueManager("QM1");
-        connectionFactory.setChannel("DEV.APP.SVRCONN");
+  private static MQConnectionFactory initDefaultFactory(MQConnectionFactory connectionFactory)
+      throws JMSException {
+    // #ibmmq-connection-factory
+    // align to docker image: ibmcom/mq:9.1.1.0
+    connectionFactory.setHostName("localhost");
+    connectionFactory.setPort(1414);
+    connectionFactory.setQueueManager("QM1");
+    connectionFactory.setChannel("DEV.APP.SVRCONN");
 
-        // #ibmmq-connection-factory
-        return connectionFactory;
-    }
+    // #ibmmq-connection-factory
+    return connectionFactory;
+  }
 
-    @AfterClass
-    public static void teardown() {
-        TestKit.shutdownActorSystem(system);
-    }
+  @AfterClass
+  public static void teardown() {
+    TestKit.shutdownActorSystem(system);
+  }
 
-    @Test
-    public void publishAndConsumeQueue()
-        throws JMSException, InterruptedException, ExecutionException, TimeoutException {
+  @Test
+  public void publishAndConsumeQueue()
+      throws JMSException, InterruptedException, ExecutionException, TimeoutException {
 
-        // #ibmmq-queue
-        // Connect to IBM MQ over TCP/IP
-        queueConnectionFactory.setTransportType(CommonConstants.WMQ_CM_CLIENT);
-        String queueName = "DEV.QUEUE.1";
+    // #ibmmq-queue
+    // Connect to IBM MQ over TCP/IP
+    queueConnectionFactory.setTransportType(CommonConstants.WMQ_CM_CLIENT);
+    String queueName = "DEV.QUEUE.1";
 
-        Sink<String, CompletionStage<Done>> jmsSink =
-            JmsProducer.textSink(
-                JmsProducerSettings.create(system, queueConnectionFactory).withQueue(queueName));
+    Sink<String, CompletionStage<Done>> jmsSink =
+        JmsProducer.textSink(
+            JmsProducerSettings.create(system, queueConnectionFactory).withQueue(queueName));
 
-        // #ibmmq-queue
+    // #ibmmq-queue
 
-        List<String> in = Arrays.asList("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k");
-        Source.from(in).runWith(jmsSink, materializer);
+    List<String> in = Arrays.asList("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k");
+    Source.from(in).runWith(jmsSink, materializer);
 
-        // #ibmmq-queue
-        // Option1: create Source using default factory with just name
-        Source<TxEnvelope, JmsConsumerControl> txJmsSource =
-            JmsConsumer.txSource(
-                JmsConsumerSettings.create(system, queueConnectionFactory).withQueue(queueName));
+    // #ibmmq-queue
+    // Option1: create Source using default factory with just name
+    Source<TxEnvelope, JmsConsumerControl> txJmsSource =
+        JmsConsumer.txSource(
+            JmsConsumerSettings.create(system, queueConnectionFactory).withQueue(queueName));
 
-        // #ibmmq-queue
+    // #ibmmq-queue
 
-        CompletionStage<List<String>> result =
-            txJmsSource
-                .take(in.size())
-                .map(
-                    envelope -> {
-                        envelope.commit();
-                        if (envelope.message() instanceof TextMessage) {
-                            TextMessage message = (TextMessage) envelope.message();
-                            return message.getText();
-                        } else {
-                            throw new RuntimeException(
-                                "unexpected message type " + envelope.message().getClass());
-                        }
-                    })
-                .runWith(Sink.seq(), materializer);
+    CompletionStage<List<String>> result =
+        txJmsSource
+            .take(in.size())
+            .map(
+                envelope -> {
+                  envelope.commit();
+                  if (envelope.message() instanceof TextMessage) {
+                    TextMessage message = (TextMessage) envelope.message();
+                    return message.getText();
+                  } else {
+                    throw new RuntimeException(
+                        "unexpected message type " + envelope.message().getClass());
+                  }
+                })
+            .runWith(Sink.seq(), materializer);
 
-        List<String> out = new ArrayList<>(result.toCompletableFuture().get(3, TimeUnit.SECONDS));
-        Collections.sort(out);
-        assertEquals(in, out);
-    }
+    List<String> out = new ArrayList<>(result.toCompletableFuture().get(3, TimeUnit.SECONDS));
+    Collections.sort(out);
+    assertEquals(in, out);
+  }
 
-    @Test
-    public void publishAndConsumeCustomDestination()
-        throws JMSException, InterruptedException, ExecutionException, TimeoutException {
+  @Test
+  public void publishAndConsumeCustomDestination()
+      throws JMSException, InterruptedException, ExecutionException, TimeoutException {
 
-        queueConnectionFactory.setTransportType(CommonConstants.WMQ_CM_CLIENT);
-
-        // #ibmmq-custom-destination
-        // Option2: create Source using custom factory
-        String customQueue = "DEV.QUEUE.3";
-        // #ibmmq-custom-destination
-
-        // "app" clients are limited to perform create queue
-        // existing queue in use
-        Sink<String, CompletionStage<Done>> jmsSink =
-            JmsProducer.textSink(
-                JmsProducerSettings.create(system, queueConnectionFactory)
-                    .withDestination(new CustomDestination("custom", createQueue(customQueue))));
-
-        List<String> in = Arrays.asList("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k");
-
-        Source.from(in).runWith(jmsSink, materializer);
-
-        // #ibmmq-custom-destination
-        Source<String, JmsConsumerControl> jmsSource =
-            JmsConsumer.textSource(
-                JmsConsumerSettings.create(system, queueConnectionFactory)
-                    .withDestination(new CustomDestination("custom", createQueue(customQueue))));
-
-        // #ibmmq-custom-destination
-
-        CompletionStage<List<String>> result =
-            jmsSource.take(in.size()).runWith(Sink.seq(), materializer);
-
-        assertEquals(in.size(), result.toCompletableFuture().get(5, TimeUnit.SECONDS).size());
-    }
-
-    @Test
-    public void publishAndConsumeTopic()
-        throws JMSException, InterruptedException, ExecutionException, TimeoutException {
-
-        List<String> in = Arrays.asList("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k");
-
-        // #ibmmq-topic
-        // Connect to IBM MQ over TCP/IP
-        topicConnectionFactory.setTransportType(CommonConstants.WMQ_CM_CLIENT);
-        String testTopicName = "dev/";
-
-        Sink<String, CompletionStage<Done>> jmsTopicSink =
-            JmsProducer.textSink(
-                JmsProducerSettings.create(system, topicConnectionFactory).withTopic(testTopicName));
-
-        // Option1: create Source using default factory with just name
-        Source<String, JmsConsumerControl> jmsTopicSource =
-            JmsConsumer.textSource(
-                JmsConsumerSettings.create(system, topicConnectionFactory).withTopic(testTopicName));
-        // #ibmmq-topic
-
-        CompletionStage<List<String>> result =
-            jmsTopicSource.take(in.size()).runWith(Sink.seq(), materializer);
-
-        Thread.sleep(500);
-
-        Source.from(in).runWith(jmsTopicSink, materializer);
-
-        assertEquals(in, result.toCompletableFuture().get(5, TimeUnit.SECONDS));
-    }
+    queueConnectionFactory.setTransportType(CommonConstants.WMQ_CM_CLIENT);
 
     // #ibmmq-custom-destination
-    Function<Session, Destination> createQueue(String destinationName) {
-        return (session) -> {
-            // cast to correct session implementation: MQQueueSession, MQTopicSession, MQSession
-            MQQueueSession mqSession = (MQQueueSession) session;
-            try {
-                return mqSession.createQueue(destinationName);
-            } catch (JMSException e) {
-                throw new RuntimeException(e);
-            }
-        };
-    }
+    // Option2: create Source using custom factory
+    String customQueue = "DEV.QUEUE.3";
     // #ibmmq-custom-destination
+
+    // "app" clients are limited to perform create queue
+    // existing queue in use
+    Sink<String, CompletionStage<Done>> jmsSink =
+        JmsProducer.textSink(
+            JmsProducerSettings.create(system, queueConnectionFactory)
+                .withDestination(new CustomDestination("custom", createQueue(customQueue))));
+
+    List<String> in = Arrays.asList("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k");
+
+    Source.from(in).runWith(jmsSink, materializer);
+
+    // #ibmmq-custom-destination
+    Source<String, JmsConsumerControl> jmsSource =
+        JmsConsumer.textSource(
+            JmsConsumerSettings.create(system, queueConnectionFactory)
+                .withDestination(new CustomDestination("custom", createQueue(customQueue))));
+
+    // #ibmmq-custom-destination
+
+    CompletionStage<List<String>> result =
+        jmsSource.take(in.size()).runWith(Sink.seq(), materializer);
+
+    assertEquals(in.size(), result.toCompletableFuture().get(5, TimeUnit.SECONDS).size());
+  }
+
+  @Test
+  public void publishAndConsumeTopic()
+      throws JMSException, InterruptedException, ExecutionException, TimeoutException {
+
+    List<String> in = Arrays.asList("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k");
+
+    // #ibmmq-topic
+    // Connect to IBM MQ over TCP/IP
+    topicConnectionFactory.setTransportType(CommonConstants.WMQ_CM_CLIENT);
+    String testTopicName = "dev/";
+
+    Sink<String, CompletionStage<Done>> jmsTopicSink =
+        JmsProducer.textSink(
+            JmsProducerSettings.create(system, topicConnectionFactory).withTopic(testTopicName));
+
+    // Option1: create Source using default factory with just name
+    Source<String, JmsConsumerControl> jmsTopicSource =
+        JmsConsumer.textSource(
+            JmsConsumerSettings.create(system, topicConnectionFactory).withTopic(testTopicName));
+    // #ibmmq-topic
+
+    CompletionStage<List<String>> result =
+        jmsTopicSource.take(in.size()).runWith(Sink.seq(), materializer);
+
+    Thread.sleep(500);
+
+    Source.from(in).runWith(jmsTopicSink, materializer);
+
+    assertEquals(in, result.toCompletableFuture().get(5, TimeUnit.SECONDS));
+  }
+
+  // #ibmmq-custom-destination
+  Function<Session, Destination> createQueue(String destinationName) {
+    return (session) -> {
+      // cast to correct session implementation: MQQueueSession, MQTopicSession, MQSession
+      MQQueueSession mqSession = (MQQueueSession) session;
+      try {
+        return mqSession.createQueue(destinationName);
+      } catch (JMSException e) {
+        throw new RuntimeException(e);
+      }
+    };
+  }
+  // #ibmmq-custom-destination
 
 }
