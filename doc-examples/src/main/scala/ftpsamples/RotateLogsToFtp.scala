dd735f45ba5a87c894d5a4be145ab2eda09f4497
Enno <458526+ennru@users.noreply.github.com>
2019-02-14 14:30:59 +0100

    Larger files, list created files

--- doc-examples/src/main/scala/ftpsamples/RotateLogsToFtp.scala
+++ doc-examples/src/main/scala/ftpsamples/RotateLogsToFtp.scala
@@ -7,7 +7,7 @@ package ftpsamples
 import java.net.InetAddress
 import java.nio.file.{Files, Path}
 
-import akka.stream.alpakka.file.scaladsl.LogRotatorSink
+import akka.stream.alpakka.file.scaladsl.{Directory, LogRotatorSink}
 import akka.stream.alpakka.ftp.scaladsl.Sftp
 import akka.stream.alpakka.ftp.{FtpCredentials, SftpIdentity, SftpSettings}
 import akka.stream.scaladsl.{Compression, Flow, Keep, Source}
@@ -16,26 +16,24 @@ import org.apache.mina.util.AvailablePortFinder
 import playground.filesystem.FileSystemMock
 import playground.{ActorSystemAvailable, SftpServerEmbedded}
 
-import scala.util.{Failure, Success}
-
 object RotateLogsToFtp extends ActorSystemAvailable with App {
 
-  val ftpFileSystem = new FileSystemMock()
+  private val ftpFileSystem = new FileSystemMock().fileSystem
   private val privateKeyPassphrase = SftpServerEmbedded.clientPrivateKeyPassphrase
   private val pathToIdentityFile = SftpServerEmbedded.clientPrivateKeyFile
   private val username = "username"
-  private val password = "username"
+  private val password = username
   private val hostname = "localhost"
   val port = AvailablePortFinder.getNextAvailable(21000)
 
-  val home: Path = ftpFileSystem.fileSystem.getPath(SftpServerEmbedded.FtpRootDir).resolve("tmp")
+  val home: Path = ftpFileSystem.getPath(SftpServerEmbedded.FtpRootDir).resolve("tmp")
   if (!Files.exists(home)) Files.createDirectories(home)
 
-  SftpServerEmbedded.start(ftpFileSystem.fileSystem, port)
+  SftpServerEmbedded.start(ftpFileSystem, port)
 
   // #sample
   val data = ('a' to 'd') // (1)
-    .flatMap(letter => Seq.fill(10)(ByteString(letter.toString * 100)))
+    .flatMap(letter => Seq.fill(10)(ByteString(letter.toString * 10000)))
 
   // (2)
   val rotator = () => {
@@ -62,19 +60,24 @@ object RotateLogsToFtp extends ActorSystemAvailable with App {
 
   val sink = (path: String) =>
     Flow[ByteString]
-      .via(Compression.gzip) // (4s)
+      .via(Compression.gzip) // (4)
       .toMat(Sftp.toPath(s"tmp/$path", settings))(Keep.right)
 
   val completion = Source(data).runWith(LogRotatorSink.withSinkFactory(rotator, sink))
   // #sample
 
-  completion.onComplete {
-    case Success(_) => println("Done")
-    case Failure(f) => f.printStackTrace()
-  }
-
-  completion.onComplete { _ =>
-    SftpServerEmbedded.stopServer()
-    terminateActorSystem()
-  }
+  completion
+    .flatMap { _ =>
+      Directory
+        .ls(home)
+        .runForeach(f => println(f))
+    }
+    .recover {
+      case f =>
+        f.printStackTrace()
+    }
+    .onComplete { _ =>
+      SftpServerEmbedded.stopServer()
+      terminateActorSystem()
+    }
 }
