8e29d32099a27a64fd99f4e0b977bc45d30978e0
Martynas Mickevičius <self@2m.lt>
2019-02-20 19:27:32 +0200

    Use IODispatcher attribute (#1498)
    ## Fixes
    
    Fixes #1173
    
    ## Purpose
    
    Uses the IODispatcher attribute value provided by akka-stream jar, instead of the config path.

--- amqp/src/main/scala/akka/stream/alpakka/amqp/impl/AmqpReplyToSinkStage.scala
+++ amqp/src/main/scala/akka/stream/alpakka/amqp/impl/AmqpReplyToSinkStage.scala
@@ -26,9 +26,7 @@ private[amqp] final class AmqpReplyToSinkStage(settings: AmqpReplyToSinkSettings
   override def shape: SinkShape[OutgoingMessage] = SinkShape.of(in)
 
   override protected def initialAttributes: Attributes =
-    Attributes
-      .name("AmqpReplyToSink")
-      .and(ActorAttributes.dispatcher("akka.stream.default-blocking-io-dispatcher"))
+    super.initialAttributes and Attributes.name("AmqpReplyToSink") and ActorAttributes.IODispatcher
 
   override def createLogicAndMaterializedValue(inheritedAttributes: Attributes): (GraphStageLogic, Future[Done]) = {
     val promise = Promise[Done]()
