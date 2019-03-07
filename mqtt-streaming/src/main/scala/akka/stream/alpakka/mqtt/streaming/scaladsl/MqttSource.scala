827ede3d11787fdaca4d4c5165a87982919e7eb0
Enno <458526+ennru@users.noreply.github.com>
2019-03-07 11:31:38 +0100

    Mqtt streaming: convenience API for the casual user

---
+++ mqtt-streaming/src/main/scala/akka/stream/alpakka/mqtt/streaming/scaladsl/MqttSource.scala
@@ -0,0 +1,81 @@
+/*
+ * Copyright (C) 2016-2019 Lightbend Inc. <http://www.lightbend.com>
+ */
+
+package akka.stream.alpakka.mqtt.streaming.scaladsl
+
+import akka.actor.ActorSystem
+import akka.{Done, NotUsed}
+import akka.event.{Logging, LoggingAdapter}
+import akka.stream.OverflowStrategy
+import akka.stream.alpakka.mqtt.streaming.impl.Setup
+import akka.stream.alpakka.mqtt.streaming._
+import akka.stream.scaladsl.{BroadcastHub, Flow, Keep, Source, SourceQueueWithComplete, Tcp}
+
+import scala.collection.immutable
+import scala.concurrent.{Future, Promise}
+
+object MqttSource {
+
+  def atMostOnce(settings: MqttSessionSettings,
+                 clientId: String,
+                 subscriptions: immutable.Seq[(String, ControlPacketFlags)]): Source[Publish, Future[Done]] =
+    Setup
+      .source { implicit materializer => implicit attributes =>
+        implicit val system: ActorSystem = materializer.system
+        implicit val logging: LoggingAdapter = Logging.getLogger(system, this)
+
+        val mqttClientSession = ActorMqttClientSession(settings)
+        val mqttFlow: Flow[Command[Nothing], Either[MqttCodec.DecodeError, Event[Nothing]], NotUsed] =
+          Mqtt
+            .clientSessionFlow(mqttClientSession)
+            .join(Tcp(system).outgoingConnection("localhost", 1883))
+
+        val subscribed = Promise[Done]()
+
+        val (commands: SourceQueueWithComplete[Command[Nothing]], subscription: Source[Event[Nothing], NotUsed]) =
+          Source
+            .queue[Command[Nothing]](10, OverflowStrategy.fail)
+            .via(mqttFlow)
+            .log("source received")
+            .map {
+              case Left(decodeError) =>
+                throw new RuntimeException(decodeError.toString)
+// TODO anything we should do if no ConnAck arrives "in time"?
+//            case Right(event @ Event(_: ConnAck, _)) =>
+//              event
+              case Right(event @ Event(_: SubAck, _)) =>
+                subscribed.trySuccess(Done)
+                event
+              case Right(event) =>
+                event
+            }
+            .toMat(BroadcastHub.sink)(Keep.both)
+            .run()
+
+        commands.offer(Command(Connect(clientId, ConnectFlags.CleanSession)))
+        commands.offer(Command(Subscribe(subscriptions)))
+
+        val publishSource: Source[Publish, Future[Done]] = subscription
+          .collect {
+            case Event(publish @ Publish(flags, _, Some(packetId), _), _)
+                if flags.contains(ControlPacketFlags.RETAIN) =>
+              commands.offer(Command(PubAck(packetId)))
+              publish
+            case Event(publish: Publish, _) =>
+              publish
+          }
+          .mapMaterializedValue(_ => subscribed.future)
+          .watchTermination() {
+            case (publishSourceCompletion, completion) =>
+              completion.foreach { _ =>
+                // shut down the client flow and session
+                commands.complete()
+                commands.watchCompletion().foreach(_ => mqttClientSession.shutdown())(system.dispatcher)
+              }(system.dispatcher)
+              publishSourceCompletion
+          }
+        publishSource
+      }
+      .mapMaterializedValue(_.flatten)
+}
---
+++ mqtt-streaming/src/test/scala/docs/scaladsl/MqttSourceSpec.scala
@@ -0,0 +1,84 @@
+/*
+ * Copyright (C) 2016-2019 Lightbend Inc. <http://www.lightbend.com>
+ */
+
+package docs.scaladsl
+
+import akka.Done
+import akka.actor.ActorSystem
+import akka.event.{Logging, LoggingAdapter}
+import akka.stream._
+import akka.stream.alpakka.mqtt.streaming._
+import akka.stream.alpakka.mqtt.streaming.scaladsl._
+import akka.stream.scaladsl.{Keep, Sink, Source, Tcp}
+import akka.testkit.TestKit
+import akka.stream.testkit.scaladsl.StreamTestKit.assertAllStagesStopped
+import akka.util.ByteString
+import org.scalatest._
+import org.scalatest.concurrent.ScalaFutures
+
+import scala.concurrent.ExecutionContext
+import scala.concurrent.duration._
+
+class MqttSourceSpec
+    extends TestKit(ActorSystem("MqttFlowSpec"))
+    with WordSpecLike
+    with Matchers
+    with BeforeAndAfterAll
+    with ScalaFutures {
+
+  private implicit val defaultPatience: PatienceConfig = PatienceConfig(10.seconds, interval = 500.millis)
+
+  private implicit val mat: Materializer = ActorMaterializer()
+  private implicit val ec: ExecutionContext = system.dispatcher
+  private implicit val logging: LoggingAdapter = Logging.getLogger(system, this)
+
+  override def afterAll(): Unit =
+    TestKit.shutdownActorSystem(system)
+
+  "mqtt source" should {
+    "receive subscribed messages" in assertAllStagesStopped {
+      val clientId = "source-spec/flow1"
+      val topic = "source-spec/topic1"
+
+      val settings = MqttSessionSettings()
+      val input = Vector("one", "two", "three", "four", "five")
+
+      val (subscribed, received) = MqttSource
+        .atMostOnce(settings, clientId, List(topic -> ControlPacketFlags.QoSAtLeastOnceDelivery))
+        .log("received", p => p.payload.utf8String)
+        .take(input.size)
+        .toMat(Sink.seq)(Keep.both)
+        .run()
+
+      subscribed.futureValue shouldBe Done
+
+      val session = ActorMqttClientSession(settings)
+      val connection = Tcp().outgoingConnection("localhost", 1883)
+      val commands =
+        Source
+          .queue(10, OverflowStrategy.fail)
+          .via(
+            Mqtt
+              .clientSessionFlow(session)
+              .join(connection)
+          )
+          .log("sender response")
+          .to(Sink.ignore)
+          .run()
+
+      commands.offer(Command(Connect(clientId + "_2", ConnectFlags.CleanSession)))
+      for {
+        data <- input
+      } session ! Command(
+        Publish(ControlPacketFlags.RETAIN, topic, ByteString(data))
+      )
+
+      received.futureValue.map(_.payload.utf8String) should contain theSameElementsAs input
+
+      commands.complete()
+      commands.watchCompletion().foreach(_ => session.shutdown())
+    }
+  }
+
+}
