87871feb9248c521d7e7582a5c713ef2f18db433
Enno <458526+ennru@users.noreply.github.com>
2019-04-02 14:21:14 +0200

    AWS S3: put an upper limit on upload chunks (#1619)
    ## Fixes
    
    Fixes #980 
    
    ## Purpose
    
    Split data streams to be uploaded to S3 if the chunk size would exceed the limit of 10 MB.
    
    ## Background Context
    
    AWS S3 has a lower and an upper size limit for chunks when uploading data. The lower limit of 5 MB is described in http://docs.aws.amazon.com/AmazonS3/latest/API/mpUploadUploadPart.html
    The upper limit is 10 MB.

--- s3/src/main/scala/akka/stream/alpakka/s3/impl/SplitAfterSize.scala
+++ s3/src/main/scala/akka/stream/alpakka/s3/impl/SplitAfterSize.scala
@@ -17,35 +17,53 @@ import akka.stream.stage.OutHandler
 import akka.stream.stage.InHandler
 import akka.stream.scaladsl.Flow
 
+import scala.annotation.tailrec
+
 /**
  * Internal Api
  *
  * Splits up a byte stream source into sub-flows of a minimum size. Does not attempt to create chunks of an exact size.
  */
 @InternalApi private[impl] object SplitAfterSize {
-  def apply[I, M](minChunkSize: Long)(in: Flow[I, ByteString, M]): SubFlow[ByteString, M, in.Repr, in.Closed] =
-    in.via(insertMarkers(minChunkSize)).splitWhen(_ == NewStream).collect { case bs: ByteString => bs }
+  def apply[I, M](minChunkSize: Int,
+                  maxChunkSize: Int)(in: Flow[I, ByteString, M]): SubFlow[ByteString, M, in.Repr, in.Closed] = {
+    require(minChunkSize < maxChunkSize, "the min chunk size must be smaller than the max chunk size")
+    in.via(insertMarkers(minChunkSize, maxChunkSize)).splitWhen(_ == NewStream).collect { case bs: ByteString => bs }
+  }
 
   private case object NewStream
 
-  private def insertMarkers(minChunkSize: Long) = new GraphStage[FlowShape[ByteString, Any]] {
+  private def insertMarkers(minChunkSize: Long, maxChunkSize: Int) = new GraphStage[FlowShape[ByteString, Any]] {
     val in = Inlet[ByteString]("SplitAfterSize.in")
     val out = Outlet[Any]("SplitAfterSize.out")
     override val shape = FlowShape.of(in, out)
 
     override def createLogic(inheritedAttributes: Attributes): GraphStageLogic =
       new GraphStageLogic(shape) with OutHandler with InHandler {
-        var count: Long = 0
+        var count: Int = 0
         override def onPull(): Unit = pull(in)
 
         override def onPush(): Unit = {
           val elem = grab(in)
           count += elem.size
-          if (count >= minChunkSize) {
+          if (count > maxChunkSize) {
+            splitElement(elem, elem.size - (count - maxChunkSize))
+          } else if (count >= minChunkSize) {
             count = 0
             emitMultiple(out, elem :: NewStream :: Nil)
           } else emit(out, elem)
         }
+
+        @tailrec private def splitElement(elem: ByteString, splitPos: Int): Unit =
+          if (elem.size > splitPos) {
+            val (part1, rest) = elem.splitAt(splitPos)
+            emitMultiple(out, part1 :: NewStream :: Nil)
+            splitElement(rest, maxChunkSize)
+          } else {
+            count = elem.size
+            emit(out, elem)
+          }
+
         setHandlers(in, out, this)
       }
   }
