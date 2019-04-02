a6f85d947f8001bd7e20529e895144c768844a34
Enno <458526+ennru@users.noreply.github.com>
2019-04-02 15:37:36 +0200

    Add DIR to all build steps

--- scripts/buildChanged.sh
+++ scripts/buildChanged.sh
@@ -3,13 +3,14 @@
 DIR=$1
 JDK=$2
 PRE_CMD=$3
+CMD=$4
 
 git diff origin/master --exit-code --quiet $DIR
 
 if [ $? -eq 1 ]
 then
   echo "Changes in ${DIR}"
-  jabba use ${JDK}
+  ./jabba use ${JDK}
   java -version
   $PRE_CMD
   sbt -jvm-opts .jvmopts-travis "$CMD"
