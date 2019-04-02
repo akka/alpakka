f73d85fa86c93a6462ba60a234c29daecb26c2da
Enno <458526+ennru@users.noreply.github.com>
2019-04-02 14:17:00 +0200

    Test for git changes via shell script

---
+++ scripts/buildChanged.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+DIR=$1
+JDK=$2
+PRE_CMD=$3
+
+git diff origin/master --exit-code --quiet $DIR
+
+if [ $? -eq 1 ]
+then
+  echo "Changes in ${DIR}"
+  jabba use ${JDK}
+  java -version
+  $PRE_CMD
+  sbt -jvm-opts .jvmopts-travis "$CMD"
+else
+  echo "No changes in ${DIR}"
+  exit 0
+fi
