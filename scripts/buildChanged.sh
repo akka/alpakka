82ef44d9fef40a8696f16e04481bfba47d95347f
Enno <458526+ennru@users.noreply.github.com>
2019-04-05 09:43:10 +0200

    Test for git changes via shell script (#1620)
    ## Purpose
    
    Make the decision if a project should be built in the shell to cut built time.
    
    ## Background Context
    
    With the increasing number of projects, the Alpakka build is running too long time.

---
+++ scripts/buildChanged.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+DIR=$1
+JDK=$2
+PRE_CMD=$3
+CMD=$4
+
+git diff origin/master --exit-code --quiet $DIR build.sbt project/ .travis.yml
+
+if [ $? -eq 1 ]
+then
+  echo "Changes in ${DIR}"
+  # using jabba for custom jdk management
+  curl -sL https://raw.githubusercontent.com/shyiko/jabba/0.11.2/install.sh | bash
+  . ~/.jabba/jabba.sh
+  jabba install ${JDK}
+  jabba use ${JDK}
+  java -version
+  $PRE_CMD
+  sbt -jvm-opts .jvmopts-travis "$CMD"
+else
+  echo "No changes in ${DIR}"
+  exit 0
+fi
