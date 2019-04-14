55a3a39258943d3672d4b9b47e207d7726809f0d
gkatzioura <gkatzioura@gmail.com>
2019-04-14 19:14:35 +0100

    Fixed influxdb source

--- influxdb/src/main/scala/akka/stream/alpakka/influxdb/javadsl/InfluxDBSource.scala
+++ influxdb/src/main/scala/akka/stream/alpakka/influxdb/javadsl/InfluxDBSource.scala
@@ -1,25 +1,39 @@
 package akka.stream.alpakka.influxdb.javadsl
 
+import java.util.concurrent.TimeUnit
+
 import akka.NotUsed
-import akka.stream.alpakka.influxdb.InfluxDBClientSettings
 import akka.stream.javadsl.Source
 import org.influxdb.InfluxDB
 import org.influxdb.dto.Query
-
-import scala.concurrent.Future
+import akka.stream.alpakka.influxdb.impl.InfluxDBSourceStage
+import akka.stream.alpakka.influxdb.impl
+import org.influxdb.impl.InfluxDBResultMapperHelper
 
 /**
  * Java API to create InfluxDB sources.
  */
 object InfluxDBSource {
 
-  /*
-  def create[M](
-             query: Query,
-             influxDB: InfluxDB,
-             influxDBClientSettings: InfluxDBClientSettings): Source[M,NotUsed] =
-    null
-    */
+  /**
+   * Java API: creates a [[InfluxDBSourceStage]] from a given statement.
+   */
+  def create[T](query: Query, influxDB: InfluxDB,clazz: Class[T]): Source[T, NotUsed] =
+    akka.stream.javadsl.Source.fromGraph(new InfluxDBSourceStage[T](query, influxDB,new RowReaderImpl[T](clazz)))
 
+  private final class RowReaderImpl[T](clazz: Class[T]) extends impl.RowReader[T] {
+
+    val mapperHelper = init()
+
+    def init(): InfluxDBResultMapperHelper = {
+      val resultMapperHelper = new InfluxDBResultMapperHelper
+      resultMapperHelper.cacheClassFields(classOf[T])
+      resultMapperHelper
+    }
+
+    override def convert(column: java.util.List[String],row: java.util.List[AnyRef]): T = {
+      mapperHelper.parseRowAs(classOf[T],column,row,TimeUnit.MILLISECONDS)
+    }
+  }
 
 }
