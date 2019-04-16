6cbeb437bfd058bca9727b3ea308ee293a05dff8
shimamoto <chibochibo@gmail.com>
2019-04-16 13:05:31 +0900

    HBase: add source

--- hbase/src/main/scala/akka/stream/alpakka/hbase/javadsl/HTableStage.scala
+++ hbase/src/main/scala/akka/stream/alpakka/hbase/javadsl/HTableStage.scala
@@ -7,9 +7,10 @@ package akka.stream.alpakka.hbase.javadsl
 import java.util.concurrent.CompletionStage
 
 import akka.stream.alpakka.hbase.HTableSettings
-import akka.stream.alpakka.hbase.impl.HBaseFlowStage
-import akka.stream.scaladsl.{Flow, Keep, Sink}
+import akka.stream.alpakka.hbase.impl.{HBaseFlowStage, HBaseSourceStage}
+import akka.stream.scaladsl.{Flow, Keep, Sink, Source}
 import akka.{Done, NotUsed}
+import org.apache.hadoop.hbase.client.{Result, Scan}
 
 import scala.compat.java8.FutureConverters._
 
@@ -28,5 +29,11 @@ object HTableStage {
    */
   def flow[A](settings: HTableSettings[A]): akka.stream.javadsl.Flow[A, A, NotUsed] =
     Flow.fromGraph(new HBaseFlowStage[A](settings)).asJava
+
+  /**
+   * Reads an element from HBase.
+   */
+  def source[A](scan: Scan, settings: HTableSettings[A]): akka.stream.javadsl.Source[Result, NotUsed] =
+    Source.fromGraph(new HBaseSourceStage[A](scan, settings)).asJava
 
 }
