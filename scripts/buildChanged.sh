294e8133c64e4fa91c51ca4fe51d848d488daced
Martynas Mickevičius <self@2m.lt>
2019-04-16 12:34:52 +0300

    Fix looking for changes when building master

--- scripts/buildChanged.sh
+++ scripts/buildChanged.sh
@@ -5,7 +5,18 @@ JDK=$2
 PRE_CMD=$3
 CMD=$4
 
-git diff origin/master --exit-code --quiet $DIR build.sbt project/ .travis.yml
+if [ "$TRAVIS_PULL_REQUEST" ]
+then
+  COMPARE_TO="origin/master"
+else
+  # for non-pr (master) builds, comparison for changes is made against the previous commit,
+  # which might be:
+  # * merge commit, then HEAD^ is the last commit before branching out
+  # * squash commit, then HEAD^ is the previous state of master
+  COMPARE_TO="HEAD^"
+fi
+
+git diff "$COMPARE_TO" --exit-code --quiet "$DIR" build.sbt project/ .travis.yml
 
 if [ $? -eq 1 ]
 then
@@ -13,12 +24,12 @@ then
   # using jabba for custom jdk management
   curl -sL https://raw.githubusercontent.com/shyiko/jabba/0.11.2/install.sh | bash
   . ~/.jabba/jabba.sh
-  jabba install ${JDK}
-  jabba use ${JDK}
+  jabba install "$JDK"
+  jabba use "$JDK"
   java -version
   $PRE_CMD
   sbt -jvm-opts .jvmopts-travis "$CMD"
 else
-  echo "No changes in ${DIR}"
+  echo "No changes in $DIR"
   exit 0
 fi
