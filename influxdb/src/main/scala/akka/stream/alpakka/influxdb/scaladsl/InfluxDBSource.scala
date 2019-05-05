c4a420bbeccb2e51fda9a957dd2a8aa7d738ee84
gkatzioura <gkatzioura@gmail.com>
2019-05-05 10:31:08 +0100

    Added influxdb source implementation for scala

--- influxdb/src/main/scala/akka/stream/alpakka/influxdb/scaladsl/InfluxDBSource.scala
+++ influxdb/src/main/scala/akka/stream/alpakka/influxdb/scaladsl/InfluxDBSource.scala
@@ -1,53 +1,44 @@
 package akka.stream.alpakka.influxdb.scaladsl
 
-import java.util.concurrent.TimeUnit
-
 import akka.NotUsed
+import akka.stream.alpakka.influxdb.InfluxDBSettings
+import akka.stream.alpakka.influxdb.impl.{InfluxDBRawSourceStage, InfluxDBSourceStage}
 import akka.stream.scaladsl.Source
 import org.influxdb.InfluxDB
 import org.influxdb.dto.{Query, QueryResult}
-import akka.stream.alpakka.influxdb.impl
-import akka.stream.alpakka.influxdb.impl.InfluxDBSourceStage
-import org.influxdb.impl.{InfluxDBResultMapper, InfluxDBResultMapperHelper}
-
-import scala.collection.JavaConverters._
 
 /**
- * Scala API to create InfluxDB sources.
+ * Scala API.
  */
 object InfluxDBSource {
 
   /**
-   * Scala API: creates a [[InfluxDBSourceStage]] from a given statement.
+   * Java API: creates an [[akka.stream.alpakka.influxdb.impl.InfluxDBRawSourceStage]] from a given statement.
    */
-  def apply[T](query: Query)(implicit influxDB: InfluxDB): Source[T, NotUsed] =
+  def apply(influxDB: InfluxDB,
+            query: Query
+           ): Source[QueryResult, NotUsed] =
     Source.fromGraph(
-      new impl.InfluxDBSourceStage[T](query,influxDB, new RowReaderImpl[T])
+      new InfluxDBRawSourceStage(
+        query,
+        influxDB
+      )
     )
 
-  private final class QueryResultReader[T] extends impl.MessageReader[List[T]] {
-    override def convert(queryResult: QueryResult): List[T] = {
-      val influxDBResultMapper = new InfluxDBResultMapper();
-      influxDBResultMapper.toPOJO(queryResult,classOf[T])
-        .asScala
-        .toList
-    }
-  }
-
-  private final class RowReaderImpl[T] extends impl.RowReader[T] {
-
-    val mapperHelper = init()
-
-    def init(): InfluxDBResultMapperHelper = {
-      val resultMapperHelper = new InfluxDBResultMapperHelper
-      resultMapperHelper.cacheClassFields(classOf[T])
-      resultMapperHelper
-    }
-
-    override def convert(column: java.util.List[String],row: java.util.List[AnyRef]): T = {
-      mapperHelper.parseRowAs(classOf[T],column,row,TimeUnit.MILLISECONDS)
-    }
-  }
+  /**
+   * Read elements of `T` from `className` or by `query`.
+   */
+  def typed[T](clazz: Class[T],
+               settings: InfluxDBSettings,
+               influxDB: InfluxDB,
+               query: Query): Source[T, NotUsed] =
+    Source.fromGraph(
+      new InfluxDBSourceStage[T](
+        clazz,
+        settings,
+        influxDB,
+        query
+      )
+    )
 
 }
-
