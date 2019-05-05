778c91ceba184b495844ccd7b06173821839483c
gkatzioura <gkatzioura@gmail.com>
2019-05-05 10:30:17 +0100

    Added influxdb source stage

--- influxdb/src/main/scala/akka/stream/alpakka/influxdb/impl/InfluxDBSourceStage.scala
+++ influxdb/src/main/scala/akka/stream/alpakka/influxdb/impl/InfluxDBSourceStage.scala
@@ -1,75 +1,130 @@
 package akka.stream.alpakka.influxdb.impl
 
+import java.util.concurrent.TimeUnit
+
 import akka.annotation.InternalApi
-import akka.stream.stage._
-import akka.stream.{ActorMaterializer, Attributes, Outlet, SourceShape}
+import akka.stream.alpakka.influxdb.InfluxDBSettings
+import akka.stream.{Attributes, Outlet, SourceShape}
+import akka.stream.stage.{GraphStage, GraphStageLogic, OutHandler}
 import org.influxdb.InfluxDB
 import org.influxdb.dto.{Query, QueryResult}
+import org.influxdb.impl.InfluxDBResultMapperHelper
+
 import scala.collection.JavaConverters._
 
-/**
- * INTERNAL API
- */
-@InternalApi
-private[influxdb] trait MessageReader[T] {
-  def convert(json: QueryResult): T
-}
+
 
 /**
  * INTERNAL API
  */
 @InternalApi
-private[influxdb] trait RowReader[T] {
-  def convert(columns: java.util.List[String],row: java.util.List[AnyRef]): T
-}
+private[influxdb] final class InfluxDBSourceStage[T](clazz: Class[T],
+                                                     settings: InfluxDBSettings,
+                                                     influxDB: InfluxDB,
+                                                     query: Query,
+                                                    ) extends GraphStage[SourceShape[T]] {
 
+  val out: Outlet[T] = Outlet("InfluxDB.out")
+  override val shape = SourceShape(out)
 
-/**
-  * INTERNAL API
-  */
-@InternalApi
-private[influxdb] final class InfluxDBSourceStage[T](query: Query,
-                                                               client: InfluxDB,
-                                                               reader: RowReader[T])
-    extends GraphStage[SourceShape[T]] {
-
-  val out: Outlet[T] = Outlet("InfluxDBSourceStage.out")
-  override val shape: SourceShape[T] = SourceShape(out)
-
-  override def createLogic(inheritedAttributes: Attributes): GraphStageLogic = new InfluxDBSourceLogic[T](query,client, out,shape)
+  override def createLogic(inheritedAttributes: Attributes): GraphStageLogic =
+    new InfluxDBSourceLogic[T](clazz, settings, influxDB, query, out, shape)
 
 }
 
 /**
   * INTERNAL API
   */
-private[influxdb] final class InfluxDBSourceLogic[T](query: Query,
-                                                     client: InfluxDB,
+@InternalApi
+private[influxdb] final class InfluxDBSourceLogic[T](clazz: Class[T],
+                                                     settings: InfluxDBSettings,
+                                                     influxDB: InfluxDB,
+                                                     query: Query,
                                                      outlet: Outlet[T],
-                                                     shape: SourceShape[T],
-                                                     reader: RowReader[T])
+                                                     shape: SourceShape[T])
   extends GraphStageLogic(shape)
     with OutHandler {
 
-  private var columns: java.util.List[String] = null;
-  private var rows : Iterator[java.util.List[AnyRef]] = Iterator()
+  setHandler(outlet, this)
+
+  var dataRetrieved: Option[QueryResult] = Option.empty
+  var resultMapperHelper: InfluxDBResultMapperHelper = _
 
   override def preStart(): Unit = {
-    val results = client.query(query)
-    columns = results.getResults.get(0).getSeries().get(0).getColumns;
-    rows = results.getResults.get(0).getSeries.get(0).getValues.asScala.toIterator
-  }
+    resultMapperHelper = new InfluxDBResultMapperHelper
+    resultMapperHelper.cacheClassFields(clazz)
 
-  setHandler(outlet,this)
-
-  override def onPull(): Unit = {
-    if(rows.hasNext) {
-      val row = rows.next();
-      val measure = reader.convert(columns,row)
-      emit(outlet, measure)
-    } else {
-      completeStage()
+    val queryResult = influxDB.query(query)
+    if(!queryResult.hasError) {
+      dataRetrieved = Option(queryResult)
     }
   }
+
+  override def onPull(): Unit = {
+    if (dataRetrieved.isEmpty)
+      completeStage()
+    else {
+      var queryResult = dataRetrieved.get
+      for {
+        result <- queryResult.getResults.asScala
+        series <- result.getSeries.asScala
+      } (
+        emitMultiple(outlet, resultMapperHelper.parseSeriesAs(clazz, series, settings.precision).asScala.toIterator)
+      )
+
+      dataRetrieved = Option.empty
+    }
+  }
+
 }
+
+/**
+ * INTERNAL API
+ */
+@InternalApi
+private[influxdb] final class InfluxDBRawSourceStage(query: Query,
+                                                     influxDB: InfluxDB
+                                                    ) extends GraphStage[SourceShape[QueryResult]] {
+
+  val out: Outlet[QueryResult] = Outlet("InfluxDB.out")
+  override val shape = SourceShape(out)
+
+  override def createLogic(inheritedAttributes: Attributes): GraphStageLogic =
+    new InfluxDBSourceRawLogic(query, influxDB, out, shape)
+
+}
+
+/**
+  * INTERNAL API
+  */
+@InternalApi
+private[influxdb] final class InfluxDBSourceRawLogic(query: Query,
+                                                     influxDB: InfluxDB,
+                                                     outlet: Outlet[QueryResult],
+                                                     shape: SourceShape[QueryResult])
+  extends GraphStageLogic(shape)
+    with OutHandler {
+
+  setHandler(outlet, this)
+
+  var dataRetrieved: Option[QueryResult] = Option.empty
+
+  override def preStart(): Unit = {
+    val queryResult = influxDB.query(query)
+    if(!queryResult.hasError) {
+      dataRetrieved = Option(queryResult)
+    }
+  }
+
+  override def onPull(): Unit = {
+    if(dataRetrieved.isEmpty) {
+      completeStage()
+    } else {
+      emit(outlet,dataRetrieved.get)
+      dataRetrieved = Option.empty
+    }
+  }
+
+}
+
 
