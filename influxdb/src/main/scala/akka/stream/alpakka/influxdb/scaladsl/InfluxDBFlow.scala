c9a0b06273b98622c1b8780e1f93fa83f08c2360
gkatzioura <gkatzioura@gmail.com>
2019-05-05 10:31:54 +0100

    Added scala flow for influxdb connector

---
+++ influxdb/src/main/scala/akka/stream/alpakka/influxdb/scaladsl/InfluxDBFlow.scala
@@ -0,0 +1,93 @@
+package akka.stream.alpakka.influxdb.scaladsl
+
+import akka.NotUsed
+import akka.stream.alpakka.influxdb.{InfluxDBSettings, InfluxDBWriteMessage, InfluxDBWriteResult, impl}
+import akka.stream.scaladsl.Flow
+import org.influxdb.InfluxDB
+
+import scala.collection.immutable
+/**
+ * Scala API to create InfluxDB flows.
+ */
+object InfluxDBFlow {
+
+  def create[T](settings: InfluxDBSettings)(
+      implicit influxDB: InfluxDB
+  ): Flow[InfluxDBWriteMessage[T, NotUsed], InfluxDBWriteResult[T, NotUsed], NotUsed] =
+    Flow[InfluxDBWriteMessage[T, NotUsed]]
+      .batch(settings.batchSize, immutable.Seq(_)) { case (seq, wm) => seq :+ wm }
+      .via(
+        new impl.InfluxDBFlowStage[T, NotUsed](
+          Option.empty,
+          influxDB
+        )
+      ).mapConcat(identity)
+
+  def typed[T](clazz: Class[T], settings: InfluxDBSettings)(
+    implicit influxDB: InfluxDB
+  ): Flow[InfluxDBWriteMessage[T, NotUsed], InfluxDBWriteResult[T, NotUsed], NotUsed] =
+    Flow[InfluxDBWriteMessage[T, NotUsed]]
+      .batch(settings.batchSize, immutable.Seq(_)) { case (seq, wm) => seq :+ wm }
+      .via(
+        new impl.InfluxDBFlowStage[T,NotUsed](
+          Option(clazz),
+          influxDB
+        )
+      )
+    .mapConcat(identity)
+
+  def createWithPassThrough[T, C](settings: InfluxDBSettings)(
+      implicit influxDB: InfluxDB
+  ): Flow[InfluxDBWriteMessage[T, C], InfluxDBWriteResult[T, C], NotUsed] =
+    Flow[InfluxDBWriteMessage[T, C]]
+      .batch(settings.batchSize, immutable.Seq(_)) { case (seq, wm) => seq :+ wm }
+      .via(
+        new impl.InfluxDBFlowStage[T, C](
+          Option.empty,
+          influxDB
+        )
+      )
+      .mapConcat(identity)
+
+  def typedWithPassThrough[T, C](clazz: Class[T], settings: InfluxDBSettings)(
+      implicit influxDB: InfluxDB
+  ): Flow[InfluxDBWriteMessage[T, C], InfluxDBWriteResult[T, C], NotUsed] =
+    Flow[InfluxDBWriteMessage[T, C]]
+      .batch(settings.batchSize, immutable.Seq(_)) { case (seq, wm) => seq :+ wm }
+      .via(
+        new impl.InfluxDBFlowStage[T, C](
+          Option(clazz),
+          influxDB
+        )
+      )
+      .mapConcat(identity)
+
+  def createWithContext[T, C](settings: InfluxDBSettings)(
+      implicit influxDB: InfluxDB
+  ): Flow[(InfluxDBWriteMessage[T, NotUsed], C), (InfluxDBWriteResult[T, C], C), NotUsed] = {
+    Flow[(InfluxDBWriteMessage[T, NotUsed], C)]
+      .map {
+        case (wm, pt) =>
+          InfluxDBWriteMessage(wm.point,pt)
+      }
+      .via(createWithPassThrough(settings))
+      .map { wr =>
+        (wr, wr.writeMessage.passThrough)
+      }
+  }
+
+  def typedWithContext[T, C](clazz: Class[T], settings: InfluxDBSettings)(
+      implicit influxDB: InfluxDB
+  ): Flow[(InfluxDBWriteMessage[T, NotUsed], C), (InfluxDBWriteResult[T, C], C), NotUsed] = {
+    Flow[(InfluxDBWriteMessage[T, NotUsed], C)]
+      .map {
+        case (wm, pt) =>
+          InfluxDBWriteMessage(wm.point,pt)
+      }
+      .via(typedWithPassThrough(clazz, settings))
+      .map { wr =>
+        (wr, wr.writeMessage.passThrough)
+      }
+  }
+
+}
