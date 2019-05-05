f7ba613688f0cb153b2d0f1f2ec8742fe6cb6d53
gkatzioura <gkatzioura@gmail.com>
2019-05-05 10:29:45 +0100

    Added models for influxdb write operations

---
+++ influxdb/src/main/scala/akka/stream/alpakka/influxdb/InfluxDBWriteMessage.scala
@@ -0,0 +1,22 @@
+package akka.stream.alpakka.influxdb
+
+import akka.NotUsed
+
+object InfluxDBWriteMessage {
+  // Apply method to use when not using passThrough
+  def apply[T](point: T): InfluxDBWriteMessage[T, NotUsed] =
+    InfluxDBWriteMessage(point, NotUsed)
+
+  // Java-api - without passThrough
+  def create[T](point: T): InfluxDBWriteMessage[T, NotUsed] =
+    InfluxDBWriteMessage(point, NotUsed)
+
+  // Java-api - with passThrough
+  def create[T, C](point: T, passThrough: C) =
+    InfluxDBWriteMessage(point, passThrough)
+}
+
+final case class InfluxDBWriteMessage[T, C](point: T, passThrough: C)
+
+final case class InfluxDBWriteResult[T,C](writeMessage : InfluxDBWriteMessage[T,C], error: Option[String])
+
