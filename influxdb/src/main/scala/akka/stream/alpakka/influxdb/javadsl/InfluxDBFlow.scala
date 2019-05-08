54c09c88513bc92f38177906947ec8675b629999
gkatzioura <gkatzioura@gmail.com>
2019-05-08 23:05:17 +0100

    Added imports to clean flow for InfluxDB connector

--- influxdb/src/main/scala/akka/stream/alpakka/influxdb/javadsl/InfluxDBFlow.scala
+++ influxdb/src/main/scala/akka/stream/alpakka/influxdb/javadsl/InfluxDBFlow.scala
@@ -7,46 +7,48 @@ package akka.stream.alpakka.influxdb.javadsl
 import akka.NotUsed
 import akka.stream.alpakka.influxdb.{InfluxDBSettings, InfluxDBWriteMessage, InfluxDBWriteResult}
 import org.influxdb.InfluxDB
+import akka.stream.javadsl.Flow
+import akka.stream.alpakka.influxdb.scaladsl
 
 object InfluxDBFlow {
 
   def create[T](
       settings: InfluxDBSettings,
       influxDB: InfluxDB
-  ): akka.stream.javadsl.Flow[InfluxDBWriteMessage[T, NotUsed], InfluxDBWriteResult[T, NotUsed], NotUsed] =
-    akka.stream.alpakka.influxdb.scaladsl.InfluxDBFlow.create(settings)(influxDB).asJava
+  ): Flow[InfluxDBWriteMessage[T, NotUsed], InfluxDBWriteResult[T, NotUsed], NotUsed] =
+    scaladsl.InfluxDBFlow.create(settings)(influxDB).asJava
 
   def typed[T](
       clazz: Class[T],
       settings: InfluxDBSettings,
       influxDB: InfluxDB
-  ): akka.stream.javadsl.Flow[InfluxDBWriteMessage[T, NotUsed], InfluxDBWriteResult[T, NotUsed], NotUsed] =
-    akka.stream.alpakka.influxdb.scaladsl.InfluxDBFlow.typed(clazz, settings)(influxDB).asJava
+  ): Flow[InfluxDBWriteMessage[T, NotUsed], InfluxDBWriteResult[T, NotUsed], NotUsed] =
+    scaladsl.InfluxDBFlow.typed(clazz, settings)(influxDB).asJava
 
   def createWithPassThrough[T, C](
       settings: InfluxDBSettings,
       influxDB: InfluxDB
-  ): akka.stream.javadsl.Flow[InfluxDBWriteMessage[T, C], InfluxDBWriteResult[T, C], NotUsed] =
-    akka.stream.alpakka.influxdb.scaladsl.InfluxDBFlow.createWithPassThrough(settings)(influxDB).asJava
+  ): Flow[InfluxDBWriteMessage[T, C], InfluxDBWriteResult[T, C], NotUsed] =
+    scaladsl.InfluxDBFlow.createWithPassThrough(settings)(influxDB).asJava
 
   def typedWithPassThrough[T, C](
       clazz: Class[T],
       settings: InfluxDBSettings,
       influxDB: InfluxDB
-  ): akka.stream.javadsl.Flow[InfluxDBWriteMessage[T, C], InfluxDBWriteResult[T, C], NotUsed] =
-    akka.stream.alpakka.influxdb.scaladsl.InfluxDBFlow.typedWithPassThrough(clazz, settings)(influxDB).asJava
+  ): Flow[InfluxDBWriteMessage[T, C], InfluxDBWriteResult[T, C], NotUsed] =
+    scaladsl.InfluxDBFlow.typedWithPassThrough(clazz, settings)(influxDB).asJava
 
   def createWithContext[T, C](
       settings: InfluxDBSettings,
       influxDB: InfluxDB
-  ): akka.stream.javadsl.Flow[(InfluxDBWriteMessage[T, NotUsed], C), (InfluxDBWriteResult[T, C], C), NotUsed] =
-    akka.stream.alpakka.influxdb.scaladsl.InfluxDBFlow.createWithContext(settings)(influxDB).asJava
+  ): Flow[(InfluxDBWriteMessage[T, NotUsed], C), (InfluxDBWriteResult[T, C], C), NotUsed] =
+    scaladsl.InfluxDBFlow.createWithContext(settings)(influxDB).asJava
 
   def typedWithContext[T, C](
       clazz: Class[T],
       settings: InfluxDBSettings,
       influxDB: InfluxDB
-  ): akka.stream.javadsl.Flow[(InfluxDBWriteMessage[T, NotUsed], C), (InfluxDBWriteResult[T, C], C), NotUsed] =
-    akka.stream.alpakka.influxdb.scaladsl.InfluxDBFlow.typedWithContext(clazz, settings)(influxDB).asJava
+  ): Flow[(InfluxDBWriteMessage[T, NotUsed], C), (InfluxDBWriteResult[T, C], C), NotUsed] =
+    scaladsl.InfluxDBFlow.typedWithContext(clazz, settings)(influxDB).asJava
 
 }
