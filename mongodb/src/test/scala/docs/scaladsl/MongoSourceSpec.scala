fd42e1a9c596cf83c2bd2b3f060dc7a14a2379b1
Shivansh Srivastava <shiv4nsh@gmail.com>
2019-05-10 07:50:01 +0100

    Mongo DB: Add missing imports to docs. (#1689)

--- mongodb/src/test/scala/docs/scaladsl/MongoSourceSpec.scala
+++ mongodb/src/test/scala/docs/scaladsl/MongoSourceSpec.scala
@@ -43,6 +43,7 @@ class MongoSourceSpec
   // #pojo
 
   // #codecs
+  import org.bson.codecs.configuration.CodecRegistries.{fromProviders, fromRegistries}
   import org.mongodb.scala.bson.codecs.DEFAULT_CODEC_REGISTRY
   import org.mongodb.scala.bson.codecs.Macros._
 
