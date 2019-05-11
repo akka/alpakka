a12867c9bededf320cf211fd3d55c1e4bf0042aa
gkatzioura <gkatzioura@gmail.com>
2019-05-11 17:01:17 +0100

    Added information on database and retention policy on InfluxDBWriteMessage for InfluxDB connector

--- influxdb/src/main/scala/akka/stream/alpakka/influxdb/InfluxDBWriteMessage.scala
+++ influxdb/src/main/scala/akka/stream/alpakka/influxdb/InfluxDBWriteMessage.scala
@@ -9,7 +9,7 @@ import akka.NotUsed
 object InfluxDBWriteMessage {
   // Apply method to use when not using passThrough
   def apply[T](point: T): InfluxDBWriteMessage[T, NotUsed] =
-    InfluxDBWriteMessage(point, NotUsed)
+    InfluxDBWriteMessage(point = point, passThrough = NotUsed)
 
   // Java-api - without passThrough
   def create[T](point: T): InfluxDBWriteMessage[T, NotUsed] =
@@ -20,6 +20,30 @@ object InfluxDBWriteMessage {
     InfluxDBWriteMessage(point, passThrough)
 }
 
-final case class InfluxDBWriteMessage[T, C](point: T, passThrough: C)
+final case class InfluxDBWriteMessage[T, C](point: T,
+                                            passThrough: C,
+                                            databaseName: Option[String] = None,
+                                            retentionPolicy: Option[String] = None) {
+
+  def withPoint(point: T): InfluxDBWriteMessage[T, C] =
+    copy(point = point)
+
+  def withDatabaseName(databaseName: String): InfluxDBWriteMessage[T, C] =
+    copy(databaseName = Some(databaseName))
+
+  def withRetentionPolicy(retentionPolicy: String): InfluxDBWriteMessage[T, C] =
+    copy(retentionPolicy = Some(retentionPolicy))
+
+  private def copy(
+      point: T = point,
+      passThrough: C = passThrough,
+      databaseName: Option[String] = databaseName,
+      retentionPolicy: Option[String] = retentionPolicy
+  ): InfluxDBWriteMessage[T, C] =
+    new InfluxDBWriteMessage(point = point,
+                             passThrough = passThrough,
+                             databaseName = databaseName,
+                             retentionPolicy = retentionPolicy)
+}
 
 final case class InfluxDBWriteResult[T, C](writeMessage: InfluxDBWriteMessage[T, C], error: Option[String])
