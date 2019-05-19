fb3f93d108aa98ff3d7fc1fa4ddce883c4b9dc56
gkatzioura <gkatzioura@gmail.com>
2019-05-19 11:28:58 +0100

    Separated logic on flow for InfluxDb connector

--- influxdb/src/main/scala/akka/stream/alpakka/influxdb/impl/InfluxDbFlowStage.scala
+++ influxdb/src/main/scala/akka/stream/alpakka/influxdb/impl/InfluxDbFlowStage.scala
@@ -31,84 +31,44 @@ private[influxdb] class InfluxDbFlowStage[T, C](
 
   override def createLogic(inheritedAttributes: Attributes): GraphStageLogic =
     clazz match {
-      case Some(c) => new InfluxDbMapperRecordLogic
-      case None => new InfluxDbRecordLogic
+      case Some(c) => new InfluxDbMapperRecordLogic(influxDB, in, out, shape)
+      case None => new InfluxDbRecordLogic(influxDB, in, out, shape)
     }
 
-  sealed abstract class InfluxDbLogic extends GraphStageLogic(shape) with InHandler with OutHandler {
+}
 
-    protected def write(messages: immutable.Seq[InfluxDbWriteMessage[T, C]]): Unit
+private[influxdb] sealed abstract class InfluxDbLogic[T, C](
+    influxDB: InfluxDB,
+    in: Inlet[immutable.Seq[InfluxDbWriteMessage[T, C]]],
+    out: Outlet[immutable.Seq[InfluxDbWriteResult[T, C]]],
+    shape: FlowShape[immutable.Seq[InfluxDbWriteMessage[T, C]], immutable.Seq[InfluxDbWriteResult[T, C]]]
+) extends GraphStageLogic(shape)
+    with InHandler
+    with OutHandler {
 
-    setHandlers(in, out, this)
+  setHandlers(in, out, this)
 
-    override def onPull(): Unit = if (!isClosed(in) && !hasBeenPulled(in)) pull(in)
+  protected def write(messages: immutable.Seq[InfluxDbWriteMessage[T, C]]): Unit
 
-    override def onPush(): Unit = {
-      val messages = grab(in)
-      if (messages.nonEmpty) {
+  override def onPull(): Unit = if (!isClosed(in) && !hasBeenPulled(in)) pull(in)
 
-        influxDB.enableBatch(BatchOptions.DEFAULTS)
-        write(messages)
-        val writtenMessages = messages.map(m => new InfluxDbWriteResult(m, None))
-        influxDB.close()
-        push(out, writtenMessages)
-      }
+  override def onPush(): Unit = {
+    val messages = grab(in)
+    if (messages.nonEmpty) {
 
-      tryPull(in)
+      influxDB.enableBatch(BatchOptions.DEFAULTS)
+      write(messages)
+      val writtenMessages = messages.map(m => new InfluxDbWriteResult(m, None))
+      influxDB.close()
+      push(out, writtenMessages)
     }
+
+    tryPull(in)
   }
 
-  final class InfluxDbRecordLogic extends InfluxDbLogic {
-
-    override protected def write(messages: immutable.Seq[InfluxDbWriteMessage[T, C]]): Unit =
-      messages
-        .filter {
-          case InfluxDbWriteMessage(_: Point, _, _, _) => {
-            true
-          }
-          case InfluxDbWriteMessage(_: AnyRef, _, _, _) => {
-            failStage(new RuntimeException(s"unexpected type Point required"))
-            false
-          }
-        }
-        .groupBy(im => (im.databaseName, im.retentionPolicy))
-        .map(wm => toBatchPoints(wm._1._1, wm._1._2, wm._2))
-        .foreach(influxDB.write)
-
-  }
-
-  final class InfluxDbMapperRecordLogic extends InfluxDbLogic {
-
-    private val mapperHelper: InfluxDbResultMapperHelper = new InfluxDbResultMapperHelper
-
-    override protected def write(messages: immutable.Seq[InfluxDbWriteMessage[T, C]]): Unit =
-      messages
-        .groupBy(groupByDbRp)
-        .map(convertToBatchPoints)
-        .foreach(influxDB.write)
-
-    def groupByDbRp(im: InfluxDbWriteMessage[T, C]) =
-      (
-        im.databaseName match {
-          case dbn: Some[String] => dbn
-          case None => Some(mapperHelper.databaseName(im.point))
-        },
-        im.retentionPolicy match {
-          case dbn: Some[String] => dbn
-          case None => Some(mapperHelper.retentionPolicy(im.point))
-        }
-      )
-
-    def convertToBatchPoints(wm: ((Some[String], Some[String]), immutable.Seq[InfluxDbWriteMessage[T, C]])) =
-      toBatchPoints(wm._1._1,
-                    wm._1._2,
-                    wm._2.map(im => im.withPoint(mapperHelper.convertModelToPoint(im.point).asInstanceOf[T])))
-  }
-
-  private def toBatchPoints(databaseName: Option[String],
-                            retentionPolicy: Option[String],
-                            seq: Seq[InfluxDbWriteMessage[T, C]]) = {
-
+  protected def toBatchPoints(databaseName: Option[String],
+                              retentionPolicy: Option[String],
+                              seq: Seq[InfluxDbWriteMessage[T, C]]) = {
     val builder = databaseName match {
       case Some(databaseName) => BatchPoints.database(databaseName)
       case None => BatchPoints.builder()
@@ -128,4 +88,60 @@ private[influxdb] class InfluxDbFlowStage[T, C](
     convert(seq)
   }
 
+}
+
+private[influxdb] final class InfluxDbRecordLogic[T, C](
+    influxDB: InfluxDB,
+    in: Inlet[immutable.Seq[InfluxDbWriteMessage[T, C]]],
+    out: Outlet[immutable.Seq[InfluxDbWriteResult[T, C]]],
+    shape: FlowShape[immutable.Seq[InfluxDbWriteMessage[T, C]], immutable.Seq[InfluxDbWriteResult[T, C]]]
+) extends InfluxDbLogic(influxDB, in, out, shape) {
+
+  override protected def write(messages: immutable.Seq[InfluxDbWriteMessage[T, C]]): Unit =
+    messages
+      .filter {
+        case InfluxDbWriteMessage(_: Point, _, _, _) => {
+          true
+        }
+        case InfluxDbWriteMessage(_: AnyRef, _, _, _) => {
+          failStage(new RuntimeException(s"unexpected type Point required"))
+          false
+        }
+      }
+      .groupBy(im => (im.databaseName, im.retentionPolicy))
+      .map(wm => toBatchPoints(wm._1._1, wm._1._2, wm._2))
+      .foreach(influxDB.write)
+}
+
+private[influxdb] final class InfluxDbMapperRecordLogic[T, C](
+    influxDB: InfluxDB,
+    in: Inlet[immutable.Seq[InfluxDbWriteMessage[T, C]]],
+    out: Outlet[immutable.Seq[InfluxDbWriteResult[T, C]]],
+    shape: FlowShape[immutable.Seq[InfluxDbWriteMessage[T, C]], immutable.Seq[InfluxDbWriteResult[T, C]]]
+) extends InfluxDbLogic(influxDB, in, out, shape) {
+
+  private val mapperHelper: InfluxDbResultMapperHelper = new InfluxDbResultMapperHelper
+
+  override protected def write(messages: immutable.Seq[InfluxDbWriteMessage[T, C]]): Unit =
+    messages
+      .groupBy(groupByDbRp)
+      .map(convertToBatchPoints)
+      .foreach(influxDB.write)
+
+  def groupByDbRp(im: InfluxDbWriteMessage[T, C]) =
+    (
+      im.databaseName match {
+        case dbn: Some[String] => dbn
+        case None => Some(mapperHelper.databaseName(im.point))
+      },
+      im.retentionPolicy match {
+        case dbn: Some[String] => dbn
+        case None => Some(mapperHelper.retentionPolicy(im.point))
+      }
+    )
+
+  def convertToBatchPoints(wm: ((Some[String], Some[String]), immutable.Seq[InfluxDbWriteMessage[T, C]])) =
+    toBatchPoints(wm._1._1,
+                  wm._1._2,
+                  wm._2.map(im => im.withPoint(mapperHelper.convertModelToPoint(im.point).asInstanceOf[T])))
 }
