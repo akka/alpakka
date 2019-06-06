7904b6817164b0d57f6972d03d62e054371e0b7e
gipeshka <gipeshka@gmail.com>
2019-06-06 18:30:36 +0200

    code style + 2.11 compatibility

--- sqs/src/main/scala/akka/stream/alpakka/sqs/ControlledThrottling.scala
+++ sqs/src/main/scala/akka/stream/alpakka/sqs/ControlledThrottling.scala
@@ -1,3 +1,7 @@
+/*
+ * Copyright (C) 2016-2019 Lightbend Inc. <http://www.lightbend.com>
+ */
+
 package akka.stream.alpakka.sqs
 
 import java.time.Clock
@@ -11,43 +15,42 @@ import scala.concurrent.duration.FiniteDuration
 class ControlledThrottling[T](clock: Clock, delay: FiniteDuration) extends SimpleLinearGraphStage[T] {
   private var isThrottling: Boolean = false
 
-  def setThrottling(shouldBeThrottled: Boolean): Unit = {
+  def setThrottling(shouldBeThrottled: Boolean): Unit =
     isThrottling = shouldBeThrottled
-  }
 
-  def createLogic(inheritedAttributes: Attributes): GraphStageLogic = new TimerGraphStageLogic(shape) with InHandler with OutHandler {
+  def createLogic(inheritedAttributes: Attributes): GraphStageLogic =
+    new TimerGraphStageLogic(shape) with InHandler with OutHandler {
 
-    private case object DelayTimerKey
+      private case object DelayTimerKey
 
-    private var lastPull: Long = 0
+      private var lastPull: Long = 0
 
-    def onPush(): Unit = {
-      val elem = grab(in)
+      def onPush(): Unit = {
+        val elem = grab(in)
 
-      push(out, elem)
-    }
+        push(out, elem)
+      }
 
-    def onPull(): Unit = delayfulPull()
+      def onPull(): Unit = delayfulPull()
 
-    override def onTimer(timerKey: Any): Unit = timerKey match {
-      case DelayTimerKey => delayfulPull()
-    }
+      override def onTimer(timerKey: Any): Unit = timerKey match {
+        case DelayTimerKey => delayfulPull()
+      }
 
-    private def delayfulPull(): Unit = {
-      if (isThrottling) {
-        if (clock.millis() - lastPull < delay.toMillis) {
-          scheduleOnce(DelayTimerKey, delay)
+      private def delayfulPull(): Unit =
+        if (isThrottling) {
+          if (clock.millis() - lastPull < delay.toMillis) {
+            scheduleOnce(DelayTimerKey, delay)
+          } else {
+            lastPull = clock.millis()
+            pull(in)
+          }
         } else {
           lastPull = clock.millis()
           pull(in)
         }
-      } else {
-        lastPull = clock.millis()
-        pull(in)
-      }
-    }
 
-    setHandler(out, this)
-    setHandler(in, this)
-  }
+      setHandler(out, this)
+      setHandler(in, this)
+    }
 }
