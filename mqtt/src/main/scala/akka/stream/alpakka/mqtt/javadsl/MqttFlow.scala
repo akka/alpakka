872d76d27ffaaa00ea10dad1cbe64ff5eb23b9cd
nsandroni <51410599+nsandroni@users.noreply.github.com>
2019-06-20 14:01:01 +0200

    Ack MqttMessage in Sink Flow (#1747)
    * add ack after publish
    * documentation for MqttFlow.atLeastOnceWithAck

--- mqtt/src/main/scala/akka/stream/alpakka/mqtt/javadsl/MqttFlow.scala
+++ mqtt/src/main/scala/akka/stream/alpakka/mqtt/javadsl/MqttFlow.scala
@@ -7,7 +7,7 @@ package akka.stream.alpakka.mqtt.javadsl
 import java.util.concurrent.CompletionStage
 
 import akka.Done
-import akka.stream.alpakka.mqtt._
+import akka.stream.alpakka.mqtt.{javadsl, _}
 import akka.stream.javadsl.Flow
 
 import scala.compat.java8.FutureConverters._
@@ -105,6 +105,26 @@ object MqttFlow {
   ): Flow[MqttMessage, MqttMessageWithAck, CompletionStage[Done]] =
     scaladsl.MqttFlow
       .atLeastOnce(settings, subscriptions, bufferSize, defaultQos)
+      .map(MqttMessageWithAck.toJava)
+      .mapMaterializedValue(_.toJava)
+      .asJava
+
+  /**
+   * Create a flow to send messages to MQTT , send acknowledge AND subscribe to MQTT messages with a commit handle to acknowledge message reception.
+   *
+   * The materialized value completes on successful connection to the MQTT broker.
+   *
+   * @param bufferSize max number of messages read from MQTT before back-pressure applies
+   * @param defaultQos Quality of service level applied for messages not specifying a message specific value
+   */
+  def atLeastOnceWithAck(
+      settings: MqttConnectionSettings,
+      subscriptions: MqttSubscriptions,
+      bufferSize: Int,
+      defaultQos: MqttQoS
+  ): Flow[MqttMessageWithAck, MqttMessageWithAck, CompletionStage[Done]] =
+    scaladsl.MqttFlow
+      .atLeastOnceWithAckForJava(settings, subscriptions, bufferSize, defaultQos)
       .map(MqttMessageWithAck.toJava)
       .mapMaterializedValue(_.toJava)
       .asJava
--- mqtt/src/main/scala/akka/stream/alpakka/mqtt/scaladsl/MqttMessageWithAck.scala
+++ mqtt/src/main/scala/akka/stream/alpakka/mqtt/scaladsl/MqttMessageWithAck.scala
@@ -5,8 +5,10 @@
 package akka.stream.alpakka.mqtt.scaladsl
 
 import akka.Done
+import akka.annotation.InternalApi
 import akka.stream.alpakka.mqtt.MqttMessage
 
+import scala.compat.java8.FutureConverters
 import scala.concurrent.Future
 
 /**
@@ -30,4 +32,20 @@ trait MqttMessageWithAck {
    * @return a future indicating, if the acknowledge reached MQTT
    */
   def ack(): Future[Done]
+}
+/*
+ * INTERNAL API
+ */
+@InternalApi
+private[scaladsl] object MqttMessageWithAck {
+  def fromJava(e: akka.stream.alpakka.mqtt.javadsl.MqttMessageWithAck): MqttMessageWithAck = new MqttMessageWithAck {
+    override val message: MqttMessage = e.message
+
+    /**
+     * Signals `messageArrivedComplete` to MQTT.
+     *
+     * @return a future indicating, if the acknowledge reached MQTT
+     */
+    override def ack(): Future[Done] = FutureConverters.toScala(e.ack)
+  }
 }
