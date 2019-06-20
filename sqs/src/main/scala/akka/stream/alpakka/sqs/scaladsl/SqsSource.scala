2ca6d413ec7ef4fe1fb1b5c971e1f1136b17a9d8
gipeshka <gipeshka@gmail.com>
2019-06-20 09:09:10 +0200

    1740 map async now ordered

--- sqs/src/main/scala/akka/stream/alpakka/sqs/scaladsl/SqsSource.scala
+++ sqs/src/main/scala/akka/stream/alpakka/sqs/scaladsl/SqsSource.scala
@@ -7,7 +7,8 @@ package akka.stream.alpakka.sqs.scaladsl
 import akka._
 import akka.stream._
 import akka.stream.alpakka.sqs.SqsSourceSettings
-import akka.stream.alpakka.sqs.impl.BalancingMapAsyncUnordered
+import akka.stream.alpakka.sqs.impl.BalancingMapAsync
+import akka.stream.impl.fusing.MapAsyncUnordered
 import akka.stream.scaladsl.Source
 import software.amazon.awssdk.services.sqs.SqsAsyncClient
 import software.amazon.awssdk.services.sqs.model._
@@ -43,15 +44,24 @@ object SqsSource {
           case Some(t) => requestBuilder.visibilityTimeout(t.toSeconds.toInt).build()
         }
       }
-      .via(
-        BalancingMapAsyncUnordered[ReceiveMessageRequest, ReceiveMessageResponse](
-          settings.parallelRequests,
-          sqsClient.receiveMessage(_).toScala,
-          response => if (response.messages().isEmpty) 1 else settings.parallelRequests
-        )
-      )
+      .via(resolveHandler(settings.parallelRequests))
       .map(_.messages().asScala.toList)
       .takeWhile(messages => !settings.closeOnEmptyReceive || messages.nonEmpty)
       .mapConcat(identity)
       .buffer(settings.maxBufferSize, OverflowStrategy.backpressure)
+
+  private def resolveHandler(parallelism: Int)(implicit sqsClient: SqsAsyncClient) = {
+    if (parallelism == 1) {
+      MapAsyncUnordered[ReceiveMessageRequest, ReceiveMessageResponse](
+        parallelism,
+        sqsClient.receiveMessage(_).toScala
+      )
+    } else {
+      BalancingMapAsync[ReceiveMessageRequest, ReceiveMessageResponse](
+        parallelism,
+        sqsClient.receiveMessage(_).toScala,
+        (response, _) => if (response.messages().isEmpty) 1 else parallelism
+      )
+    }
+  }
 }
