bfdf3153673c284372164bab4d7b0cc956572052
Marc Karassev <markarasev@protonmail.com>
2019-08-05 14:50:43 +0200

    HDFS: fix target file overwrite (#1834)
    * Fixed HDFS target file overwrite.
    * propagate overwrite information to HdfsWriter base trait
    * made child classes override this property
    * deleted existing file if overwrite is enabled in HdfsWriter#moveToTarget function
    * [Boyscout] Explicitely overrided inherited properties from HdfsWriter in child classes for better code structure understanding.

--- hdfs/src/main/scala/akka/stream/alpakka/hdfs/impl/writer/DataWriter.scala
+++ hdfs/src/main/scala/akka/stream/alpakka/hdfs/impl/writer/DataWriter.scala
@@ -15,30 +15,30 @@ import org.apache.hadoop.fs.{FSDataOutputStream, FileSystem, Path}
  */
 @InternalApi
 private[writer] final case class DataWriter(
-    fs: FileSystem,
-    pathGenerator: FilePathGenerator,
+    override val fs: FileSystem,
+    override val pathGenerator: FilePathGenerator,
     maybeTargetPath: Option[Path],
-    overwrite: Boolean
+    override val overwrite: Boolean
 ) extends HdfsWriter[FSDataOutputStream, ByteString] {
 
-  protected lazy val target: Path =
+  override protected lazy val target: Path =
     getOrCreatePath(maybeTargetPath, createTargetPath(pathGenerator, 0))
 
-  def sync(): Unit = output.hsync()
+  override def sync(): Unit = output.hsync()
 
-  def write(input: ByteString, separator: Option[Array[Byte]]): Long = {
+  override def write(input: ByteString, separator: Option[Array[Byte]]): Long = {
     val bytes = input.toArray
     output.write(bytes)
     separator.foreach(output.write)
     output.size()
   }
 
-  def rotate(rotationCount: Long): DataWriter = {
+  override def rotate(rotationCount: Long): DataWriter = {
     output.close()
     copy(maybeTargetPath = Some(createTargetPath(pathGenerator, rotationCount)))
   }
 
-  def create(fs: FileSystem, file: Path): FSDataOutputStream = fs.create(file, overwrite)
+  override def create(fs: FileSystem, file: Path): FSDataOutputStream = fs.create(file, overwrite)
 
 }
 
