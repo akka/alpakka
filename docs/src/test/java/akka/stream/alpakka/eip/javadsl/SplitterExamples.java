3b5783fe56b74eaf07edc3d860095d8d4a9c0762
Enno <458526+ennru@users.noreply.github.com>
2019-12-19 18:04:10 +0100

    sbt 1.3.6 and new style check

--- docs/src/test/java/akka/stream/alpakka/eip/javadsl/SplitterExamples.java
+++ docs/src/test/java/akka/stream/alpakka/eip/javadsl/SplitterExamples.java
@@ -20,66 +20,66 @@ import java.util.concurrent.CompletionStage;
 import java.util.concurrent.ExecutionException;
 
 public class SplitterExamples {
-    private static ActorSystem system;
-    private static Materializer materializer;
+  private static ActorSystem system;
+  private static Materializer materializer;
 
-    @Test
-    public void simpleSplit() throws ExecutionException, InterruptedException {
-        //#Simple-Split
-        //Sample Source
-        Source<String, NotUsed> source = Source.from(Arrays.asList(new String[]{"1-2-3", "2-3", "3-4"}));
+  @Test
+  public void simpleSplit() throws ExecutionException, InterruptedException {
+    // #Simple-Split
+    // Sample Source
+    Source<String, NotUsed> source =
+        Source.from(Arrays.asList(new String[] {"1-2-3", "2-3", "3-4"}));
 
+    CompletionStage<List<Integer>> ret =
+        source
+            .map(s -> Arrays.asList(s.split("-")))
+            .mapConcat(f -> f)
+            // Sub-streams logic
+            .map(s -> Integer.valueOf(s))
+            .runWith(Sink.seq(), materializer);
 
-        CompletionStage<List<Integer>> ret =
-                source.map(s -> Arrays.asList(s.split("-")))
-                        .mapConcat(f -> f)
-                        //Sub-streams logic
-                        .map(s -> Integer.valueOf(s))
-                        .runWith(Sink.seq(), materializer);
+    // Verify results
+    List<Integer> list = ret.toCompletableFuture().get();
+    assert list.equals(Arrays.asList(1, 2, 3, 2, 3, 3, 4));
+    // #Simple-Split
+  }
 
-        //Verify results
-        List<Integer> list = ret.toCompletableFuture().get();
-        assert list.equals(Arrays.asList(1, 2, 3, 2, 3, 3, 4));
-        //#Simple-Split
-    }
+  @Test
+  public void splitAggregate() throws ExecutionException, InterruptedException {
+    // #Aggregate-Split
+    // Sample Source
+    Source<String, NotUsed> source =
+        Source.from(Arrays.asList(new String[] {"1-2-3", "2-3", "3-4"}));
 
+    CompletionStage<List<Integer>> ret =
+        source
+            .map(s -> Arrays.asList(s.split("-")))
+            // split all messages into sub-streams
+            .splitWhen(a -> true)
+            // now split each collection
+            .mapConcat(f -> f)
+            // Sub-streams logic
+            .map(s -> Integer.valueOf(s))
+            // aggregate each sub-stream
+            .reduce((a, b) -> a + b)
+            // and merge back the result into the original stream
+            .mergeSubstreams()
+            .runWith(Sink.seq(), materializer);
 
-    @Test
-    public void splitAggregate() throws ExecutionException, InterruptedException {
-        //#Aggregate-Split
-        //Sample Source
-        Source<String, NotUsed> source = Source.from(Arrays.asList(new String[]{"1-2-3", "2-3", "3-4"}));
+    // Verify results
+    List<Integer> list = ret.toCompletableFuture().get();
+    assert list.equals(Arrays.asList(6, 5, 7));
+    // #Aggregate-Split
+  }
 
+  @BeforeClass
+  public static void setup() throws Exception {
+    system = ActorSystem.create();
+    materializer = ActorMaterializer.create(system);
+  }
 
-        CompletionStage<List<Integer>> ret =
-                source.map(s -> Arrays.asList(s.split("-")))
-                        //split all messages into sub-streams
-                        .splitWhen(a -> true)
-                        //now split each collection
-                        .mapConcat(f -> f)
-                        //Sub-streams logic
-                        .map(s -> Integer.valueOf(s))
-                        //aggregate each sub-stream
-                        .reduce((a, b) -> a + b)
-                        //and merge back the result into the original stream
-                        .mergeSubstreams()
-                        .runWith(Sink.seq(), materializer);
-
-        //Verify results
-        List<Integer> list = ret.toCompletableFuture().get();
-        assert list.equals(Arrays.asList(6, 5, 7));
-        //#Aggregate-Split
-    }
-
-    @BeforeClass
-    public static void setup() throws Exception {
-        system = ActorSystem.create();
-        materializer = ActorMaterializer.create(system);
-    }
-
-    @AfterClass
-    public static void teardown() throws Exception {
-        TestKit.shutdownActorSystem(system);
-    }
-
+  @AfterClass
+  public static void teardown() throws Exception {
+    TestKit.shutdownActorSystem(system);
+  }
 }
