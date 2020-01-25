1c82a20cb6777eaac450dca58de4645a53182796
gkatzioura <gkatzioura@gmail.com>
2020-01-25 23:31:07 +0000

    Alpakka BigQuery: Fixed scenario with oauth

--- google-cloud-bigquery/src/test/scala/akka/stream/alpakka/googlecloud/bigquery/impl/sendrequest/SendRequestWithOauthHandlingSpec.scala
+++ google-cloud-bigquery/src/test/scala/akka/stream/alpakka/googlecloud/bigquery/impl/sendrequest/SendRequestWithOauthHandlingSpec.scala
@@ -5,11 +5,14 @@
 package akka.stream.alpakka.googlecloud.bigquery.impl.sendrequest
 
 import akka.actor.ActorSystem
-import akka.http.scaladsl.HttpExt
+import akka.event.LoggingAdapter
+import akka.http.scaladsl.{HttpExt, HttpsConnectionContext}
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.model.headers.{Authorization, OAuth2BearerToken}
+import akka.http.scaladsl.settings.ConnectionPoolSettings
 import akka.stream.ActorMaterializer
 import akka.stream.alpakka.googlecloud.bigquery.BigQueryConfig
+import akka.stream.alpakka.googlecloud.bigquery.e2e.BigQueryTableHelper
 import akka.stream.alpakka.googlecloud.bigquery.impl.GoogleSession
 import akka.stream.scaladsl.{Sink, Source}
 import akka.testkit.TestKit
@@ -17,6 +20,7 @@ import akka.util.Timeout
 import org.mockito.Mockito._
 import org.scalatest.mockito.MockitoSugar
 import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpecLike}
+import org.mockito.ArgumentMatchers._
 
 import scala.concurrent.duration._
 import scala.concurrent.{Await, Future}
@@ -27,43 +31,43 @@ class SendRequestWithOauthHandlingSpec
     with WordSpecLike
     with Matchers
     with BeforeAndAfterAll
+    with BigQueryTableHelper
     with MockitoSugar {
+
+  override implicit val actorSystem: ActorSystem = ActorSystem("BigQueryEndToEndSpec")
+  override implicit val materializer: ActorMaterializer = ActorMaterializer()
 
   override def afterAll {
     TestKit.shutdownActorSystem(system)
   }
 
-  implicit val materializer = ActorMaterializer()
   implicit val timeout = Timeout(1.second)
 
   "SendRequestWithOauthHandling" must {
 
     "handle unexpected http error" in {
 
-      val bigQueryProjectConfig = mock[BigQueryConfig]
-      val session = mock[GoogleSession]
-      when(bigQueryProjectConfig.session) thenReturn session
-      when(session.getToken()) thenReturn Future.successful("TOKEN")
-
-      implicit val system = mock[ActorSystem]
       val http = mock[HttpExt]
       when(
         http.singleRequest(
-          HttpRequest()
-            .addHeader(Authorization(OAuth2BearerToken("TOKEN")))
+          any[HttpRequest],
+          any[HttpsConnectionContext],
+          any[ConnectionPoolSettings],
+          any[LoggingAdapter]
         )
       ) thenReturn Future.successful(HttpResponse(StatusCodes.InternalServerError, Nil, HttpEntity("my custom error")))
 
       val resultF = Source
         .single(HttpRequest())
-        .via(SendRequestWithOauthHandling(bigQueryProjectConfig, http))
+        .via(SendRequestWithOauthHandling(projectConfig, http))
         .runWith(Sink.last)
 
-      val result = Try(Await.result(resultF, 1.second))
+      val result = Try(Await.result(resultF, 2.second))
       result.toString shouldEqual Failure(
         new IllegalStateException(s"Unexpected error in response: 500 Internal Server Error, my custom error")
       ).toString
     }
 
   }
+
 }
