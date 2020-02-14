8c1d02d50d0c59645140b13630e10d3c1f463986
Francis De Brabandere <francisdb@gmail.com>
2020-02-14 17:38:12 +0100

    move comments to docs

--- google-cloud-storage/src/main/scala/akka/stream/alpakka/googlecloud/storage/impl/GoogleRetry.scala
+++ google-cloud-storage/src/main/scala/akka/stream/alpakka/googlecloud/storage/impl/GoogleRetry.scala
@@ -18,11 +18,12 @@ import akka.stream.scaladsl.{Flow, RestartSource, Sink, Source}
 import scala.concurrent.Future
 import scala.concurrent.duration._
 
+/**
+ * Applies exponential backoff as specified in the GCS SLA: https://cloud.google.com/storage/sla
+ * 1 initial attempt, plus 2^^5 exponential requests to get to 32 seconds
+ */
 @InternalApi
 private[googlecloud] object GoogleRetry {
-
-  // Exponential backoff as specified in the GCS SLA: https://cloud.google.com/storage/sla
-  // 1 initial attempt, plus 2^5 exponential requests to get to 32 seconds
 
   def singleRequest(
       http: HttpExt,
