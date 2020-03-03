72d44a45f6e671979946e293df2cf83d947aaec9
Francis De Brabandere <francisdb@gmail.com>
2020-03-03 08:28:23 +0100

    adds java getter for new optional field

--- google-cloud-storage/src/main/scala/akka/stream/alpakka/googlecloud/storage/Owner.scala
+++ google-cloud-storage/src/main/scala/akka/stream/alpakka/googlecloud/storage/Owner.scala
@@ -11,6 +11,9 @@ final class Owner private (entity: String, entityId: Option[String]) {
   def withEntity(entity: String): Owner = copy(entity = entity)
   def withEntityId(entityId: String): Owner = copy(entityId = Option(entityId))
 
+  /** Java API */
+  def getEntityId: Optional[String] = entityId.asJava
+
   private def copy(entity: String = entity, entityId: Option[String] = entityId): Owner =
     new Owner(entity, entityId)
 
