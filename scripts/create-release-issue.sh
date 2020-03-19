6882c988139cb0983c6bec504110690b58bd952e
Enno <458526+ennru@users.noreply.github.com>
2020-03-19 11:50:20 +0100

    Review release train template (#2215)

--- scripts/create-release-issue.sh
+++ scripts/create-release-issue.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 VERSION=$1
-if [ -z $VERSION ]
+BINARY_VERSION=$2
+if [ -z $BINARY_VERSION ]
 then
-  echo specify the version name to be released, eg. 1.0.0
+  echo specify the version name to be released and its minor version, eg. 2.0.0 2.0
 else
-  sed -e 's/\$VERSION\$/'$VERSION'/g' docs/release-train-issue-template.md > /tmp/release-$VERSION.md
+  sed -e 's/\$VERSION\$/'$VERSION'/g' docs/release-train-issue-template.md > /tmp/release-$VERSION.tmp
+  sed -e 's/\$BINARY_VERSION\$/'$BINARY_VERSION'/g' /tmp/release-$VERSION.tmp > /tmp/release-$VERSION.md
   echo Created $(hub issue create -F /tmp/release-$VERSION.md -M $VERSION --browse)
 fi
