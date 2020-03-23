99b20482205666c09916bcff91b0137f0b06dc06
Anthony Tjuatja <anthonytjuatja@gmail.com>
2020-03-23 08:21:47 -0400

    Kinesis: more descriptive Source error log (#2224)

--- kinesis/src/main/scala/akka/stream/alpakka/kinesis/KinesisErrors.scala
+++ kinesis/src/main/scala/akka/stream/alpakka/kinesis/KinesisErrors.scala
@@ -11,14 +11,15 @@ object KinesisErrors {
   sealed trait KinesisSourceError extends NoStackTrace
   case object NoShardsError extends KinesisSourceError
   class GetShardIteratorError(val shardId: String, e: Throwable)
-      extends RuntimeException(s"Failed to get a shard iterator for shard [$shardId]", e)
+      extends RuntimeException(s"Failed to get a shard iterator for shard [$shardId]. Reason : ${e.getMessage}", e)
       with KinesisSourceError
   class GetRecordsError(val shardId: String, e: Throwable)
-      extends RuntimeException(s"Failed to fetch records from Kinesis for shard [$shardId]", e)
+      extends RuntimeException(s"Failed to fetch records from Kinesis for shard [$shardId]. Reason : ${e.getMessage}",
+                               e)
       with KinesisSourceError
 
   sealed trait KinesisFlowErrors extends NoStackTrace
   case class FailurePublishingRecords(e: Throwable)
-      extends RuntimeException("Failure publishing records to Kinesis", e)
+      extends RuntimeException(s"Failure publishing records to Kinesis. Reason : ${e.getMessage}", e)
       with KinesisFlowErrors
 }
