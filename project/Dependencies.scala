2d11364ff0e2292aa694ee7583619a48a9d01a9f
Enno <458526+ennru@users.noreply.github.com>
2020-03-27 17:10:07 +0100

    JSON Streaming: Jsurfer 1.6.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -301,8 +301,8 @@ object Dependencies {
 
   val JsonStreaming = Seq(
     libraryDependencies ++= Seq(
-        "com.github.jsurfer" % "jsurfer" % "1.4.3", // MIT,
-        "com.github.jsurfer" % "jsurfer-jackson" % "1.4.3" // MIT
+        "com.github.jsurfer" % "jsurfer" % "1.6.0", // MIT,
+        "com.github.jsurfer" % "jsurfer-jackson" % "1.6.0" // MIT
       ) ++ JacksonDatabindDependencies
   )
 
