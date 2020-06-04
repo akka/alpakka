5178ad31cf29513b03d286ea04d1194deb4740bc
Vasilis Nicolaou <vaslabs@yahoo.gr>
2020-06-04 10:45:36 +0100

    Kinesis: Construction safe API (#2331)

---
+++ kinesis/src/main/scala/akka/stream/alpakka/kinesis/ShardIterator.scala
@@ -0,0 +1,93 @@
+/*
+ * Copyright (C) 2016-2020 Lightbend Inc. <https://www.lightbend.com>
+ */
+
+package akka.stream.alpakka.kinesis
+
+import java.time.Instant
+
+import software.amazon.awssdk.services.kinesis.model.ShardIteratorType
+
+sealed trait ShardIterator {
+  def timestamp: Option[Instant]
+  def startingSequenceNumber: Option[String]
+  def shardIteratorType: ShardIteratorType
+}
+object ShardIterator {
+
+  case object Latest extends ShardIterator {
+    override final val timestamp: Option[Instant] = None
+
+    override final val startingSequenceNumber: Option[String] = None
+
+    override final val shardIteratorType: ShardIteratorType = ShardIteratorType.LATEST
+  }
+
+  case object TrimHorizon extends ShardIterator {
+    override final val timestamp: Option[Instant] = None
+
+    override final val startingSequenceNumber: Option[String] = None
+
+    override final val shardIteratorType: ShardIteratorType = ShardIteratorType.TRIM_HORIZON
+  }
+
+  case class AtTimestamp private (value: Instant) extends ShardIterator {
+    override final val timestamp: Option[Instant] = Some(value)
+
+    override final val startingSequenceNumber: Option[String] = None
+
+    override final val shardIteratorType: ShardIteratorType = ShardIteratorType.AT_TIMESTAMP
+  }
+
+  case class AtSequenceNumber(sequenceNumber: String) extends ShardIterator {
+    override final val timestamp: Option[Instant] = None
+
+    override final val startingSequenceNumber: Option[String] = Some(sequenceNumber)
+
+    override final val shardIteratorType: ShardIteratorType = ShardIteratorType.AT_SEQUENCE_NUMBER
+  }
+
+  case class AfterSequenceNumber(sequenceNumber: String) extends ShardIterator {
+    override final val timestamp: Option[Instant] = None
+
+    override final val startingSequenceNumber: Option[String] = Some(sequenceNumber)
+
+    override final val shardIteratorType: ShardIteratorType = ShardIteratorType.AFTER_SEQUENCE_NUMBER
+  }
+}
+
+/**
+ * Java API
+ */
+object ShardIterators {
+
+  /**
+   * Java API
+   */
+  def latest(): ShardIterator =
+    ShardIterator.Latest
+
+  /**
+   * Java API
+   */
+  def trimHorizon(): ShardIterator =
+    ShardIterator.TrimHorizon
+
+  /**
+   * Java API
+   */
+  def atTimestamp(timestamp: Instant): ShardIterator =
+    ShardIterator.AtTimestamp(timestamp)
+
+  /**
+   * Java API
+   */
+  def atSequenceNumber(value: String): ShardIterator =
+    ShardIterator.AtSequenceNumber(value)
+
+  /**
+   * Java API
+   */
+  def afterSequenceNumber(value: String): ShardIterator =
+    ShardIterator.AfterSequenceNumber(value)
+}
