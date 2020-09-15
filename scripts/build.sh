e95fc76cd67213af909be726286682fb89bff391
Enno <458526+ennru@users.noreply.github.com>
2020-09-15 15:43:06 +0200

    CI: cache Jabba, install in .travis

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,9 +7,6 @@ CMD=$2
 
 [ "$TRAVIS_EVENT_TYPE" == "cron" ] && JDK="adopt@~1.11-0" || JDK="adopt@~1.8-0"
 
-# using jabba for custom jdk management
-if [ ! -f ~/.jabba/jabba.sh ]; then curl -L -v --retry 5 -o jabba-install.sh https://raw.githubusercontent.com/shyiko/jabba/0.11.2/install.sh && bash jabba-install.sh; fi
-. ~/.jabba/jabba.sh
 jabba install "$JDK"
 jabba use "$JDK"
 java -version
