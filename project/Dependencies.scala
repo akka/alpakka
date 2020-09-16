25e8207bb8f1d0625bbd702dc8cb759632269065
Olivier NOUGUIER <olivier.nouguier@teads.tv>
2020-09-16 13:49:08 +0200

    Geode: Apache Geode 1.13.0 (was 1.12.0) (#2402)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -194,8 +194,8 @@ object Dependencies {
       )
   )
 
-  val GeodeVersion = "1.12.0"
-  val GeodeVersionForDocs = "112"
+  val GeodeVersion = "1.13.0"
+  val GeodeVersionForDocs = "113"
 
   val Geode = Seq(
     libraryDependencies ++=
