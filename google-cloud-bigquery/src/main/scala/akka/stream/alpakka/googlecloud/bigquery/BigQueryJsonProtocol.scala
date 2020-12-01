e8b53ddfffb5fcc9ea3b5529070c606170535106
Arman Bilge <armanbilge@gmail.com>
2020-12-01 05:49:09 -0800

    Make BigQueryJsonProtocol case classes final

--- google-cloud-bigquery/src/main/scala/akka/stream/alpakka/googlecloud/bigquery/BigQueryJsonProtocol.scala
+++ google-cloud-bigquery/src/main/scala/akka/stream/alpakka/googlecloud/bigquery/BigQueryJsonProtocol.scala
@@ -8,17 +8,17 @@ import spray.json.{DefaultJsonProtocol, JsonFormat, RootJsonFormat}
 
 object BigQueryJsonProtocol extends DefaultJsonProtocol {
 
-  case class Response(jobReference: Option[JobReference],
-                      pageToken: Option[String],
-                      nextPageToken: Option[String],
-                      jobComplete: Option[Boolean])
+  final case class Response(jobReference: Option[JobReference],
+                            pageToken: Option[String],
+                            nextPageToken: Option[String],
+                            jobComplete: Option[Boolean])
 
-  case class JobReference(jobId: Option[String])
+  final case class JobReference(jobId: Option[String])
 
   implicit val jobReferenceFormat: JsonFormat[JobReference] = jsonFormat1(JobReference)
   implicit val responseFormat: RootJsonFormat[Response] = jsonFormat4(Response)
 
-  case class ResponseRows[T](rows: Option[Seq[T]])
+  final case class ResponseRows[T](rows: Option[Seq[T]])
 
   implicit def responseRowsFormat[T: JsonFormat]: RootJsonFormat[ResponseRows[T]] = jsonFormat1(ResponseRows[T])
 
