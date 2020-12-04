4ca618dc566225406acc733d30e96e22b7c085ce
Arman Bilge <armanbilge@gmail.com>
2020-12-03 17:26:52 -0800

    Add create methods for Java API

--- google-cloud-bigquery/src/main/scala/akka/stream/alpakka/googlecloud/bigquery/BigQueryJsonProtocol.scala
+++ google-cloud-bigquery/src/main/scala/akka/stream/alpakka/googlecloud/bigquery/BigQueryJsonProtocol.scala
@@ -4,6 +4,10 @@
 
 package akka.stream.alpakka.googlecloud.bigquery
 
+import java.util.Optional
+
+import scala.compat.java8.OptionConverters._
+import scala.collection.JavaConverters._
 import spray.json.{DefaultJsonProtocol, JsonFormat, RootJsonFormat}
 
 object BigQueryJsonProtocol extends DefaultJsonProtocol {
@@ -13,12 +17,34 @@ object BigQueryJsonProtocol extends DefaultJsonProtocol {
                             nextPageToken: Option[String],
                             jobComplete: Option[Boolean])
 
+  /**
+   * Java API
+   */
+  def createResponse(jobReference: Optional[JobReference],
+                     pageToken: Optional[String],
+                     nextPageToken: Optional[String],
+                     jobComplete: Optional[java.lang.Boolean]): Response = {
+    Response(jobReference.asScala, pageToken.asScala, nextPageToken.asScala, jobComplete.asScala.map(_.booleanValue()))
+  }
+
   final case class JobReference(jobId: Option[String])
+
+  /**
+   * Java API
+   */
+  def createJobReference(jobId: Optional[String]): JobReference = JobReference(jobId.asScala)
 
   implicit val jobReferenceFormat: JsonFormat[JobReference] = jsonFormat1(JobReference)
   implicit val responseFormat: RootJsonFormat[Response] = jsonFormat4(Response)
 
   final case class ResponseRows[T](rows: Option[Seq[T]])
+
+  /**
+   * Java API
+   */
+  def createResponseRows[T](rows: Optional[java.util.List[T]]): ResponseRows[T] = {
+    ResponseRows(rows.asScala.map(_.asScala))
+  }
 
   implicit def responseRowsFormat[T: JsonFormat]: RootJsonFormat[ResponseRows[T]] = jsonFormat1(ResponseRows[T])
 
