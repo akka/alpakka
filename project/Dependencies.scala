bc3b84bcb7c85cf5dd78bf29b305844d1b9a917f
Sean C. Sullivan <github@seansullivan.com>
2020-12-22 16:18:18 -0800

    jms-testkit 0.4.1
    This version of jms-testkit has been cross-compiled
    for Scala 2.11, Scala 2,12, Scala 2.13, and Scala 3.0.0-M3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -302,7 +302,7 @@ object Dependencies {
         "com.ibm.mq" % "com.ibm.mq.allclient" % "9.2.0.0" % Test, // IBM International Program License Agreement https://public.dhe.ibm.com/ibmdl/export/pub/software/websphere/messaging/mqdev/maven/licenses/L-APIG-AZYF2E/LI_en.html
         "org.apache.activemq" % "activemq-broker" % "5.16.0" % Test, // ApacheV2
         "org.apache.activemq" % "activemq-client" % "5.16.0" % Test, // ApacheV2
-        "io.github.sullis" %% "jms-testkit" % "0.2.8" % Test // ApacheV2
+        "io.github.sullis" %% "jms-testkit" % "0.4.1" % Test // ApacheV2
       ) ++ Mockito,
     // Having JBoss as a first resolver is a workaround for https://github.com/coursier/coursier/issues/200
     externalResolvers := ("jboss" at "https://repository.jboss.org/nexus/content/groups/public") +: externalResolvers.value
