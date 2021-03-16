ce749b41b3fd993127b5fac6bba7d19a11899209
Sean Glover <sean@seanglover.com>
2021-03-16 11:05:53 -0400

    Couchbase Akka 2.6 (#2617)

--- couchbase/src/test/scala/docs/scaladsl/DiscoverySpec.scala
+++ couchbase/src/test/scala/docs/scaladsl/DiscoverySpec.scala
@@ -6,23 +6,21 @@ package docs.scaladsl
 
 import akka.actor.ActorSystem
 import akka.stream.alpakka.testkit.scaladsl.LogCapturing
-import akka.stream.{ActorMaterializer, Materializer}
 import com.couchbase.client.java.document.JsonDocument
 import com.typesafe.config.{Config, ConfigFactory}
-import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.BeforeAndAfterAll
+import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
 
 import scala.concurrent.duration._
 import scala.concurrent.{ExecutionContext, Future}
-import org.scalatest.matchers.should.Matchers
-import org.scalatest.wordspec.AnyWordSpec
 
 class DiscoverySpec extends AnyWordSpec with Matchers with BeforeAndAfterAll with ScalaFutures with LogCapturing {
 
   val config: Config = ConfigFactory.parseResources("discovery.conf")
 
   implicit val actorSystem: ActorSystem = ActorSystem("DiscoverySpec", config)
-  implicit val mat: Materializer = ActorMaterializer()
 
   override implicit def patienceConfig: PatienceConfig = PatienceConfig(10.seconds, 250.millis)
 
@@ -48,8 +46,8 @@ class DiscoverySpec extends AnyWordSpec with Matchers with BeforeAndAfterAll wit
 
     "be created from settings" in {
       // #create
-      import akka.stream.alpakka.couchbase.scaladsl.{CouchbaseSession, DiscoverySupport}
       import akka.stream.alpakka.couchbase.CouchbaseSessionSettings
+      import akka.stream.alpakka.couchbase.scaladsl.{CouchbaseSession, DiscoverySupport}
 
       implicit val ec: ExecutionContext = actorSystem.dispatcher
       val sessionSettings = CouchbaseSessionSettings(actorSystem)
