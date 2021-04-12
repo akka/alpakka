0df5d7eaecd8d0812fec1b40082fb0aa5caffae3
Arman Bilge <armanbilge@gmail.com>
2021-04-12 07:53:44 -0700

    Google: Drop old jwt-core dependencies (#2641)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -224,7 +224,6 @@ object Dependencies {
         "io.spray" %% "spray-json" % "1.3.6",
         "com.fasterxml.jackson.core" % "jackson-annotations" % JacksonDatabindVersion,
         "com.fasterxml.jackson.datatype" % "jackson-datatype-jsr310" % JacksonDatabindVersion % Test,
-        "com.pauldijou" %% "jwt-core" % "3.1.0", //ApacheV2
         "io.specto" % "hoverfly-java" % hoverflyVersion % Test //ApacheV2
       ) ++ Mockito ++ Silencer
   )
@@ -233,7 +232,6 @@ object Dependencies {
     libraryDependencies ++= Seq(
         "com.typesafe.akka" %% "akka-http" % AkkaHttpVersion,
         "com.typesafe.akka" %% "akka-http-spray-json" % AkkaHttpVersion,
-        "com.pauldijou" %% "jwt-core" % JwtCoreVersion, // ApacheV2
         "com.github.tomakehurst" % "wiremock" % "2.25.1" % Test // ApacheV2
       ) ++ Mockito ++ Silencer
   )
@@ -253,8 +251,7 @@ object Dependencies {
   val GoogleFcm = Seq(
     libraryDependencies ++= Seq(
         "com.typesafe.akka" %% "akka-http" % AkkaHttpVersion,
-        "com.typesafe.akka" %% "akka-http-spray-json" % AkkaHttpVersion,
-        "com.pauldijou" %% "jwt-core" % JwtCoreVersion // ApacheV2
+        "com.typesafe.akka" %% "akka-http-spray-json" % AkkaHttpVersion
       ) ++ Mockito ++ Silencer
   )
 
@@ -262,7 +259,6 @@ object Dependencies {
     libraryDependencies ++= Seq(
         "com.typesafe.akka" %% "akka-http" % AkkaHttpVersion,
         "com.typesafe.akka" %% "akka-http-spray-json" % AkkaHttpVersion,
-        "com.pauldijou" %% "jwt-core" % JwtCoreVersion, //ApacheV2
         "io.specto" % "hoverfly-java" % hoverflyVersion % Test // ApacheV2
       ) ++ Mockito ++ Silencer
   )
