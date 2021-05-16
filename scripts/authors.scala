14207e98b58ea95cc57273bcdf7badf96746e472
Sean Glover <sean@seanglover.com>
2021-05-16 12:32:00 -0400

    lazylines

--- scripts/authors.scala
+++ scripts/authors.scala
@@ -28,7 +28,7 @@ val FilesExp = """(\d+)\sfile[s]? changed""".r
 val InsertionsExp = """(\d+)\sinsertion[s]?\(\+\)""".r
 val DeletionsExp = """(\d+)\sdeletion[s]?\(-\)""".r
 
-val entries = gitCmd.lineStream_!.foldLeft("")(_ + "\n" + _).split('\u0000')
+val entries = gitCmd.lazyLines_!.foldLeft("")(_ + "\n" + _).split('\u0000')
 
 val map = entries.foldLeft(Map.empty[String, Stats]) { (map, entry) =>
   val lines = entry.trim.split('\n')
