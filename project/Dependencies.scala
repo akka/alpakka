c417bf309db988fad4d4618e4d8d116226a92ae0
gkatzioura <gkatzioura@gmail.com>
2021-06-07 07:58:28 +0100

    Changing Dependencies on arrow

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -231,12 +231,10 @@ object Dependencies {
     // see Akka gRPC version in plugins.sbt
     libraryDependencies ++= Seq(
         // https://github.com/googleapis/java-bigquerystorage/tree/master/proto-google-cloud-bigquerystorage-v1
-        "com.google.api.grpc" % "proto-google-cloud-bigquerystorage-v1" % "1.22.0" % "protobuf-src", //exclude ("com.google.api.grpc", "proto-google-common-protos"), // ApacheV2
+        "com.google.api.grpc" % "proto-google-cloud-bigquerystorage-v1" % "1.22.0" % "protobuf-src",  // ApacheV2
         "org.apache.avro" % "avro" % "1.9.2" % "provided",
-        "org.apache.arrow" % "arrow-vector" % "4.0.0",
-        "org.apache.arrow" % "arrow-memory-netty" % "4.0.0",
-        //"com.thesamet.scalapb.common-protos" %% "proto-google-common-protos-scalapb_0.10" % "1.17.0-0",
-        //"com.thesamet.scalapb.common-protos" %% "proto-google-common-protos-scalapb_0.10" % "1.17.0-0" % "protobuf",
+        "org.apache.arrow" % "arrow-vector" % "4.0.0" % "provided",
+        "org.apache.arrow" % "arrow-memory-netty" % "4.0.0" % Test,
         "io.grpc" % "grpc-auth" % akka.grpc.gen.BuildInfo.grpcVersion, // ApacheV2
         "com.typesafe.akka" %% "akka-http-spray-json" % "10.1.12" % Test,
         "com.typesafe.akka" %% "akka-discovery" % AkkaVersion
