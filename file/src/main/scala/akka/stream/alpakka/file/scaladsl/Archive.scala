f9214c8a53ea99eacb34915550e4c89be8d6f9f8
Gergő Törcsvári <torcsvari.gergo@gmail.com>
2021-08-25 16:00:50 +0200

    file: unzip introduced (#2692)

--- file/src/main/scala/akka/stream/alpakka/file/scaladsl/Archive.scala
+++ file/src/main/scala/akka/stream/alpakka/file/scaladsl/Archive.scala
@@ -5,10 +5,12 @@
 package akka.stream.alpakka.file.scaladsl
 
 import akka.NotUsed
-import akka.stream.alpakka.file.{ArchiveMetadata, TarArchiveMetadata}
-import akka.stream.alpakka.file.impl.archive.{TarArchiveManager, TarReaderStage, ZipArchiveManager}
+import akka.stream.alpakka.file.{ArchiveMetadata, TarArchiveMetadata, ZipArchiveMetadata}
+import akka.stream.alpakka.file.impl.archive.{TarArchiveManager, TarReaderStage, ZipArchiveManager, ZipSource}
 import akka.stream.scaladsl.{Flow, Source}
 import akka.util.ByteString
+
+import java.io.File
 
 /**
  * Scala API.
@@ -20,6 +22,14 @@ object Archive {
    */
   def zip(): Flow[(ArchiveMetadata, Source[ByteString, Any]), ByteString, NotUsed] =
     ZipArchiveManager.zipFlow()
+
+  /**
+   * Flow for reading ZIP files.
+   */
+  def zipReader(file: File, chunkSize: Int): Source[(ZipArchiveMetadata, Source[ByteString, Any]), NotUsed] =
+    Source.fromGraph(new ZipSource(file, chunkSize))
+  def zipReader(file: File): Source[(ZipArchiveMetadata, Source[ByteString, Any]), NotUsed] =
+    Source.fromGraph(new ZipSource(file, 8192))
 
   /**
    * Flow for packaging multiple files into one TAR file.
