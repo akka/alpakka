f22cb55d1fcf8906ec1719a9ef3d8e3c70dc91ae
Matthew de Detrich <matthew.dedetrich@aiven.io>
2021-09-06 14:02:50 +0200

    Add GCSSettings to make endpointUrl + basePath configurable (#2726)

---
+++ google-cloud-storage/src/main/scala/akka/stream/alpakka/googlecloud/storage/GCSExt.scala
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2016-2020 Lightbend Inc. <https://www.lightbend.com>
+ */
+
+package akka.stream.alpakka.googlecloud.storage
+
+import akka.actor.{ClassicActorSystemProvider, ExtendedActorSystem, Extension, ExtensionId, ExtensionIdProvider}
+
+/**
+ * Manages one [[GCSSettings]] per `ActorSystem`.
+ */
+final class GCSExt private (sys: ExtendedActorSystem) extends Extension {
+  val settings: GCSSettings = settings(GCSSettings.ConfigPath)
+
+  def settings(prefix: String): GCSSettings = GCSSettings(sys.settings.config.getConfig(prefix))
+}
+
+object GCSExt extends ExtensionId[GCSExt] with ExtensionIdProvider {
+  override def lookup = GCSExt
+  override def createExtension(system: ExtendedActorSystem) = new GCSExt(system)
+
+  /**
+   * Java API.
+   * Get the GCS extension with the classic actors API.
+   */
+  override def get(system: akka.actor.ActorSystem): GCSExt = super.apply(system)
+
+  /**
+   * Java API.
+   * Get the GCS extension with the new actors API.
+   */
+  override def get(system: ClassicActorSystemProvider): GCSExt = super.apply(system)
+}
---
+++ google-cloud-storage/src/test/scala/akka/stream/alpakka/googlecloud/storage/GCSSettingsSpec.scala
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2016-2020 Lightbend Inc. <https://www.lightbend.com>
+ */
+
+package akka.stream.alpakka.googlecloud.storage
+
+import akka.stream.alpakka.testkit.scaladsl.LogCapturing
+import com.typesafe.config.ConfigFactory
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+import scala.collection.JavaConverters._
+
+class GCSSettingsSpec extends AnyFlatSpec with Matchers with LogCapturing {
+  "GCSSettings" should "create settings from application config" in {
+    val endpointUrl = "https://storage.googleapis.com/"
+    val basePath = "/storage/v1"
+
+    val config = ConfigFactory.parseMap(
+      Map(
+        "endpoint-url" -> endpointUrl,
+        "base-path" -> basePath
+      ).asJava
+    )
+
+    val settings = GCSSettings(config)
+
+    settings.endpointUrl shouldBe endpointUrl
+    settings.basePath shouldBe basePath
+  }
+
+}
