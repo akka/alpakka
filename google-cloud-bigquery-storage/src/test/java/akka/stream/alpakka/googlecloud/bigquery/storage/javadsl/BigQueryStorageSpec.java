52937f08a0a1367e900e211dd4c2b7d7880fd58a
gkatzioura <gkatzioura@gmail.com>
2021-11-15 10:22:54 +0000

    Fixed compile error

--- google-cloud-bigquery-storage/src/test/java/akka/stream/alpakka/googlecloud/bigquery/storage/javadsl/BigQueryStorageSpec.java
+++ google-cloud-bigquery-storage/src/test/java/akka/stream/alpakka/googlecloud/bigquery/storage/javadsl/BigQueryStorageSpec.java
@@ -8,18 +8,14 @@ import akka.stream.Attributes;
 import akka.stream.alpakka.googlecloud.bigquery.storage.BigQueryRecord;
 import akka.stream.alpakka.googlecloud.bigquery.storage.BigQueryStorageSettings;
 import akka.stream.alpakka.googlecloud.bigquery.storage.BigQueryStorageSpecBase;
-import akka.stream.alpakka.googlecloud.bigquery.storage.impl.AvroDecoder;
 import akka.stream.alpakka.googlecloud.bigquery.storage.scaladsl.BigQueryStorageAttributes;
 import akka.stream.alpakka.googlecloud.bigquery.storage.scaladsl.GrpcBigQueryStorageReader;
 import akka.stream.alpakka.testkit.javadsl.LogCapturingJunit4;
 import akka.stream.javadsl.Sink;
 
-import com.google.cloud.bigquery.storage.v1.AvroSchema;
 import com.google.cloud.bigquery.storage.v1.DataFormat;
 import com.google.cloud.bigquery.storage.v1.ReadSession;
-import com.google.cloud.bigquery.storage.v1.avro.AvroRows;
 
-import org.apache.avro.generic.GenericRecord;
 import org.junit.*;
 
 import java.util.List;
@@ -56,33 +52,6 @@ public class BigQueryStorageSpec extends BigQueryStorageSpecBase {
     assertTrue(
         "number of generic records should be more than 0",
         bigQueryRecords.toCompletableFuture().get(5, TimeUnit.SECONDS).isEmpty());
-  }
-
-  @Test
-  public void streamResultsUsingAvroDeserializer()
-      throws ExecutionException, InterruptedException, TimeoutException {
-    AvroSchema avroSchema = AvroSchema.newBuilder().setSchema(Col1Schema().toString()).build();
-    AvroRows avroRows = recordsAsRows(FullAvroRecord());
-
-    AvroDecoder avroDecoder = AvroDecoder.apply(avroSchema.getSchema());
-    List<BigQueryRecord> records =
-        JavaConverters.asJavaCollection(avroDecoder.decodeRows(avroRows.serializedBinaryRows()))
-            .stream()
-            .map(BigQueryRecord::fromAvro)
-            .collect(Collectors.toList());
-
-    AvroByteStringDecoder um = new AvroByteStringDecoder(FullAvroSchema());
-
-    List<List<BigQueryRecord>> bigQueryRecords =
-        BigQueryStorage.createMergedStreams(Project(), Dataset(), Table(), DataFormat.AVRO, um)
-            .withAttributes(mockBQReader())
-            .runWith(Sink.seq(), system())
-            .toCompletableFuture()
-            .get();
-
-    assertEquals(bigQueryRecords.size(), DefaultNumStreams());
-    assertEquals(bigQueryRecords.get(0), ResponsesPerStream());
-    assertEquals(bigQueryRecords.get(0), records);
   }
 
   public Attributes mockBQReader() {
