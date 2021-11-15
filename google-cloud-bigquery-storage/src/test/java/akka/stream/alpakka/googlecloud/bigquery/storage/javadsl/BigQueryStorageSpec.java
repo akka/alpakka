7a2799d2769327e06c11f69d1c78c4161f5a120f
gkatzioura <gkatzioura@gmail.com>
2021-11-15 08:26:31 +0000

    Changing format

--- google-cloud-bigquery-storage/src/test/java/akka/stream/alpakka/googlecloud/bigquery/storage/javadsl/BigQueryStorageSpec.java
+++ google-cloud-bigquery-storage/src/test/java/akka/stream/alpakka/googlecloud/bigquery/storage/javadsl/BigQueryStorageSpec.java
@@ -8,14 +8,18 @@ import akka.stream.Attributes;
 import akka.stream.alpakka.googlecloud.bigquery.storage.BigQueryRecord;
 import akka.stream.alpakka.googlecloud.bigquery.storage.BigQueryStorageSettings;
 import akka.stream.alpakka.googlecloud.bigquery.storage.BigQueryStorageSpecBase;
+import akka.stream.alpakka.googlecloud.bigquery.storage.impl.AvroDecoder;
 import akka.stream.alpakka.googlecloud.bigquery.storage.scaladsl.BigQueryStorageAttributes;
 import akka.stream.alpakka.googlecloud.bigquery.storage.scaladsl.GrpcBigQueryStorageReader;
 import akka.stream.alpakka.testkit.javadsl.LogCapturingJunit4;
 import akka.stream.javadsl.Sink;
 
+import com.google.cloud.bigquery.storage.v1.AvroSchema;
 import com.google.cloud.bigquery.storage.v1.DataFormat;
 import com.google.cloud.bigquery.storage.v1.ReadSession;
+import com.google.cloud.bigquery.storage.v1.avro.AvroRows;
 
+import org.apache.avro.generic.GenericRecord;
 import org.junit.*;
 
 import java.util.List;
@@ -23,9 +27,11 @@ import java.util.concurrent.CompletionStage;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
+import java.util.stream.Collectors;
 
 import static org.junit.Assert.*;
 import scala.Tuple2;
+import scala.collection.JavaConverters;
 import scala.collection.immutable.Seq;
 
 public class BigQueryStorageSpec extends BigQueryStorageSpecBase {
@@ -52,6 +58,26 @@ public class BigQueryStorageSpec extends BigQueryStorageSpecBase {
         bigQueryRecords.toCompletableFuture().get(5, TimeUnit.SECONDS).isEmpty());
   }
 
+  @Test
+  public void streamResultsUsingAvroDeserializer()
+      throws ExecutionException, InterruptedException, TimeoutException {
+    AvroSchema avroSchema = AvroSchema.newBuilder().setSchema(Col1Schema().toString()).build();
+    AvroRows avroRows = recordsAsRows(FullAvroRecord());
+
+    AvroDecoder avroDecoder = AvroDecoder.apply(avroSchema.getSchema());
+    List<BigQueryRecord> records =
+        JavaConverters.asJavaCollection(avroDecoder.decodeRows(avroRows.serializedBinaryRows()))
+            .stream()
+            .map(BigQueryRecord::fromAvro)
+            .collect(Collectors.toList());
+
+    AvroByteStringDecoder um = new AvroByteStringDecoder(FullAvroSchema());
+
+    CompletionStage<List<List<BigQueryRecord>>> bigQueryRecords =
+        BigQueryStorage.createMergedStreams(Project(), Dataset(), Table(), DataFormat.AVRO, um)
+            .withAttributes(mockBQReader())
+            .runWith(Sink.seq(), system());
+  }
 
   public Attributes mockBQReader() {
     return mockBQReader(bqHost(), bqPort());
