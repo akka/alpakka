f1dbbcb30f0f4a267eea7c01094d0bf9d6b2200d
LukBed <50501718+LukBed@users.noreply.github.com>
2022-04-29 10:02:16 +0200

    Google Cloud Pub/Sub ordering key #2863 (#2864)
    * Support ordering key for Pub/Sub
    * backward compatibility for PubSubMessage.create Java API

--- google-cloud-pub-sub/src/test/scala/docs/scaladsl/ExampleUsage.scala
+++ google-cloud-pub-sub/src/test/scala/docs/scaladsl/ExampleUsage.scala
@@ -66,6 +66,15 @@ class ExampleUsage {
     .to(Sink.seq)
   //#publish-fast
 
+  //#publish with ordering key
+  val messageWithOrderingKey =
+    PublishMessage(new String(Base64.getEncoder.encode("Hello Google!".getBytes)), None, Some("my-ordering-key"))
+  val publishedMessageWithOrderingKeyIds: Future[Seq[Seq[String]]] = Source
+    .single(PublishRequest(Seq(messageWithOrderingKey)))
+    .via(publishFlow)
+    .runWith(Sink.seq)
+  //#publish with ordering key
+
   //#subscribe
   val subscriptionSource: Source[ReceivedMessage, Cancellable] =
     GooglePubSub.subscribe(subscription, config)
