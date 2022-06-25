2107518d10ff50d5f2fb4e56e0f144e290b6f2b6
Lukasz Bednorz <lbednorz@virtuslab.com>
2022-06-25 16:52:02 +0200

    Java DSL

---
+++ typesense/src/main/scala/akka/stream/alpakka/typesense/javadsl/Typesense.scala
@@ -0,0 +1,32 @@
+package akka.stream.alpakka.typesense.javadsl
+
+import akka.stream.alpakka.typesense.{CollectionResponse, CollectionSchema, TypesenseSettings}
+import akka.stream.javadsl.{Flow, Sink}
+import akka.{Done, NotUsed}
+
+import java.util.concurrent.CompletionStage
+import scala.compat.java8.FutureConverters.FutureOps
+
+object Typesense {
+  private val ScalaTypesense = akka.stream.alpakka.typesense.scaladsl.Typesense
+
+  /**
+   * Creates a flow for creating collections.
+   */
+  def createCollectionFlow(
+      settings: TypesenseSettings
+  ): Flow[CollectionSchema, CollectionResponse, CompletionStage[NotUsed]] =
+    ScalaTypesense
+      .createCollectionFlow(settings)
+      .mapMaterializedValue(_.toJava)
+      .asJava
+
+  /**
+   * Creates a sink for creating collections.
+   */
+  def createCollection(settings: TypesenseSettings): Sink[CollectionSchema, CompletionStage[Done]] =
+    ScalaTypesense
+      .createCollection(settings)
+      .mapMaterializedValue(_.toJava)
+      .asJava
+}
