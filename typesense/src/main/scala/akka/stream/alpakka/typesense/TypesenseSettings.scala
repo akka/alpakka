9db02aefd62f6600c373dcfb667792fd70e1ca1d
Lukasz Bednorz <lbednorz@virtuslab.com>
2022-06-25 20:18:06 +0200

    improve settings class according to guideline

--- typesense/src/main/scala/akka/stream/alpakka/typesense/TypesenseSettings.scala
+++ typesense/src/main/scala/akka/stream/alpakka/typesense/TypesenseSettings.scala
@@ -4,4 +4,24 @@
 
 package akka.stream.alpakka.typesense
 
-final case class TypesenseSettings(host: String, apiKey: String)
+final class TypesenseSettings private (val host: String, val apiKey: String) {
+
+  def withHost(host: String): TypesenseSettings = new TypesenseSettings(host, apiKey)
+
+  def withApiKey(apiKey: String): TypesenseSettings = new TypesenseSettings(host, apiKey)
+
+  override def equals(other: Any): Boolean = other match {
+    case that: TypesenseSettings =>
+      host == that.host &&
+      apiKey == that.apiKey
+    case _ => false
+  }
+
+  override def hashCode: Int = java.util.Objects.hash(host, apiKey)
+}
+
+object TypesenseSettings {
+  def apply(host: String, apiKey: String): TypesenseSettings = new TypesenseSettings(host, apiKey)
+
+  def create(host: String, apiKey: String): TypesenseSettings = new TypesenseSettings(host, apiKey)
+}
