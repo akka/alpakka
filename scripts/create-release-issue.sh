107b315559f5953e0ca86dab28305f67551e8c93
Patrik Nordwall <patrik.nordwall@gmail.com>
2022-10-25 14:29:34 +0200

    Update release issue template

--- scripts/create-release-issue.sh
+++ scripts/create-release-issue.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
 VERSION=$1
-BINARY_VERSION=$2
-if [ -z $BINARY_VERSION ]
+if [ -z $VERSION ]
 then
-  echo specify the version name to be released and its minor version, eg. 2.0.0 2.0
+  echo specify the version name to be released, eg. 1.0.0
 else
   sed -e 's/\$VERSION\$/'$VERSION'/g' docs/release-train-issue-template.md > /tmp/release-$VERSION.tmp
-  sed -e 's/\$BINARY_VERSION\$/'$BINARY_VERSION'/g' /tmp/release-$VERSION.tmp > /tmp/release-$VERSION.md
-  echo Created $(gh issue create --title "Release Alpakka $VERSION" --body-file /tmp/release-$VERSION.md --milestone $VERSION --web)
+  echo Created $(gh issue create --title "Release $VERSION" --body-file /tmp/release-$VERSION.md --milestone $VERSION --web)
 fi
