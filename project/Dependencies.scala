15c24e6a10bfbc5b4aaa0aba8e5dc6867a7c3b3d
Enno <458526+ennru@users.noreply.github.com>
2023-09-18 13:18:23 +0200

    Use Scala 3.3.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
 
   val Scala213 = "2.13.11" // update even in link-validator.conf
   val Scala212 = "2.12.18"
-  val Scala3 = "3.3.0"
+  val Scala3 = "3.3.1"
   val Scala2Versions = Seq(Scala213, Scala212)
   val ScalaVersions = Dependencies.Scala2Versions :+ Dependencies.Scala3
 
