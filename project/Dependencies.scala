c7c1a019e1e1a4df498af0733093903fc1d3ad98
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2024-03-28 14:49:02 +0100

    bump: amqp-client 5.20.0 (was 5.18.0) (#3073)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -77,7 +77,7 @@ object Dependencies {
 
   val Amqp = Seq(
     libraryDependencies ++= Seq(
-        "com.rabbitmq" % "amqp-client" % "5.18.0" // APLv2
+        "com.rabbitmq" % "amqp-client" % "5.20.0" // APLv2
       ) ++ Mockito
   )
 
