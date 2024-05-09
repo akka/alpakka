a27a30cdb119988dac002211d2f2df6b097aa560
Scala Steward <scala_steward@virtuslab.com>
2024-05-09 12:35:43 +0000

    bump: mongo-scala-driver 4.11.2 (was 4.10.2)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -392,7 +392,7 @@ object Dependencies {
 
   val MongoDb = Seq(
     libraryDependencies ++= Seq(
-        "org.mongodb.scala" %% "mongo-scala-driver" % "4.10.2" // ApacheV2
+        "org.mongodb.scala" %% "mongo-scala-driver" % "4.11.2" // ApacheV2
       )
   )
 
