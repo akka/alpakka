f1de2093bb2944380a6897119c8016fe7a361042
sfali <syed.f.ali@improving.com>
2024-08-24 11:34:20 -0400

    Introducing Akka stream attributes in order to configure http request #3253

---
+++ azure-storage/src/main/scala/akka/stream/alpakka/azure/storage/StorageAttributes.scala
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) since 2016 Lightbend Inc. <https://www.lightbend.com>
+ */
+
+package akka.stream.alpakka
+package azure
+package storage
+
+import akka.stream.Attributes
+import akka.stream.Attributes.Attribute
+
+/**
+ * Akka Stream attributes that are used when materializing AzureStorage stream blueprints.
+ */
+object StorageAttributes {
+
+  /**
+   * Settings to use for the S3 stream
+   */
+  def settings(settings: StorageSettings): Attributes = Attributes(StorageSettingsValue(settings))
+
+  /**
+   * Config path which will be used to resolve required AzureStorage settings
+   */
+  def settingsPath(path: String): Attributes = Attributes(StorageSettingsPath(path))
+}
+
+final class StorageSettingsPath private (val path: String) extends Attribute
+object StorageSettingsPath {
+  val Default: StorageSettingsPath = StorageSettingsPath(StorageSettings.ConfigPath)
+
+  def apply(path: String) = new StorageSettingsPath(path)
+}
+
+final class StorageSettingsValue private (val settings: StorageSettings) extends Attribute
+object StorageSettingsValue {
+  def apply(settings: StorageSettings) = new StorageSettingsValue(settings)
+}
