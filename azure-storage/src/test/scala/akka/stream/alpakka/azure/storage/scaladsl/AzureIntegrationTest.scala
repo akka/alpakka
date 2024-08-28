be29661c3e223f1872d0f68a86e51e6acd5fcc35
sfali <syed.f.ali@improving.com>
2024-08-28 13:49:04 -0400

    FileService test  #3253

--- azure-storage/src/test/scala/akka/stream/alpakka/azure/storage/scaladsl/AzureIntegrationTest.scala
+++ azure-storage/src/test/scala/akka/stream/alpakka/azure/storage/scaladsl/AzureIntegrationTest.scala
@@ -8,6 +8,10 @@ package storage
 package scaladsl
 
 import akka.actor.ActorSystem
+import akka.http.scaladsl.model.ContentTypes
+import akka.http.scaladsl.model.headers.ByteRange
+import akka.stream.scaladsl.{Keep, Sink, Source}
+import akka.util.ByteString
 import org.scalatest.Ignore
 
 @Ignore
@@ -15,4 +19,112 @@ class AzureIntegrationTest extends StorageIntegrationSpec {
 
   override protected implicit val system: ActorSystem = ActorSystem("AzureIntegrationTest")
 
+  // Azurite doesn't support FileService yet so can't add these tests in the base class
+  "FileService" should {
+    "create file" in {
+      val maybeObjectMetadata = FileService
+        .createFile(objectPath = objectPath, contentType = ContentTypes.`text/plain(UTF-8)`, maxSize = contentLength)
+        .withAttributes(getDefaultAttributes)
+        .runWith(Sink.head)
+        .futureValue
+
+      maybeObjectMetadata.isDefined shouldBe true
+      val objectMetadata = maybeObjectMetadata.get
+      objectMetadata.contentLength shouldBe 0
+    }
+
+    "put range" in {
+      val maybeObjectMetadata = FileService
+        .updateRange(objectPath = objectPath,
+                     contentType = ContentTypes.`text/plain(UTF-8)`,
+                     range = ByteRange(0, contentLength - 1),
+                     payload = Source.single(ByteString(sampleText)))
+        .withAttributes(getDefaultAttributes)
+        .runWith(Sink.head)
+        .futureValue
+
+      maybeObjectMetadata.isDefined shouldBe true
+      val objectMetadata = maybeObjectMetadata.get
+      objectMetadata.contentLength shouldBe 0
+    }
+
+    "get file" in {
+      val (maybeEventualObjectMetadata, eventualText) = FileService
+        .getFile(objectPath = objectPath)
+        .via(framing)
+        .map(byteString => byteString.utf8String + System.lineSeparator())
+        .toMat(Sink.seq)(Keep.both)
+        .run()
+
+      val objectMetadata = maybeEventualObjectMetadata.futureValue
+      objectMetadata.contentType shouldBe Some(ContentTypes.`text/plain(UTF-8)`.value)
+      objectMetadata.contentLength shouldBe sampleText.length
+      eventualText.futureValue.mkString("") shouldBe sampleText
+    }
+
+    "get file properties" in {
+      val maybeObjectMetadata =
+        FileService
+          .getProperties(objectPath)
+          .withAttributes(getDefaultAttributes)
+          .runWith(Sink.head)
+          .futureValue
+
+      maybeObjectMetadata shouldBe defined
+      val objectMetadata = maybeObjectMetadata.get
+      objectMetadata.contentType shouldBe Some(ContentTypes.`text/plain(UTF-8)`.value)
+      objectMetadata.contentLength shouldBe sampleText.length
+    }
+
+    "get file range" in {
+      val range = ByteRange(0, 8)
+      val (maybeEventualObjectMetadata, eventualText) =
+        FileService
+          .getFile(objectPath, Some(range))
+          .withAttributes(getDefaultAttributes)
+          .via(framing)
+          .map(_.utf8String)
+          .toMat(Sink.seq)(Keep.both)
+          .run()
+
+      val objectMetadata = maybeEventualObjectMetadata.futureValue
+      objectMetadata.contentLength shouldBe (range.last - range.first + 1)
+      eventualText.futureValue.mkString("") shouldBe "The quick"
+    }
+
+    "clear range" in {
+      val range = ByteRange(16, 24)
+      val maybeObjectMetadata = FileService
+        .clearRange(objectPath = objectPath, range = range)
+        .withAttributes(getDefaultAttributes)
+        .runWith(Sink.head)
+        .futureValue
+
+      maybeObjectMetadata.get.contentLength shouldBe 0
+    }
+
+    "delete file" in {
+      val maybeObjectMetadata =
+        FileService
+          .deleteFile(objectPath)
+          .withAttributes(getDefaultAttributes)
+          .toMat(Sink.head)(Keep.right)
+          .run()
+          .futureValue
+
+      maybeObjectMetadata.get.contentLength shouldBe 0
+    }
+
+    "get file after delete" in {
+      val maybeObjectMetadata =
+        FileService
+          .getProperties(objectPath)
+          .withAttributes(getDefaultAttributes)
+          .toMat(Sink.head)(Keep.right)
+          .run()
+          .futureValue
+
+      maybeObjectMetadata shouldBe empty
+    }
+  }
 }
