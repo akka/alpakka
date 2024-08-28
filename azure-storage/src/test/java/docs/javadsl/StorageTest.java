e0afb817b605944fba241cdaecf1a76ebd46246e
sfali <syed.f.ali@improving.com>
2024-08-28 12:17:07 -0400

    Scala and Java mock tests for documentation  #3253

---
+++ azure-storage/src/test/java/docs/javadsl/StorageTest.java
@@ -0,0 +1,283 @@
+/*
+ * Copyright (C) since 2016 Lightbend Inc. <https://www.lightbend.com>
+ */
+
+package docs.javadsl;
+
+import akka.NotUsed;
+import akka.actor.ActorSystem;
+import akka.http.javadsl.Http;
+import akka.http.javadsl.model.ContentTypes;
+import akka.stream.alpakka.azure.storage.ObjectMetadata;
+import akka.stream.alpakka.azure.storage.javadsl.BlobService;
+import akka.stream.alpakka.azure.storage.javadsl.FileService;
+import akka.stream.alpakka.azure.storage.scaladsl.StorageWireMockBase;
+import akka.stream.alpakka.testkit.javadsl.LogCapturingJunit4;
+import akka.stream.javadsl.Source;
+import akka.stream.javadsl.Sink;
+import akka.testkit.javadsl.TestKit;
+import akka.util.ByteString;
+import com.github.tomakehurst.wiremock.WireMockServer;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.CompletionStage;
+import java.util.stream.Collectors;
+
+public class StorageTest extends StorageWireMockBase {
+
+    @Rule
+    public final LogCapturingJunit4 logCapturing = new LogCapturingJunit4();
+
+    private static ActorSystem system;
+    private static WireMockServer wireMockServerForShutdown;
+
+    @Before
+    public void before() {
+        wireMockServerForShutdown = _wireMockServer();
+        system = system();
+    }
+
+    @AfterClass
+    public static void afterAll() {
+        wireMockServerForShutdown.stop();
+        Http.get(system)
+                .shutdownAllConnectionPools()
+                .thenRun(() -> TestKit.shutdownActorSystem(system));
+    }
+
+
+    @Test
+    public void createContainer() throws Exception {
+        mockCreateContainer();
+
+        //#create-container
+        final Source<Optional<ObjectMetadata>, NotUsed> source = BlobService.createContainer(containerName());
+
+        final CompletionStage<Optional<ObjectMetadata>> optionalCompletionStage = source.runWith(Sink.head(), system);
+        //#create-container
+
+        final var optionalObjectMetadata = optionalCompletionStage.toCompletableFuture().get();
+        Assert.assertTrue(optionalObjectMetadata.isPresent());
+        final var objectMetadata = optionalObjectMetadata.get();
+        Assert.assertEquals(objectMetadata.getContentLength(), 0L);
+        Assert.assertEquals(objectMetadata.getETag().get(), ETagRawValue());
+    }
+
+
+    // TODO: There are couple of issues, firstly there are two `Content-Length` headers being added, one by `putBlob`
+    // function and secondly by, most likely, by WireMock. Need to to figure out how to tell WireMock not to add `Content-Length`
+    // header, secondly once that resolve then we get `akka.http.scaladsl.model.EntityStreamException`.
+    @Ignore("Fix this test case")
+    @Test
+    public void putBlob() throws Exception {
+        mockPutBlob();
+
+        //#put-blob
+        final Source<Optional<ObjectMetadata>, NotUsed> source =
+                BlobService.putBlob(containerName() + "/" + blobName(),
+                        ContentTypes.TEXT_PLAIN_UTF8,
+                        contentLength(),
+                        Source.single(ByteString.fromString(payload())),
+                        "BlockBlob", Optional.empty());
+
+        final CompletionStage<Optional<ObjectMetadata>> optionalCompletionStage = source.runWith(Sink.head(), system);
+        //#put-blob
+
+        final var optionalObjectMetadata = optionalCompletionStage.toCompletableFuture().get();
+        Assert.assertTrue(optionalObjectMetadata.isPresent());
+    }
+
+    @Test
+    public void getBlob() throws Exception {
+        mockGetBlob();
+
+        //#get-blob
+        final Source<ByteString, CompletionStage<ObjectMetadata>> source =
+                BlobService.getBlob(containerName() + "/" + blobName(), Optional.empty(), Optional.empty());
+
+        final CompletionStage<List<ByteString>> eventualPayload = source.runWith(Sink.seq(), system);
+        //#get-blob
+
+        final var actualPayload = eventualPayload.toCompletableFuture().get().stream()
+                .map(ByteString::utf8String).collect(Collectors.joining());
+        Assert.assertEquals(actualPayload, payload());
+    }
+
+    @Test
+    public void getBlobRange() throws Exception {
+        mockGetBlobWithRange();
+
+        //#get-blob-range
+        final Source<ByteString, CompletionStage<ObjectMetadata>> source =
+                BlobService.getBlob(containerName() + "/" + blobName(), subRange(),
+                        Optional.empty(), Optional.empty());
+
+        final CompletionStage<List<ByteString>> eventualPayload = source.runWith(Sink.seq(), system);
+        //#get-blob-range
+
+        final var actualPayload = eventualPayload.toCompletableFuture().get().stream()
+                .map(ByteString::utf8String).collect(Collectors.joining());
+        Assert.assertEquals("quick", actualPayload);
+    }
+
+    @Test
+    public void getBlobProperties() throws Exception {
+        mockGetBlobProperties();
+
+        //#get-blob-properties
+        final Source<Optional<ObjectMetadata>, NotUsed> source =
+                BlobService.getProperties(containerName() + "/" + blobName(), Optional.empty(), Optional.empty());
+
+        final CompletionStage<Optional<ObjectMetadata>> optionalCompletionStage = source.runWith(Sink.head(), system);
+        //#get-blob-properties
+
+        final var maybeObjectMetadata = optionalCompletionStage.toCompletableFuture().get();
+        Assert.assertTrue(maybeObjectMetadata.isPresent());
+        final var objectMetadata = maybeObjectMetadata.get();
+        Assert.assertEquals(Optional.of(ETagRawValue()), objectMetadata.getETag());
+        Assert.assertEquals(contentLength(), objectMetadata.getContentLength());
+        Assert.assertEquals(Optional.of(ContentTypes.TEXT_PLAIN_UTF8.toString()), objectMetadata.getContentType());
+    }
+
+    @Test
+    public void deleteBlob() throws Exception {
+        mockDeleteBlob();
+
+        //#delete-blob
+        final Source<Optional<ObjectMetadata>, NotUsed> source =
+                BlobService.deleteBlob(containerName() + "/" + blobName(), Optional.empty(), Optional.empty());
+
+        final CompletionStage<Optional<ObjectMetadata>> optionalCompletionStage = source.runWith(Sink.head(), system);
+        //#delete-blob
+
+        final var maybeObjectMetadata = optionalCompletionStage.toCompletableFuture().get();
+        Assert.assertTrue(maybeObjectMetadata.isPresent());
+        final var objectMetadata = maybeObjectMetadata.get();
+        Assert.assertEquals(Optional.of(ETagRawValue()), objectMetadata.getETag());
+        Assert.assertEquals(0L, objectMetadata.getContentLength());
+    }
+
+    @Test
+    public void createFile() throws Exception {
+        mockCreateFile();
+
+        //#create-file
+        final Source<Optional<ObjectMetadata>, NotUsed> source =
+                FileService.createFile(containerName() + "/" + blobName(), ContentTypes.TEXT_PLAIN_UTF8, contentLength(), Optional.empty());
+
+        final CompletionStage<Optional<ObjectMetadata>> optionalCompletionStage = source.runWith(Sink.head(), system);
+        //#create-file
+
+        final var maybeObjectMetadata = optionalCompletionStage.toCompletableFuture().get();
+        Assert.assertTrue(maybeObjectMetadata.isPresent());
+        final var objectMetadata = maybeObjectMetadata.get();
+        Assert.assertEquals(Optional.of(ETagRawValue()), objectMetadata.getETag());
+        Assert.assertEquals(0L, objectMetadata.getContentLength());
+    }
+
+    // TODO: There are couple of issues, firstly there are two `Content-Length` headers being added, one by `putBlob`
+    // function and secondly by, most likely, by WireMock. Need to to figure out how to tell WireMock not to add `Content-Length`
+    // header, secondly once that resolve then we get `akka.http.scaladsl.model.EntityStreamException`.
+    @Ignore("Fix this test case")
+    @Test
+    public void updateRange() throws Exception {
+        mockCreateFile();
+
+        //#update-range
+        final Source<Optional<ObjectMetadata>, NotUsed> source =
+                FileService.updateRange(containerName() + "/" + blobName(),
+                        ContentTypes.TEXT_PLAIN_UTF8, contentRange(), Source.single(ByteString.fromString(payload())),
+                        Optional.empty());
+
+        final CompletionStage<Optional<ObjectMetadata>> optionalCompletionStage = source.runWith(Sink.head(), system);
+        //#update-range
+
+        final var maybeObjectMetadata = optionalCompletionStage.toCompletableFuture().get();
+        Assert.assertTrue(maybeObjectMetadata.isPresent());
+        final var objectMetadata = maybeObjectMetadata.get();
+        Assert.assertEquals(Optional.of(ETagRawValue()), objectMetadata.getETag());
+        Assert.assertEquals(0L, objectMetadata.getContentLength());
+    }
+
+    @Test
+    public void getFile() throws Exception {
+        mockGetBlob();
+
+        //#get-file
+        final Source<ByteString, CompletionStage<ObjectMetadata>> source =
+                FileService.getFile(containerName() + "/" + blobName(), Optional.empty(), Optional.empty());
+
+        final CompletionStage<List<ByteString>> eventualPayload = source.runWith(Sink.seq(), system);
+        //#get-file
+
+        final var actualPayload = eventualPayload.toCompletableFuture().get().stream()
+                .map(ByteString::utf8String).collect(Collectors.joining());
+        Assert.assertEquals(actualPayload, payload());
+    }
+
+    @Test
+    public void getFileProperties() throws Exception {
+        mockGetBlobProperties();
+
+        //#get-file-properties
+        final Source<Optional<ObjectMetadata>, NotUsed> source =
+                FileService.getProperties(containerName() + "/" + blobName(), Optional.empty(), Optional.empty());
+
+        final CompletionStage<Optional<ObjectMetadata>> optionalCompletionStage = source.runWith(Sink.head(), system);
+        //#get-file-properties
+
+        final var maybeObjectMetadata = optionalCompletionStage.toCompletableFuture().get();
+        Assert.assertTrue(maybeObjectMetadata.isPresent());
+        final var objectMetadata = maybeObjectMetadata.get();
+        Assert.assertEquals(Optional.of(ETagRawValue()), objectMetadata.getETag());
+        Assert.assertEquals(contentLength(), objectMetadata.getContentLength());
+        Assert.assertEquals(Optional.of(ContentTypes.TEXT_PLAIN_UTF8.toString()), objectMetadata.getContentType());
+    }
+
+    // TODO: There are couple of issues, firstly there are two `Content-Length` headers being added, one by `putBlob`
+    // function and secondly by, most likely, by WireMock. Need to to figure out how to tell WireMock not to add `Content-Length`
+    // header, secondly once that resolve then we get `akka.http.scaladsl.model.EntityStreamException`.
+    @Ignore("Fix this test case")
+    @Test
+    public void clearRange() throws Exception {
+        mockClearRange();
+
+        //#clear-range
+        final Source<Optional<ObjectMetadata>, NotUsed> source =
+                FileService.clearRange(containerName() + "/" + blobName(), subRange(), Optional.empty());
+
+        final CompletionStage<Optional<ObjectMetadata>> optionalCompletionStage = source.runWith(Sink.head(), system);
+        //#clear-range
+
+        final var maybeObjectMetadata = optionalCompletionStage.toCompletableFuture().get();
+        Assert.assertTrue(maybeObjectMetadata.isPresent());
+        final var objectMetadata = maybeObjectMetadata.get();
+        Assert.assertEquals(Optional.of(ETagRawValue()), objectMetadata.getETag());
+        Assert.assertEquals(0L, objectMetadata.getContentLength());
+    }
+
+    @Test
+    public void deleteFile() throws Exception {
+        mockDeleteBlob();
+
+        //#delete-file
+        final Source<Optional<ObjectMetadata>, NotUsed> source =
+                FileService.deleteFile(containerName() + "/" + blobName(), Optional.empty(), Optional.empty());
+
+        final CompletionStage<Optional<ObjectMetadata>> optionalCompletionStage = source.runWith(Sink.head(), system);
+        //#delete-file
+
+        final var maybeObjectMetadata = optionalCompletionStage.toCompletableFuture().get();
+        Assert.assertTrue(maybeObjectMetadata.isPresent());
+        final var objectMetadata = maybeObjectMetadata.get();
+        Assert.assertEquals(Optional.of(ETagRawValue()), objectMetadata.getETag());
+        Assert.assertEquals(0L, objectMetadata.getContentLength());
+    }
+}
---
+++ azure-storage/src/test/scala/docs/scaladsl/StorageSpec.scala
@@ -0,0 +1,291 @@
+/*
+ * Copyright (C) since 2016 Lightbend Inc. <https://www.lightbend.com>
+ */
+
+package docs.scaladsl
+
+import akka.NotUsed
+import akka.http.scaladsl.model.ContentTypes
+import akka.stream.alpakka.azure.storage.scaladsl.StorageWireMockBase
+import akka.stream.alpakka.azure.storage.scaladsl.StorageWireMockBase.ETagRawValue
+import akka.stream.alpakka.testkit.scaladsl.LogCapturing
+import akka.stream.scaladsl.{Keep, Sink, Source}
+import akka.util.ByteString
+import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpecLike
+import org.scalatest.{BeforeAndAfterAll, BeforeAndAfterEach, OptionValues}
+
+import scala.concurrent.Future
+import scala.concurrent.duration._
+
+class StorageSpec
+    extends StorageWireMockBase
+    with AnyWordSpecLike
+    with BeforeAndAfterAll
+    with BeforeAndAfterEach
+    with Matchers
+    with ScalaFutures
+    with OptionValues
+    with LogCapturing {
+
+  override implicit val patienceConfig: PatienceConfig = PatienceConfig(3.minutes, 100.millis)
+
+  override protected def afterEach(): Unit = mock.removeMappings()
+
+  "AzureStorage Blob connector" should {
+
+    "create container" in {
+      mockCreateContainer()
+
+      //#create-container
+      import akka.stream.alpakka.azure.storage.scaladsl.BlobService
+      import akka.stream.alpakka.azure.storage.ObjectMetadata
+
+      val source: Source[Option[ObjectMetadata], NotUsed] = BlobService.createContainer(containerName)
+
+      val eventualMaybeMetadata: Future[Option[ObjectMetadata]] = source.runWith(Sink.head)
+      //#create-container
+
+      val maybeObjectMetadata = eventualMaybeMetadata.futureValue
+      maybeObjectMetadata shouldBe defined
+      val objectMetadata = maybeObjectMetadata.get
+      objectMetadata.contentLength shouldBe 0L
+      objectMetadata.eTag shouldBe Some(ETagRawValue)
+    }
+
+    // TODO: There are couple of issues, firstly there are two `Content-Length` headers being added, one by `putBlob`
+    // function and secondly by, most likely, by WireMock. Need to to figure out how to tell WireMock not to add `Content-Length`
+    // header, secondly once that resolve then we get `akka.http.scaladsl.model.EntityStreamException`.
+    "put blob" ignore {
+      mockPutBlob()
+
+      //#put-blob
+      import akka.stream.alpakka.azure.storage.scaladsl.BlobService
+      import akka.stream.alpakka.azure.storage.ObjectMetadata
+
+      val source: Source[Option[ObjectMetadata], NotUsed] =
+        BlobService.putBlob(
+          objectPath = s"$containerName/$blobName",
+          contentType = ContentTypes.`text/plain(UTF-8)`,
+          contentLength = contentLength,
+          payload = Source.single(ByteString(payload))
+        )
+
+      val eventualMaybeMetadata: Future[Option[ObjectMetadata]] = source.runWith(Sink.head)
+      //#put-blob
+
+      val maybeObjectMetadata = eventualMaybeMetadata.futureValue
+      maybeObjectMetadata shouldBe defined
+      val objectMetadata = maybeObjectMetadata.get
+      objectMetadata.contentLength shouldBe contentLength
+      objectMetadata.eTag shouldBe Some(ETagRawValue)
+    }
+
+    "get blob" in {
+      mockGetBlob()
+
+      //#get-blob
+      import akka.stream.alpakka.azure.storage.scaladsl.BlobService
+      import akka.stream.alpakka.azure.storage.ObjectMetadata
+
+      val source: Source[ByteString, Future[ObjectMetadata]] =
+        BlobService.getBlob(objectPath = s"$containerName/$blobName")
+
+      val eventualText = source.toMat(Sink.seq)(Keep.right).run()
+      //#get-blob
+
+      eventualText.futureValue.map(_.utf8String).mkString("") shouldBe payload
+    }
+
+    "get blob range" in {
+      mockGetBlobWithRange()
+
+      //#get-blob-range
+      import akka.stream.alpakka.azure.storage.scaladsl.BlobService
+      import akka.stream.alpakka.azure.storage.ObjectMetadata
+
+      val source: Source[ByteString, Future[ObjectMetadata]] =
+        BlobService.getBlob(objectPath = s"$containerName/$blobName", range = Some(subRange))
+
+      val eventualText: Future[Seq[ByteString]] = source.toMat(Sink.seq)(Keep.right).run()
+      //#get-blob-range
+
+      eventualText.futureValue.map(_.utf8String).mkString("") shouldBe "quick"
+    }
+
+    "get blob properties" in {
+      mockGetBlobProperties();
+
+      //#get-blob-properties
+      import akka.stream.alpakka.azure.storage.scaladsl.BlobService
+      import akka.stream.alpakka.azure.storage.ObjectMetadata
+
+      val source: Source[Option[ObjectMetadata], NotUsed] =
+        BlobService.getProperties(objectPath = s"$containerName/$blobName")
+
+      val eventualMaybeMetadata: Future[Option[ObjectMetadata]] = source.runWith(Sink.head)
+      //#get-blob-properties
+
+      val maybeMetadata = eventualMaybeMetadata.futureValue
+      maybeMetadata shouldBe defined
+      val metadata = maybeMetadata.get
+      metadata.eTag shouldBe Some(ETagRawValue)
+      metadata.contentLength shouldBe contentLength
+      metadata.contentType shouldBe Some(ContentTypes.`text/plain(UTF-8)`.value)
+    }
+
+    "delete blob" in {
+      mockDeleteBlob()
+
+      //#delete-blob
+      import akka.stream.alpakka.azure.storage.scaladsl.BlobService
+      import akka.stream.alpakka.azure.storage.ObjectMetadata
+
+      val source: Source[Option[ObjectMetadata], NotUsed] =
+        BlobService.deleteBlob(objectPath = s"$containerName/$blobName")
+
+      val eventualMaybeMetadata: Future[Option[ObjectMetadata]] = source.runWith(Sink.head)
+      //#delete-blob
+
+      val maybeMetadata = eventualMaybeMetadata.futureValue
+      maybeMetadata shouldBe defined
+      val metadata = maybeMetadata.get
+      metadata.eTag shouldBe Some(ETagRawValue)
+      metadata.contentLength shouldBe 0L
+    }
+  }
+
+  "AzureStorage File connector" should {
+
+    "create file" in {
+      mockCreateFile()
+
+      //#create-file
+      import akka.stream.alpakka.azure.storage.scaladsl.FileService
+      import akka.stream.alpakka.azure.storage.ObjectMetadata
+
+      val source: Source[Option[ObjectMetadata], NotUsed] =
+        FileService.createFile(objectPath = s"$containerName/$blobName",
+                               contentType = ContentTypes.`text/plain(UTF-8)`,
+                               maxSize = contentLength)
+
+      val eventualMaybeMetadata: Future[Option[ObjectMetadata]] = source.runWith(Sink.head)
+      //#create-file
+
+      val maybeMetadata = eventualMaybeMetadata.futureValue
+      maybeMetadata shouldBe defined
+      val metadata = maybeMetadata.get
+      metadata.eTag shouldBe Some(ETagRawValue)
+      metadata.contentLength shouldBe 0L
+    }
+
+    // TODO: There are couple of issues, firstly there are two `Content-Length` headers being added, one by `putBlob`
+    // function and secondly by, most likely, by WireMock. Need to to figure out how to tell WireMock not to add `Content-Length`
+    // header, secondly once that resolve then we get `akka.http.scaladsl.model.EntityStreamException`.
+    "update range" ignore {
+      mockUpdateRange()
+
+      //#update-range
+      import akka.stream.alpakka.azure.storage.scaladsl.FileService
+      import akka.stream.alpakka.azure.storage.ObjectMetadata
+
+      val source: Source[Option[ObjectMetadata], NotUsed] =
+        FileService.updateRange(
+          objectPath = s"$containerName/$blobName",
+          contentType = ContentTypes.`text/plain(UTF-8)`,
+          range = contentRange,
+          payload = Source.single(ByteString(payload))
+        )
+
+      val eventualMaybeMetadata: Future[Option[ObjectMetadata]] = source.toMat(Sink.head)(Keep.right).run()
+      //#update-range
+
+      val maybeObjectMetadata = eventualMaybeMetadata.futureValue
+      maybeObjectMetadata shouldBe defined
+      val objectMetadata = maybeObjectMetadata.get
+      objectMetadata.contentLength shouldBe 0
+      objectMetadata.eTag shouldBe Some(ETagRawValue)
+    }
+
+    "get file" in {
+      mockGetBlob()
+
+      //#get-file
+      import akka.stream.alpakka.azure.storage.scaladsl.FileService
+      import akka.stream.alpakka.azure.storage.ObjectMetadata
+
+      val source: Source[ByteString, Future[ObjectMetadata]] =
+        FileService.getFile(objectPath = s"$containerName/$blobName")
+
+      val eventualText: Future[Seq[ByteString]] = source.toMat(Sink.seq)(Keep.right).run()
+      //#get-file
+
+      eventualText.futureValue.map(_.utf8String).mkString("") shouldBe payload
+    }
+
+    "get file properties" in {
+      mockGetBlobProperties()
+
+      //#get-file-properties
+      import akka.stream.alpakka.azure.storage.scaladsl.FileService
+      import akka.stream.alpakka.azure.storage.ObjectMetadata
+
+      val source: Source[Option[ObjectMetadata], NotUsed] =
+        FileService.getProperties(objectPath = s"$containerName/$blobName")
+
+      val eventualMaybeMetadata: Future[Option[ObjectMetadata]] = source.toMat(Sink.head)(Keep.right).run()
+      //#get-file-properties
+
+      val maybeMetadata = eventualMaybeMetadata.futureValue
+      maybeMetadata shouldBe defined
+      val metadata = maybeMetadata.get
+      metadata.eTag shouldBe Some(ETagRawValue)
+      metadata.contentLength shouldBe contentLength
+      metadata.contentType shouldBe Some(ContentTypes.`text/plain(UTF-8)`.value)
+    }
+
+    // TODO: There are couple of issues, firstly there are two `Content-Length` headers being added, one by `putBlob`
+    // function and secondly by, most likely, by WireMock. Need to to figure out how to tell WireMock not to add `Content-Length`
+    // header, secondly once that resolve then we get `akka.http.scaladsl.model.EntityStreamException`.
+    "clear range" ignore {
+      mockClearRange()
+
+      //#clear-range
+      import akka.stream.alpakka.azure.storage.scaladsl.FileService
+      import akka.stream.alpakka.azure.storage.ObjectMetadata
+
+      val source: Source[Option[ObjectMetadata], NotUsed] =
+        FileService.clearRange(objectPath = s"$containerName/$blobName", range = subRange)
+
+      val eventualMaybeMetadata: Future[Option[ObjectMetadata]] = source.runWith(Sink.head)
+      //#clear-range
+
+      val maybeMetadata = eventualMaybeMetadata.futureValue
+      maybeMetadata shouldBe defined
+      val metadata = maybeMetadata.get
+      metadata.eTag shouldBe Some(ETagRawValue)
+      metadata.contentLength shouldBe 0L
+    }
+
+    "delete file" in {
+      mockDeleteBlob()
+
+      //#delete-file
+      import akka.stream.alpakka.azure.storage.scaladsl.FileService
+      import akka.stream.alpakka.azure.storage.ObjectMetadata
+
+      val source: Source[Option[ObjectMetadata], NotUsed] =
+        FileService.deleteFile(objectPath = s"$containerName/$blobName")
+
+      val eventualMaybeMetadata: Future[Option[ObjectMetadata]] = source.runWith(Sink.head)
+      //#delete-file
+
+      val maybeMetadata = eventualMaybeMetadata.futureValue
+      maybeMetadata shouldBe defined
+      val metadata = maybeMetadata.get
+      metadata.eTag shouldBe Some(ETagRawValue)
+      metadata.contentLength shouldBe 0L
+    }
+  }
+}
