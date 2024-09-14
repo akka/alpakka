986faa20d8af9dd54efca698ec924c410102d020
Scala Steward <scala_steward@virtuslab.com>
2024-09-14 13:02:09 +0000

    bump: spring-boot-autoconfigure, ... 3.3.3 (was 2.7.18)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -450,7 +450,7 @@ object Dependencies {
 
   val SpringWeb = {
     val SpringVersion = "5.3.33"
-    val SpringBootVersion = "2.7.18"
+    val SpringBootVersion = "3.3.3"
     Seq(
       libraryDependencies ++= Seq(
           "org.springframework" % "spring-core" % SpringVersion,
