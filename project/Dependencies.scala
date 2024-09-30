1bc75a6d84c4a81de945dda10de15348749b6cc4
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2024-09-30 11:43:09 +0200

    bump: spring-context, spring-core 5.3.39 (was 5.3.33) (#3266)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -454,7 +454,7 @@ object Dependencies {
   )
 
   val SpringWeb = {
-    val SpringVersion = "5.3.33"
+    val SpringVersion = "5.3.39"
     val SpringBootVersion = "2.7.18"
     Seq(
       libraryDependencies ++= Seq(
