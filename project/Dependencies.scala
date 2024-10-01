87816e1c2a3f563f2d825da09093c7fdafc38748
Enno Runne <458526+ennru@users.noreply.github.com>
2024-10-01 17:14:40 +0200

    Bump commons-net 3.11.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -197,7 +197,7 @@ object Dependencies {
 
   val Ftp = Seq(
     libraryDependencies ++= Seq(
-        "commons-net" % "commons-net" % "3.10.0",
+        "commons-net" % "commons-net" % "3.11.1",
         // Versions after sshj 0.35.0 depend on SLF4J 2.x
         "com.hierynomus" % "sshj" % "0.39.0",
         ("io.github.hakky54" % "sslcontext-kickstart-for-pem" % "8.3.5" % Test).exclude("org.slf4j", "slf4j-api")
