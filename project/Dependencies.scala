97ea22be7d9a876a5b276deaff1267537d78b127
Scala Steward <scala_steward@virtuslab.com>
2024-11-13 14:19:19 +0000

    bump: artemis-jakarta-client, ... 2.38.0 (was 2.33.0)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -360,8 +360,8 @@ object Dependencies {
   val JakartaJms = Seq(
     libraryDependencies ++= Seq(
         "jakarta.jms" % "jakarta.jms-api" % "3.1.0", // Eclipse Public License 2.0 + + GPLv2
-        ("org.apache.activemq" % "artemis-jakarta-server" % "2.33.0" % Test),
-        ("org.apache.activemq" % "artemis-jakarta-client" % "2.33.0" % Test)
+        ("org.apache.activemq" % "artemis-jakarta-server" % "2.38.0" % Test),
+        ("org.apache.activemq" % "artemis-jakarta-client" % "2.38.0" % Test)
       ) ++ Mockito
   )
 
