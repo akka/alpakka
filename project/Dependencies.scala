95290cfe0c614dca80ee454eaade10d2c39b58a1
Scala Steward <scala_steward@virtuslab.com>
2025-02-17 14:17:42 +0000

    bump: google-auth-library-credentials, ... 1.32.1 (was 1.24.1)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -233,7 +233,7 @@ object Dependencies {
       ) ++ JacksonDatabindDependencies
   )
 
-  val GoogleAuthVersion = "1.24.1"
+  val GoogleAuthVersion = "1.32.1"
 
   val GoogleCommon = Seq(
     libraryDependencies ++= Seq(
