20aad484dd54c866bdfb6d4b4cd89b24fc51ac5f
Sebastian Alfers <sebastian.alfers@lightbend.com>
2026-04-10 16:05:26 +0200

    bump: alpakka-kafka to 8.0.1 (#3451)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -28,7 +28,7 @@ object Dependencies {
   val AkkaHttpBinaryVersion = VersionNumber(AkkaHttpVersion).numbers match {
     case Seq(major, minor, _*) => s"$major.$minor"
   }
-  val AlpakkaKafkaVersion = "8.0.0"
+  val AlpakkaKafkaVersion = "8.0.1"
   val ScalaTestVersion = "3.2.19"
   val TestContainersScalaTestVersion = "0.40.3" // pulls Testcontainers 1.16.2
 
