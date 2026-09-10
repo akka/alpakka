a582855916259a182391fc7d995ff265b9263ab0
Johan Andrén <johan@markatta.com>
2026-09-10 07:55:20 +0200

    bump: Azure identity 1.18.5, azure-storage-blob  12.35.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -118,7 +118,7 @@ object Dependencies {
   )
 
   val AzureCoreVersion = "1.55.5"
-  val AzureIdentityVersion = "1.18.4"
+  val AzureIdentityVersion = "1.18.5"
 
   val AzureStorage = Seq(
     libraryDependencies ++= Seq(
