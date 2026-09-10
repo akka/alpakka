29ae8273e23113bcc7d08fd7c61ea8932822bb5e
Johan Andrén <johan@markatta.com>
2026-09-10 08:31:04 +0200

    bump: azure-core 1.55.5 → 1.59.0 and azure-storage-common 12.34.0 → 12.34.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -117,7 +117,7 @@ object Dependencies {
       )
   )
 
-  val AzureCoreVersion = "1.55.5"
+  val AzureCoreVersion = "1.59.0"
   val AzureIdentityVersion = "1.18.5"
 
   val AzureStorage = Seq(
@@ -127,7 +127,7 @@ object Dependencies {
         "com.azure" % "azure-core" % AzureCoreVersion, // MIT - TokenCredential API
         "com.azure" % "azure-identity" % AzureIdentityVersion % "provided,test", // MIT - optional, needed for DefaultAzureCredential
         // for testing authorization signature
-        "com.azure" % "azure-storage-common" % "12.34.0" % Test,
+        "com.azure" % "azure-storage-common" % "12.34.1" % Test,
         wiremock
       )
   )
